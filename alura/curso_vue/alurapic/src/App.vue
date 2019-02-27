796798698073e18ecd81265a7ef683c4402a6b6b
flaviogf <flavio.fernandes6@gmail.com>
2019-02-27 18:11:11 -0300

    .

---
+++ alura/curso_vue/alurapic/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h1>{{ titulo }}</h1>
+    <ul>
+      <li v-for='foto of fotos'>
+        <img :src="foto.url" :alt="foto.titulo">
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  created() {
+    this.$http.get('http://localhost:3000/v1/fotos')
+      .then(res => res.json())
+      .then(fotos => this.fotos = fotos)
+      .catch(console.log)
+  },
+  data() {
+    return {
+      titulo: 'Alurapic',
+      fotos: [],
+    }
+  },
+}
+</script>
+
+<style lang="scss">
+</style>
