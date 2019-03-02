ee8099b06f9fb4315ea2f7aa47a2ad67a29976e3
flaviogf <flavio.fernandes6@gmail.com>
2019-03-01 22:13:21 -0300

    .

--- alura/curso_vue/alurapic/src/App.vue
+++ alura/curso_vue/alurapic/src/App.vue
@@ -1,30 +1,73 @@
 <template>
   <div>
     <h1>{{ titulo }}</h1>
+    <input type="text" placeholder="filtre pelo nome" @input="filtro = $event.target.value">
     <ul>
-      <li v-for='foto of fotos'>
-        <img :src="foto.url" :alt="foto.titulo">
+      <li v-for="foto of fotosFiltradas">
+        <alurapic-painel :titulo="foto.titulo">
+          <img :src="foto.url" :alt="foto.titulo">
+        </alurapic-painel>
       </li>
     </ul>
   </div>
 </template>
 
 <script>
+import Painel from "./components/shared/painel/Painel.vue";
+
 export default {
+  components: {
+    "alurapic-painel": Painel
+  },
   created() {
-    this.$http.get('http://localhost:3000/v1/fotos')
+    this.$http
+      .get("http://localhost:3000/v1/fotos")
       .then(res => res.json())
-      .then(fotos => this.fotos = fotos)
-      .catch(console.log)
+      .then(fotos => (this.fotos = fotos))
+      .catch(console.log);
   },
   data() {
     return {
-      titulo: 'Alurapic',
+      titulo: "Alurapic",
       fotos: [],
-    }
+      filtro: ""
+    };
   },
-}
+  computed: {
+    fotosFiltradas() {
+      if (!this.filtro) {
+        return this.fotos;
+      } else {
+        const filtro = new RegExp(this.filtro, "i");
+        return this.fotos.filter(it => filtro.test(it.titulo));
+      }
+    }
+  }
+};
 </script>
 
-<style lang="scss">
+<style lang="sass">
+body
+  font-family: Helvetica, sans-serif
+  background-color: #f5f5f5
+  text-align: center
+  margin: 0 auto
+  width: 75%
+
+ul
+  padding: 0
+
+
+ul li
+  list-style-type: none
+  display: inline-block
+
+img
+  width: 100%
+
+input
+  padding: 3px 5px
+  font-size: 18px
+  height: 35px
+  width: 100%
 </style>
---
+++ alura/curso_vue/alurapic/src/components/shared/painel/Painel.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="painel">
+    <div class="painel__titulo">{{ titulo }}</div>
+    <div class="painel__conteudo">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["titulo"]
+};
+</script>
+
+<style lang="sass" scoped>
+.painel
+  box-shadow: 0 1px 1px rgba(0,0,0,.2)
+  background-color: white
+  overflow-x: hidden
+  overflow-y: hidden
+  flex-direction: column
+  border-radius: 3px
+  padding: 8px 7px
+  height: 275px
+  width: 275px
+  display: flex
+  margin: 5px
+
+.painel__titulo
+  font-weight: bold
+  padding: 5px
+  font-size: 24px
+  flex: 1
+
+.painel__conteudo
+  padding: 5px
+  align-items: center
+  display: flex
+  flex: 3
+</style>
