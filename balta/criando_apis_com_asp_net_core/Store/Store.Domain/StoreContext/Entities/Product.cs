c9c6517f153c88f41179097a1c43aee8c1c2484e
flaviogf <flavio.fernandes6@gmail.com>
2019-05-09 08:53:51 -0300

    .

--- balta/criando_apis_com_asp_net_core/Store/Store.Domain/StoreContext/Entities/Product.cs
+++ balta/criando_apis_com_asp_net_core/Store/Store.Domain/StoreContext/Entities/Product.cs
@@ -8,19 +8,41 @@ namespace Store.Domain.StoreContext.Entities
         public Product
         (
             string title,
-            string description
+            string description,
+            string image,
+            decimal price,
+            decimal quantity
         )
         {
             Title = title;
             Description = description;
+            Image = image;
+            Price = price;
+            Quantity = quantity;
 
             AddNotifications(new Contract()
                 .Requires()
                 .HasMinLen(title, 2, nameof(Title), "Invalid title")
-                .HasMinLen(description, 2, nameof(Description), "Invalid description"));
+                .HasMinLen(description, 2, nameof(Description), "Invalid description")
+                .IsNotNullOrEmpty(image, nameof(Image), "Invalid image")
+                .IsGreaterThan(price, 0, nameof(Price), "Invalid price")
+                .IsGreaterOrEqualsThan(quantity, 0, nameof(Quantity), "Invalid quantity"));
         }
 
         public string Title { get; private set; }
         public string Description { get; private set; }
+        public string Image { get; private set; }
+        public decimal Price { get; private set; }
+        public decimal Quantity { get; private set; }
+
+        public void DecreaseQuantity(decimal quantity)
+        {
+            if (Quantity - quantity < 0)
+            {
+                AddNotification(nameof(Quantity), "Quantity not available");
+                return;
+            }
+            Quantity -= quantity;
+        }
     }
 }
--- balta/criando_apis_com_asp_net_core/Store/Store.Tests/Domain/StoreContext/Entities/ProductTests.cs
+++ balta/criando_apis_com_asp_net_core/Store/Store.Tests/Domain/StoreContext/Entities/ProductTests.cs
@@ -5,40 +5,117 @@ namespace Store.Tests.Domain.StoreContext.Entities
 {
     public class ProductTests
     {
+        private Product _xbox;
+
+        public ProductTests()
+        {
+            _xbox = new Product(
+                "Xbox One",
+                "Microsof Xbox One",
+                "xbox.jpg",
+                100.99M,
+                10M
+            );
+        }
+
         [Fact]
         public void ShouldValidTrueWhenAllInformationIsValid()
         {
-            var xbox = new Product(
-                "Xbox One",
-                "Microsof Xbox One"
-            );
-
-            Assert.True(xbox.Valid);
+            Assert.True(_xbox.Valid);
         }
 
+        [Fact]
         public void ShouldInvalidTrueWhenTitleDoNotContainsMinLenTwo()
         {
+            var xbox = new Product(
+                "",
+                "Microsof Xbox One",
+                "xbox.jpg",
+                100.99M,
+                0M
+            );
 
+            Assert.True(xbox.Invalid);
         }
 
+        [Fact]
         public void ShouldInvalidTrueWhenDescriptionDoNotContainsMinLenTwo()
         {
+            var xbox = new Product(
+                "Xbox One",
+                "",
+                "xbox.jpg",
+                100.99M,
+                0M
+            );
 
+            Assert.True(xbox.Invalid);
         }
 
+        [Fact]
         public void ShouldInvalidTrueWhenImageIsEmpty()
         {
+            var xbox = new Product(
+                "Xbox One",
+                "Microsof Xbox One",
+                "",
+                100.99M,
+                0M
+            );
 
+            Assert.True(xbox.Invalid);
         }
 
+        [Fact]
         public void ShouldInvalidTrueWhenPriceIsLowerOrEqualsThanZero()
         {
+            var xbox = new Product(
+                "Xbox One",
+                "Microsof Xbox One",
+                "xbox.jpg",
+                0M,
+                0M
+            );
 
+            Assert.True(xbox.Invalid);
         }
 
+        [Fact]
         public void ShouldInvalidTrueWhenQuantityIsLowerThanZero()
         {
+            var xbox = new Product(
+                "Xbox One",
+                "Microsof Xbox One",
+                "xbox.jpg",
+                100.99M,
+                -1M
+            );
 
+            Assert.True(xbox.Invalid);
+        }
+
+        [Fact]
+        public void ShouldDecreaseQuantityWhenQuantityIsAvailable()
+        {
+            _xbox.DecreaseQuantity(5M);
+
+            Assert.Equal(5M, _xbox.Quantity);
+        }
+
+        [Fact]
+        public void ShouldDoNotDecreaseQuantityWhenDecreaseQuantityIsCalledWithQuantityGreaterThanQuantityAvailable()
+        {
+            _xbox.DecreaseQuantity(20M);
+
+            Assert.Equal(10M, _xbox.Quantity);
+        }
+
+        [Fact]
+        public void ShouldHasNotificiationWhenDecreaseQuantityIsCalledWithQuantityGreaterThanQuantityAvailable()
+        {
+            _xbox.DecreaseQuantity(20M);
+
+            Assert.Equal(1, _xbox.Notifications.Count);
         }
     }
 }
