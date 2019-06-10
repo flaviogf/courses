7ac620ac14853bcf5851d1c4a20f381a35984424
flaviogf <flavio.fernandes6@gmail.com>
2019-06-10 13:07:44 -0300

    .

---
+++ alura/certificacao_csharp/Section4/Section4.ConditionalCompilation/Program.cs
@@ -0,0 +1,27 @@
+﻿#define ADVANCED
+
+using System;
+using System.IO;
+
+namespace Section4.ConditionalCompilation
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            using (var writer = new StreamWriter("log.txt"))
+            {
+                Console.SetOut(writer);
+#if TRIAL
+                Console.WriteLine("TRIAL");
+#elif BASIC
+                Console.WriteLine("BASIC");
+#elif ADVANCED
+                Console.WriteLine("ADVANCED");
+#endif
+
+                Console.WriteLine("END");
+            }
+        }
+    }
+}
