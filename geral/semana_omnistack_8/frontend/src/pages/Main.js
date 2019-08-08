df43d5467b51f291b5f4353886e592be5644ecc0
flaviogf <flavio.fernandes6@gmail.com>
2019-08-08 18:18:18 -0300

    .

--- geral/semana_omnistack_8/frontend/src/pages/Main.js
+++ geral/semana_omnistack_8/frontend/src/pages/Main.js
@@ -1,5 +1,43 @@
 import React from "react";
+import "./Main.css";
+
+const devs = [
+  {
+    _id: "",
+    name: "Barry",
+    bio: "The fastest man alive.",
+    avatar: "https://api.adorable.io/avatars/285/abott@adorable.png"
+  },
+  {
+    _id: "",
+    name: "Barry",
+    bio: "The fastest man alive.",
+    avatar: "https://api.adorable.io/avatars/285/abott@adorable.png"
+  }
+];
 
 export default function Main() {
-  return <h1>Main</h1>;
+  return (
+    <div className="main__container">
+      <ul>
+        {devs.map(dev => (
+          <li className="main__card">
+            <header className="main__card-header">
+              <img className="main__card-img" src={dev.avatar} alt="avatar" />
+
+              <div className="main__card-description">
+                <span className="main__card-name">{dev.name}</span>
+                <span className="main__card-bio">{dev.bio}</span>
+              </div>
+            </header>
+
+            <footer className="main__card-footer">
+              <button type="button">Like</button>
+              <button type="button">Dislike</button>
+            </footer>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
