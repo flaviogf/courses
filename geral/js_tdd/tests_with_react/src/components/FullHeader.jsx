73d98208af42af2e80fc8408a1d22781c1ddd4a9
flaviogf <flavio.fernandes6@gmail.com>
2019-08-20 22:05:21 -0300

    .

--- geral/js_tdd/tests_with_react/src/components/FullHeader.jsx
+++ geral/js_tdd/tests_with_react/src/components/FullHeader.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-export default function FullHeader({ title, subtitle }) {
+export default function FullHeader({ title, subtitle, bgColor }) {
+  const headerStyle = {
+    backgroundColor: bgColor
+  };
+
   return (
-    <header>
+    <header style={headerStyle}>
       {title && <h1>{title}</h1>}
       {subtitle && <h2>{subtitle}</h2>}
     </header>
@@ -12,10 +16,12 @@ export default function FullHeader({ title, subtitle }) {
 
 FullHeader.propTypes = {
   title: PropTypes.string,
-  subtitle: PropTypes.string
+  subtitle: PropTypes.string,
+  bgColor: PropTypes.string
 };
 
 FullHeader.defaultProps = {
   title: '',
-  subtitle: ''
+  subtitle: '',
+  bgColor: '#ccc'
 };
--- geral/js_tdd/tests_with_react/src/components/FullHeader.spec.jsx
+++ geral/js_tdd/tests_with_react/src/components/FullHeader.spec.jsx
@@ -3,13 +3,13 @@ import { shallow } from 'enzyme';
 
 import FullHeader from './FullHeader';
 
-describe('<FullHeader />', () => {
+describe('FullHeader', () => {
   test('should have header tag when mount', () => {
     const wrapper = shallow(<FullHeader />);
     expect(wrapper.find('header')).toHaveLength(1);
   });
 
-  describe('<h1>', () => {
+  describe('h1', () => {
     test('should have h1 tag when title is passed', () => {
       const wrapper = shallow(<FullHeader title="Hello" />);
       expect(wrapper.find('h1')).toHaveLength(1);
@@ -26,7 +26,7 @@ describe('<FullHeader />', () => {
     });
   });
 
-  describe('<h2>', () => {
+  describe('h2', () => {
     test('should have h2 tag when subtitle is passed', () => {
       const wrapper = shallow(<FullHeader subtitle="World" />);
       expect(wrapper.find('h2')).toHaveLength(1);
@@ -40,6 +40,18 @@ describe('<FullHeader />', () => {
     test('should have h2 tag the subtitle passed', () => {
       const wrapper = shallow(<FullHeader subtitle="World" />);
       expect(wrapper.find('h2').props().children).toBe('World');
+    });
+  });
+
+  describe('bgColor', () => {
+    test('should default bgColor equal to #ccc when color not is passed', () => {
+      const wrapper = shallow(<FullHeader />);
+      expect(wrapper.get(0).props.style.backgroundColor).toBe('#ccc');
+    });
+
+    test('should bgColor equal to #f5f5f5 when bgColor is passed', () => {
+      const wrapper = shallow(<FullHeader bgColor="#f5f5f5" />);
+      expect(wrapper.get(0).props.style.backgroundColor).toBe('#f5f5f5');
     });
   });
 });
