168eef79826305f9dd08dd5928db8f3a5be08c86
flaviogf <flavio.fernandes6@gmail.com>
2019-10-17 06:56:21 -0300

    .

---
+++ alura/certificacao_csharp/Section5/Section5.CollectionGarbage/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Section5.CollectionGarbage
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+          for(int i = 0; i < 1000000; i++)
+          {
+            new Person
+            {
+              Name = "Test"
+            };
+          }
+
+          // GC.Collect();
+        }
+    }
+
+    public class Person
+    {
+        public string Name { get; set; }
+
+        public Person()
+        {
+          Console.WriteLine("Begin");
+        }
+
+        ~Person()
+        {
+          Console.WriteLine("End");
+        }
+    }
+}
