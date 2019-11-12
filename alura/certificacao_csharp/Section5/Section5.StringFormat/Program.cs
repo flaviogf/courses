5725935e5303f13025879ae77db16efa078a8cb8
flaviogf <flavio.fernandes6@gmail.com>
2019-11-12 09:03:02 -0300

    .

---
+++ alura/certificacao_csharp/Section5/Section5.StringFormat/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Section5.StringFormat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var account = new Account(holder: "Flavio", bank: "Caixa", createdAt: DateTime.Now, balance: 1000);
+
+            Console.WriteLine(account);
+        }
+    }
+
+    class Account
+    {
+        public Account(string holder, string bank, DateTime createdAt, decimal balance)
+        {
+            Holder = holder;
+            Bank = bank;
+            CreatedAt = createdAt;
+            Balance = balance;
+        }
+
+        public string Holder { get; private set; }
+
+        public string Bank { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        public decimal Balance { get; set; }
+
+        public override string ToString() => $"Account[Holder={Holder}, Bank={Bank}, CreatedAt={CreatedAt:D}, Balance={Balance:C}]";
+    }
+}
