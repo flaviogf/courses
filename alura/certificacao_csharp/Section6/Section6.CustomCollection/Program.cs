137c90822ff574437e6a3e34efcf481c4774a23c
flaviogf <flavio.fernandes6@gmail.com>
2019-11-14 09:29:24 -0300

    .

---
+++ alura/certificacao_csharp/Section6/Section6.CustomCollection/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace Section6.CustomCollection
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkMagenta;
+
+            var carLicensePlates = new CarLicensePlates
+            {
+                "AAA-0000",
+                "BBB-1111"
+            };
+
+            try
+            {
+                carLicensePlates.Add("test");
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            carLicensePlates.Print();
+        }
+    }
+
+    public class CarLicensePlates : ICollection<string>
+    {
+        private List<string> _list = new List<string>();
+
+        public int Count => _list.Count;
+
+        public bool IsReadOnly => false;
+
+        public void Add(string item)
+        {
+            if (!IsValid(item))
+            {
+                throw new ArgumentException("License must be valid");
+            }
+
+            _list.Add(item);
+        }
+
+        public void Clear()
+        {
+            _list.Clear();
+        }
+
+        public bool Contains(string item)
+        {
+            return _list.Contains(item);
+        }
+
+        public void CopyTo(string[] array, int arrayIndex)
+        {
+            _list.CopyTo(array, arrayIndex);
+        }
+
+        public IEnumerator<string> GetEnumerator()
+        {
+            return _list.GetEnumerator();
+        }
+
+        public bool Remove(string item)
+        {
+            return _list.Remove(item);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return _list.GetEnumerator();
+        }
+
+        private bool IsValid(string license)
+        {
+            var regex = new Regex(@"^\w{3}-\d{4}$");
+
+            return regex.IsMatch(license);
+        }
+    }
+
+    public static class IEnumerableExtensions
+    {
+        public static void Print<T>(this IEnumerable<T> enumerable)
+        {
+            foreach (var item in enumerable)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
---
+++ alura/certificacao_csharp/Section6/Section6.HandleQueue/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Section6.HandleQueue
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkMagenta;
+
+            var flavio = "Flavio";
+            var luis = "Luis";
+            var fatima = "Fatima";
+            var fernando = "Fernando";
+
+            var queue = new CustomerQueue();
+
+            queue
+            .Enqueue(flavio)
+            .Enqueue(luis)
+            .Enqueue(fatima)
+            .Enqueue(fernando);
+
+            Console.WriteLine(queue.Dequeue().GetOrElse("No customers"));
+            Console.WriteLine(queue.Dequeue().GetOrElse("No customers"));
+            Console.WriteLine(queue.Dequeue().GetOrElse("No customers"));
+            Console.WriteLine(queue.Dequeue().GetOrElse("No customers"));
+            Console.WriteLine(queue.Dequeue().GetOrElse("No customers"));
+            Console.WriteLine(queue.Dequeue().GetOrElse("No customers"));
+        }
+    }
+
+    public class CustomerQueue
+    {
+        private Queue<string> _customers = new Queue<string>();
+
+        public CustomerQueue Enqueue(string customer)
+        {
+            _customers.Enqueue(customer);
+
+            return this;
+        }
+
+        public Maybe<string> Dequeue()
+        {
+            _customers.TryDequeue(out var customer);
+
+            return Maybe<string>.Of(customer);
+        }
+
+        public Maybe<string> Peek()
+        {
+            _customers.TryPeek(out var customer);
+
+            return Maybe<string>.Of(customer);
+        }
+    }
+
+    public class Maybe<T> where T : class
+    {
+        private T _value;
+
+        private Maybe(T value)
+        {
+            _value = value;
+        }
+
+        public static Maybe<T> Of(T value)
+        {
+            return new Maybe<T>(value);
+        }
+
+        public T GetOrElse(T value)
+        {
+            return _value ?? value;
+        }
+    }
+}
