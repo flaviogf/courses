8c714d693742ae4afbbe814ae6e6f5f1cfa9672c
flaviogf <flavio.fernandes6@gmail.com>
2019-11-16 15:01:26 -0300

    .

---
+++ alura/certificacao_csharp/Section8/Section8.ReadAndWriteStringsIntoFile/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Section8.ReadAndWriteStringsIntoFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
