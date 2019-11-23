ab5824c1249f3b6e5598053165d1b76b6d392547
flaviogf <flavio.fernandes6@gmail.com>
2019-11-23 17:04:59 -0300

    .

---
+++ alura/certificacao_csharp/Section9/Section9.UsingStopWatch/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Diagnostics;
+
+namespace Section9.UsingStopWatch
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var watch = new Stopwatch();
+
+            watch.Start();
+
+            var connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\flavi\dev\courses\alura\certificacao_csharp\Section9\Section9.UsingStopWatch\database.mdf;Integrated Security=True;Connect Timeout=30";
+
+            using var connection = new SqlConnection(connectionString);
+
+            connection.Open();
+
+            var cmdText = "INSERT INTO Movies (Name) VALUES(@name);";
+
+            using var command = new SqlCommand(cmdText, connection);
+
+            command.Parameters.AddWithValue("name", "Lord of the rings");
+
+            command.ExecuteNonQuery();
+
+            watch.Stop();
+
+            Console.WriteLine($"Time: {watch.ElapsedMilliseconds}ms");
+        }
+    }
+}
