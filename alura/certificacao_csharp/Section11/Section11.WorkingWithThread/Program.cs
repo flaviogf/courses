6e9154329dbb14c43633dbfa0a54604c7db1558e
flaviogf <flavio.fernandes6@gmail.com>
2019-11-29 17:33:35 -0300

    .

--- alura/certificacao_csharp/Section11/Section11.WorkingWithThread/Program.cs
+++ alura/certificacao_csharp/Section11/Section11.WorkingWithThread/Program.cs
@@ -7,6 +7,53 @@ namespace Section11.WorkingWithThread
     {
         public static void Main(string[] args)
         {
+            Example01();
+
+            Example02();
+
+            Console.WriteLine("Application has been finished");
+        }
+
+        private static void Example02()
+        {
+            var stopped = false;
+
+            var spinner = new Thread(() =>
+            {
+                var sequence = new string[] { "+", "x", "+", "x" };
+
+                while (!stopped)
+                {
+                    foreach (var character in sequence)
+                    {
+                        var message = $"It's working {character}";
+                        Console.Write(new string('\r', message.Length));
+                        Console.Write(message);
+                        Thread.Sleep(100);
+                    }
+                }
+
+                Console.WriteLine();
+            });
+
+            spinner.Start();
+
+            var thread1 = new Thread(Beep);
+            thread1.Start("Thread 1");
+
+            var thread2 = new Thread(Beep);
+            thread2.Start("Thread 2");
+
+            thread1.Join();
+            thread2.Join();
+
+            stopped = true;
+
+            spinner.Join();
+        }
+
+        private static void Example01()
+        {
             var thread1 = new Thread(DoWork);
             thread1.Start("Thread 1");
 
@@ -15,15 +62,20 @@ namespace Section11.WorkingWithThread
 
             thread1.Join();
             thread2.Join();
-
-            Console.WriteLine("Application have been finished");
         }
 
         public static void DoWork(object name)
         {
             Console.WriteLine($"Work {name} have been started");
-            Thread.Sleep(500);
+            Thread.Sleep(2000);
             Console.WriteLine($"Work {name} have been finished");
+        }
+
+        public static void Beep(object name)
+        {
+            Console.Beep();
+            Thread.Sleep(2000);
+            Console.Beep();
         }
     }
 }
