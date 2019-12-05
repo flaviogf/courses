1521b32c62f6e7e610bb9a2f9859a1b6fe3b4cfe
flaviogf <flavio.fernandes6@gmail.com>
2019-12-05 12:54:34 -0300

    .

---
+++ alura/certificacao_csharp/Section12/Section12.InternalFunctionsForValidateTypesAndContentsConsole/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Globalization;
+
+namespace Section12.InternalFunctionsForValidateTypesAndContentsConsole
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Example01();
+
+            Example02();
+        }
+
+        private static void Example02()
+        {
+            DateTime.TryParse("2019-12-06", CultureInfo.CurrentCulture, DateTimeStyles.AdjustToUniversal, out var date);
+
+            Console.WriteLine(date);
+        }
+
+        private static void Example01()
+        {
+            var number = default(int);
+
+            void Read()
+            {
+                Console.WriteLine("Enter a valid number");
+
+                if (int.TryParse(Console.ReadLine(), out number))
+                {
+                    return;
+                }
+
+                Read();
+            }
+
+            Read();
+
+            Console.WriteLine($"Readed {number}");
+        }
+    }
+}
---
+++ alura/certificacao_csharp/Section12/Section12.SymmetricAndAsymmetricEncryption/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace Section12.SymmetricAndAsymmetricEncryption
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var text = "Nothing is true, everything is permitted";
+
+            var encrypted = new byte[0];
+
+            var key = new byte[0];
+
+            using var aes = Aes.Create();
+
+            key = aes.Key;
+
+            using var encryptor = aes.CreateEncryptor();
+
+            using var memoryStream = new MemoryStream();
+
+            using var cryptoStream = new CryptoStream(memoryStream, encryptor, CryptoStreamMode.Write);
+
+            using var streamWriter = new StreamWriter(cryptoStream);
+
+            streamWriter.Write(text);
+
+            streamWriter.Flush();
+
+            encrypted = memoryStream.ToArray();
+
+            Console.WriteLine(new string('*', 100));
+
+            Console.WriteLine("Text");
+
+            Console.WriteLine(text);
+
+            Console.WriteLine(new string('*', 100));
+
+            Console.WriteLine("Key");
+
+            foreach (var b in key)
+            {
+                Console.Write("{0:X} ", b);
+            }
+
+            Console.WriteLine();
+
+            Console.WriteLine(new string('*', 100));
+
+            Console.WriteLine("Encrypted");
+
+            foreach (var b in encrypted)
+            {
+                Console.Write("{0:X}", b);
+            }
+
+            Console.WriteLine();
+
+            Console.WriteLine(new string('*', 100));
+        }
+    }
+}
