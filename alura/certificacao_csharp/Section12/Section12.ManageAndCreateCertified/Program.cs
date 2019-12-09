ed226b1d84c91d6e2381f8ee233ed8523acbcf1f
flaviogf <flavio.fernandes6@gmail.com>
2019-12-09 13:24:30 -0300

    .

---
+++ alura/certificacao_csharp/Section12/Section12.ManageAndCreateCertified/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Section12.ManageAndCreateCertified
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            using var store = new X509Store(StoreLocation.CurrentUser);
+
+            store.Open(OpenFlags.ReadOnly);
+
+            var certificates = store.Certificates;
+
+            var current = certificates.Find(X509FindType.FindBySubjectName, "Flavio", false)[0];
+
+            Console.WriteLine(current);
+        }
+    }
+}
