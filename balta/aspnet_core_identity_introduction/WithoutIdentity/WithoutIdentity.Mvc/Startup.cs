045f4257aaa473b9f7ab591c06269d16e5573c4a
flaviogf <flavio.fernandes6@gmail.com>
2020-01-01 10:04:56 -0300

    .

---
+++ balta/aspnet_core_identity_introduction/WithoutIdentity/WithoutIdentity.Mvc/Startup.cs
@@ -0,0 +1,49 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using WithoutIdentity.Mvc.Data;
+using WithoutIdentity.Mvc.Models;
+
+namespace WithoutIdentity.Mvc
+{
+    public class Startup
+    {
+        private readonly IConfiguration _configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var connectionString = _configuration.GetConnectionString("Identity");
+
+            services.AddDbContext<ApplicationContext>(options => options.UseSqlServer(connectionString));
+
+            services
+                .AddIdentity<ApplicationUser, IdentityRole<Guid>>()
+                .AddEntityFrameworkStores<ApplicationContext>()
+                .AddDefaultTokenProviders();
+
+            services.AddMvc(options => options.EnableEndpointRouting = false);
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseAuthentication();
+
+            app.UseMvc();
+        }
+    }
+}
