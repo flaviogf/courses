7bd19947123b55cfab5171539c6ec0816f6b98c9
flaviogf <flavio.fernandes6@gmail.com>
2020-01-02 18:01:53 -0300

    .

---
+++ balta/aspnet_core_identity_introduction/WithoutIdentity/WithoutIdentity.Mvc/Controllers/ChangePasswordController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using WithoutIdentity.Mvc.Models;
+using WithoutIdentity.Mvc.ViewModels.ChangePassword;
+
+namespace WithoutIdentity.Mvc.Controllers
+{
+    public class ChangePasswordController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public ChangePasswordController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpGet]
+        [Authorize]
+        public async Task<IActionResult> Edit()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Authorize]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(ChangePasswordEditViewModel viewModel)
+        {
+            var user = await _userManager.GetUserAsync(User);
+
+            return View(viewModel);
+        }
+    }
+}
---
+++ balta/aspnet_core_identity_introduction/WithoutIdentity/WithoutIdentity.Mvc/ViewModels/ChangePassword/ChangePasswordEditViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WithoutIdentity.Mvc.ViewModels.ChangePassword
+{
+    public class ChangePasswordEditViewModel
+    {
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Old password")]
+        [StringLength(100, ErrorMessage = "The field {0} must has min {2} and max {1} characters.", MinimumLength = 8)]
+        public string OldPassword { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "New password")]
+        [Compare(nameof(OldPassword))]
+        public string NewPassword { get; set; }
+    }
+}
