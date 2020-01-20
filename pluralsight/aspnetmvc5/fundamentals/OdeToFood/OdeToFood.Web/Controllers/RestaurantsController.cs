c460330a6b185016d47be28cfb0248651618a1a8
flaviogf <flavio.fernandes6@gmail.com>
2020-01-20 12:12:01 -0300

    pluralsight: asp.net mvc 5 fundamentals

---
+++ pluralsight/aspnetmvc5/fundamentals/OdeToFood/OdeToFood.Web/Controllers/RestaurantsController.cs
@@ -0,0 +1,22 @@
+﻿using OdeToFood.Data.Services;
+using System.Web.Mvc;
+
+namespace OdeToFood.Web.Controllers
+{
+    public class RestaurantsController : Controller
+    {
+        private readonly IRestaurantData _restaurantData;
+
+        public RestaurantsController(IRestaurantData restaurantData)
+        {
+            _restaurantData = restaurantData;
+        }
+
+        public ActionResult Index()
+        {
+            var restaurants = _restaurantData.GetAll();
+
+            return View(restaurants);
+        }
+    }
+}
