33603970ffc4fbc71c69123b5ec765bfe539cd7b
flaviogf <flavio.fernandes6@gmail.com>
2020-01-23 14:33:13 -0300

    pluralsight: building an api with asp.net web api

---
+++ pluralsight/buildinganapiwithaspnetwebapi/TheCodeCamp/TheCodeCamp.WebApi/Controllers/CampsController.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading.Tasks;
+using System.Web.Http;
+using TheCodeCamp.WebApi.Data;
+
+namespace TheCodeCamp.WebApi.Controllers
+{
+    public class CampsController : ApiController
+    {
+        private readonly ICampRepository _repository;
+
+        public CampsController(ICampRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<IHttpActionResult> Get()
+        {
+            var camps = await _repository.GetAllCampsAsync();
+
+            return Ok(camps);
+        }
+    }
+}
---
+++ pluralsight/buildinganapiwithaspnetwebapi/TheCodeCamp/TheCodeCamp.WebApi/Data/ICampRepository.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using TheCodeCamp.WebApi.Models;
+
+namespace TheCodeCamp.WebApi.Data
+{
+    public interface ICampRepository
+    {
+        Task<IEnumerable<Camp>> GetAllCampsAsync();
+    }
+}
