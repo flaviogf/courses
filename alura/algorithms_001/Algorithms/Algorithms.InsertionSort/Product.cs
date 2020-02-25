59582d7dca553d453004342bc5e84c236e4ddd11
flaviogf <flavio.fernandes6@gmail.com>
2020-02-25 09:42:56 -0300

    alura: algorithms

---
+++ alura/algorithms_001/Algorithms/Algorithms.InsertionSort/Product.cs
@@ -0,0 +1,20 @@
+﻿namespace Algorithms.InsertionSort
+{
+    public class Product
+    {
+        public Product(string name, decimal price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public string Name { get; }
+
+        public decimal Price { get; }
+
+        public override string ToString()
+        {
+            return $"Product[Name={Name}, Price={Price:C}]";
+        }
+    }
+}
---
+++ alura/algorithms_001/Algorithms/Algorithms.InsertionSort/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Algorithms.InsertionSort
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var products = new Product[]
+            {
+                new Product("Lamborghini", 1_000_000),
+                new Product("Jipe", 46_000),
+                new Product("Brasilia", 16_000),
+                new Product("Smart", 46_000),
+                new Product("Fusca", 17_000)
+            };
+
+            InsertionSort(products, products.Length);
+
+            foreach (var product in products)
+            {
+                Console.WriteLine(product);
+            }
+        }
+
+        public static void InsertionSort(Product[] products, int quantity)
+        {
+            for (var current = 1; current < quantity; current++)
+            {
+                var analysis = current;
+
+                while (analysis > 0 && products[analysis].Price < products[analysis - 1].Price)
+                {
+                    var productBeforeTheProductInAnalysis = products[analysis - 1];
+                    var productInAnalysis = products[analysis];
+                    products[analysis - 1] = productInAnalysis;
+                    products[analysis] = productBeforeTheProductInAnalysis;
+                    analysis--;
+                }
+            }
+        }
+    }
+}
