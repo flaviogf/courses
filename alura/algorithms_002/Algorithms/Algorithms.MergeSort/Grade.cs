e75533974842c57d9ab48979b013d253ace05a85
flaviogf <flavio.fernandes6@gmail.com>
2020-02-26 13:09:08 -0300

    alura: algorithms

---
+++ alura/algorithms_002/Algorithms/Algorithms.MergeSort/Grade.cs
@@ -0,0 +1,20 @@
+﻿namespace Algorithms.MergeSort
+{
+    public class Grade
+    {
+        public Grade(string student, double value)
+        {
+            Student = student;
+            Value = value;
+        }
+
+        public string Student { get; set; }
+
+        public double Value { get; set; }
+
+        public override string ToString()
+        {
+            return $"Grade[Student={Student}, Value={Value}]";
+        }
+    }
+}
---
+++ alura/algorithms_002/Algorithms/Algorithms.MergeSort/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace Algorithms.MergeSort
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var classroom1 = new Grade[]
+            {
+                new Grade("André", 4.0),
+                new Grade("Mariana", 5.0),
+                new Grade("Carlos", 8.5),
+                new Grade("Paulo", 9.0),
+            };
+
+            var classroom2 = new Grade[]
+            {
+                new Grade("Jonas", 3.0),
+                new Grade("Juliana", 6.7),
+                new Grade("Gui", 7.0),
+                new Grade("Lucia", 9.3),
+                new Grade("Ana", 10.0),
+            };
+
+            var grades = MergeSort(classroom1, classroom2);
+
+            foreach (var item in grades)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        public static Grade[] MergeSort(Grade[] grades1, Grade[] grades2)
+        {
+            var result = new Grade[grades1.Length + grades2.Length];
+
+            var current = 0;
+            var current1 = 0;
+            var current2 = 0;
+
+            while (current1 < grades1.Length && current2 < grades2.Length)
+            {
+                var grade1 = grades1[current1];
+                var grade2 = grades2[current2];
+
+                if (grade1.Value < grade2.Value)
+                {
+                    result[current] = grade1;
+                    current1++;
+                }
+                else
+                {
+                    result[current] = grade2;
+                    current2++;
+                }
+
+                current++;
+            }
+
+            while (current1 < grades1.Length)
+            {
+                result[current] = grades1[current1];
+                current1++;
+                current++;
+            }
+
+            while (current2 < grades2.Length)
+            {
+                result[current] = grades2[current2];
+                current2++;
+                current++;
+            }
+
+            return result;
+        }
+    }
+}
