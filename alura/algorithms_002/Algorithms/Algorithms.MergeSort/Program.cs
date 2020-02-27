a5935e924fe6724a4018122a89bee2215ec7b53b
flaviogf <flavio.fernandes6@gmail.com>
2020-02-26 21:37:06 -0300

    alura: algorithms

--- alura/algorithms_002/Algorithms/Algorithms.MergeSort/Program.cs
+++ alura/algorithms_002/Algorithms/Algorithms.MergeSort/Program.cs
@@ -6,6 +6,8 @@ namespace Algorithms.MergeSort
     {
         public static void Main(string[] args)
         {
+            Console.WriteLine(new string('*', 100));
+
             var classroom1 = new Grade[]
             {
                 new Grade("André", 4.0),
@@ -23,12 +25,79 @@ namespace Algorithms.MergeSort
                 new Grade("Ana", 10.0),
             };
 
-            var grades = MergeSort(classroom1, classroom2);
-
-            foreach (var item in grades)
+            foreach (var item in MergeSort(classroom1, classroom2))
             {
                 Console.WriteLine(item);
             }
+
+            var classroom = new Grade[]
+            {
+                new Grade("André", 4.0),
+                new Grade("Mariana", 5.0),
+                new Grade("Carlos", 8.5),
+                new Grade("Paulo", 9.0),
+                new Grade("Jonas", 3.0),
+                new Grade("Juliana", 6.7),
+                new Grade("Gui", 7.0),
+                new Grade("Lucia", 9.3),
+                new Grade("Ana", 10.0)
+            };
+
+            Console.WriteLine(new string('*', 100));
+
+            foreach (var item in MergeSort(classroom, 0, 4, classroom.Length))
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        public static Grade[] MergeSort(Grade[] classroom, int begin, int middle, int end)
+        {
+            var result = new Grade[end - begin];
+
+            var current = 0;
+            var current1 = begin;
+            var current2 = middle;
+
+            while (current1 < middle && current2 < end)
+            {
+                var grade1 = classroom[current1];
+                var grade2 = classroom[current2];
+
+                if (grade1.Value < grade2.Value)
+                {
+                    result[current] = grade1;
+                    current1++;
+                }
+                else
+                {
+                    result[current] = grade2;
+                    current2++;
+                }
+
+                current++;
+            }
+
+            while (current1 < middle)
+            {
+                result[current] = classroom[current1];
+                current1++;
+                current++;
+            }
+
+            while (current2 < end)
+            {
+                result[current] = classroom[current2];
+                current2++;
+                current++;
+            }
+
+            for (var i = 0; i < current; i++)
+            {
+                classroom[begin + i] = result[i];
+            }
+
+            return classroom;
         }
 
         public static Grade[] MergeSort(Grade[] grades1, Grade[] grades2)
