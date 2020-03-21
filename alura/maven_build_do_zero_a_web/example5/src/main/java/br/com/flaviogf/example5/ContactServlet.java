533158366d58af4911d330b911bf4284bc1fd911
flaviogf <flavio.fernandes6@gmail.com>
2020-03-21 20:07:29 -0300

    alura: maven

---
+++ alura/maven_build_do_zero_a_web/example5/src/main/java/br/com/flaviogf/example5/ContactServlet.java
@@ -0,0 +1,20 @@
+package br.com.flaviogf.example5;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(urlPatterns = {"/contact"})
+public class ContactServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter writer = resp.getWriter();
+        writer.println("<html><h2>Contact</h2></html>");
+        writer.close();
+    }
+}
