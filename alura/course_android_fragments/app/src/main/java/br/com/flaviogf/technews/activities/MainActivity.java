556e57afbaa746f1bdbac7c6615132a971e6eea4
flaviogf <flavio.fernandes6@gmail.com>
2020-06-18 16:43:55 -0300

    alura: android

--- alura/course_android_fragments/app/src/main/java/br/com/flaviogf/technews/activities/MainActivity.java
+++ alura/course_android_fragments/app/src/main/java/br/com/flaviogf/technews/activities/MainActivity.java
@@ -1,5 +1,6 @@
 package br.com.flaviogf.technews.activities;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
 
 import androidx.annotation.IdRes;
@@ -13,6 +14,7 @@ import androidx.fragment.app.FragmentTransaction;
 import br.com.flaviogf.technews.R;
 import br.com.flaviogf.technews.fragments.NewsFragment;
 import br.com.flaviogf.technews.fragments.NewsListFragment;
+import br.com.flaviogf.technews.models.News;
 
 public class MainActivity extends AppCompatActivity {
     @Override
@@ -21,37 +23,50 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         setTitle("Tech News");
 
-        if (savedInstanceState != null) {
+        if (savedInstanceState == null) {
+            setFragment(newsListFragment(), R.id.main_activity_news_list_container, "@news-list", false, null);
             return;
         }
 
+        FragmentManager fragmentManager = getSupportFragmentManager();
+
+        Fragment fragment = fragmentManager.findFragmentByTag("@news");
+
+        if (fragment == null) {
+            return;
+        }
+
+        removeFragment(fragment);
+
+        if (isPortraitOrientation()) {
+            Bundle bundle = fragment.getArguments();
+            setFragment(newsFragment(), R.id.main_activity_news_list_container, "@news", true, bundle);
+            return;
+        }
+
+        Bundle bundle = fragment.getArguments();
         setFragment(newsListFragment(), R.id.main_activity_news_list_container, "@news-list", false, null);
+        setFragment(newsFragment(), R.id.main_activity_news_container, "@news", false, bundle);
     }
 
     @Override
     public void onAttachFragment(@NonNull Fragment fragment) {
         super.onAttachFragment(fragment);
 
+        MainActivityListener mainActivityListener = getMainActivityListener();
+
         if (fragment instanceof NewsListFragment) {
             NewsListFragment newsListFragment = (NewsListFragment) fragment;
 
-            newsListFragment.setOnClickNewsListener((it) -> {
-                Bundle bundle = new Bundle();
-                bundle.putString("@news-id", it.getId());
-                setFragment(newsFragment(), R.id.main_activity_news_list_container, "@news", true, bundle);
-            });
+            newsListFragment.setOnClickNewsListener(mainActivityListener);
 
-            newsListFragment.setOnFloatingActionButtonClickListener(() -> {
-                setFragment(newsFragment(), R.id.main_activity_news_list_container, "@news", true, null);
-            });
+            newsListFragment.setOnFloatingActionButtonClickListener(mainActivityListener);
         }
 
         if (fragment instanceof NewsFragment) {
             NewsFragment newsFragment = (NewsFragment) fragment;
 
-            newsFragment.setOnOptionsItemDoneSelectedListener(() -> {
-                popFragment();
-            });
+            newsFragment.setOnOptionsItemDoneSelectedListener(mainActivityListener);
         }
     }
 
@@ -83,5 +98,84 @@ public class MainActivity extends AppCompatActivity {
         FragmentManager fragmentManager = getSupportFragmentManager();
 
         fragmentManager.popBackStack();
+    }
+
+    private void removeFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        fragmentTransaction.remove(fragment);
+
+        fragmentManager.popBackStack();
+
+        fragmentTransaction.commit();
+    }
+
+    private MainActivityListener getMainActivityListener() {
+        if (isPortraitOrientation()) {
+            return new MainActivityPortraitListener(this);
+        }
+
+        return new MainActivityLandListener(this);
+    }
+
+    private Boolean isPortraitOrientation() {
+        int orientation = getResources().getConfiguration().orientation;
+
+        return orientation == Configuration.ORIENTATION_PORTRAIT;
+    }
+
+    private interface MainActivityListener extends NewsListFragment.OnClickNewsListener, NewsListFragment.OnFloatingActionButtonClickListener, NewsFragment.OnOptionsItemDoneSelectedListener {
+    }
+
+    private static class MainActivityPortraitListener implements MainActivityListener {
+        private final MainActivity mainActivity;
+
+        public MainActivityPortraitListener(MainActivity mainActivity) {
+            this.mainActivity = mainActivity;
+        }
+
+        @Override
+        public void onSelect() {
+            mainActivity.popFragment();
+        }
+
+        @Override
+        public void onClick(News news) {
+            Bundle bundle = new Bundle();
+            bundle.putString("@news-id", news.getId());
+            mainActivity.setFragment(mainActivity.newsFragment(), R.id.main_activity_news_list_container, "@news", true, bundle);
+        }
+
+        @Override
+        public void onClick() {
+            mainActivity.setFragment(mainActivity.newsFragment(), R.id.main_activity_news_list_container, "@news", true, null);
+        }
+    }
+
+    private static class MainActivityLandListener implements MainActivityListener {
+        private final MainActivity mainActivity;
+
+        public MainActivityLandListener(MainActivity mainActivity) {
+            this.mainActivity = mainActivity;
+        }
+
+        @Override
+        public void onSelect() {
+            mainActivity.setFragment(mainActivity.newsListFragment(), R.id.main_activity_news_list_container, "@news-list", false, null);
+        }
+
+        @Override
+        public void onClick(News news) {
+            Bundle bundle = new Bundle();
+            bundle.putString("@news-id", news.getId());
+            mainActivity.setFragment(mainActivity.newsFragment(), R.id.main_activity_news_container, "@news", true, bundle);
+        }
+
+        @Override
+        public void onClick() {
+            mainActivity.setFragment(mainActivity.newsFragment(), R.id.main_activity_news_container, "@news", true, null);
+        }
     }
 }
