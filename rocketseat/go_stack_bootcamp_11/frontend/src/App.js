ebe9f7b3ea979e691ec3d99ad7add4a34d19c81b
flaviogf <flavio.fernandes6@gmail.com>
2020-08-21 17:39:49 -0300

    rocketseat: go stack bootcamp 11

--- rocketseat/go_stack_bootcamp_11/frontend/src/App.js
+++ rocketseat/go_stack_bootcamp_11/frontend/src/App.js
@@ -1,23 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Header from "./components/Header";
 
 function App() {
+  const [projects, setProjects] = useState(["Study English", "Study Go Stack"]);
+
+  function addProject() {
+    const project = `Project ${Date.now()}`;
+
+    setProjects([...projects, project]);
+  }
+
   return (
     <>
-      <Header title="Homepage">
-        <ul>
-          <li>Homepage</li>
-          <li>Projects</li>
-        </ul>
-      </Header>
       <Header title="Projects">
         <ul>
-          <li>Homepage</li>
-          <li>Projects</li>
-          <li>Log in</li>
+          {projects.map((it) => (
+            <li key={it}>{it}</li>
+          ))}
         </ul>
       </Header>
+      <button onClick={addProject}>Add</button>
     </>
   );
 }
