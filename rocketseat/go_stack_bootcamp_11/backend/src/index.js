72add84ffebb0b94ca855b696dce874b20f10c81
flaviogf <flavio.fernandes6@gmail.com>
2020-08-20 21:13:10 -0300

    rocketseat: go stack bootcamp 11

---
+++ rocketseat/go_stack_bootcamp_11/backend/src/index.js
@@ -0,0 +1,66 @@
+const express = require("express");
+const { v4: uuid } = require("uuid");
+
+const app = express();
+
+app.use(express.json());
+
+const projects = [];
+
+app.post("/projects", (req, res) => {
+  const { title, description } = req.body;
+
+  const id = uuid();
+
+  const project = {
+    id,
+    title,
+    description,
+  };
+
+  projects.push(project);
+
+  return res.json(project);
+});
+
+app.get("/projects", (req, res) => {
+  return res.json(projects);
+});
+
+app.put("/projects/:id", (req, res) => {
+  const { id } = req.params;
+
+  const { title, description } = req.body;
+
+  const index = projects.findIndex((it) => it.id === id);
+
+  if (index < 0) {
+    return res.status(404).json({ error: "Project does not exist" });
+  }
+
+  const project = {
+    id,
+    title,
+    description,
+  };
+
+  projects[index] = project;
+
+  return res.json(project);
+});
+
+app.delete("/projects/:id", (req, res) => {
+  const { id } = req.params;
+
+  const index = projects.findIndex((it) => it.id === id);
+
+  if (index < 0) {
+    return res.status(404).json({ error: "Project does not exist" });
+  }
+
+  const [project] = projects.splice(index, 1);
+
+  return res.json(project);
+});
+
+app.listen(3333, () => console.log("🚀 It is running"));
