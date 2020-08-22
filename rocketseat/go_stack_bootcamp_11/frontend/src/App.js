198778b73f1dd670cbc7869ea61249cd0cf3046a
flaviogf <flavio.fernandes6@gmail.com>
2020-08-22 08:35:28 -0300

    rocketseat: go stack bootcamp 11

--- rocketseat/go_stack_bootcamp_11/frontend/src/App.js
+++ rocketseat/go_stack_bootcamp_11/frontend/src/App.js
@@ -12,7 +12,18 @@ function App() {
     api.get("/projects").then((response) => setProjects(response.data));
   }, []);
 
-  function addProject() {}
+  function addProject() {
+    const now = Date.now();
+
+    const project = {
+      title: `Something to do at ${now}`,
+      description: `I believe I will have something to do at ${now} at least I expect that will be true`,
+    };
+
+    api
+      .post("/projects", project)
+      .then((response) => setProjects((prev) => [...prev, response.data]));
+  }
 
   return (
     <>
