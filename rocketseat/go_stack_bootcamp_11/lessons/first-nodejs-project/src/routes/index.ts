c4bf5009bdeebeb52ce2abbf88fbffd1f9a0f7af
flaviogf <flavio.fernandes6@gmail.com>
2020-08-25 16:04:03 -0300

    rocketseat: go stack bootcamp 11

---
+++ rocketseat/go_stack_bootcamp_11/lessons/first-nodejs-project/src/routes/index.ts
@@ -0,0 +1,7 @@
+import { Router } from 'express'
+
+const routes = Router()
+
+routes.get('/', (req, res) => res.json({ ok: true }))
+
+export default routes
--- rocketseat/go_stack_bootcamp_11/lessons/first-nodejs-project/src/server.ts
+++ rocketseat/go_stack_bootcamp_11/lessons/first-nodejs-project/src/server.ts
@@ -1,7 +1,8 @@
 import express from 'express'
+import routes from './routes'
 
 const app = express()
 
-app.get('/', (req, res) => res.json({ ok: true }))
+app.use(routes)
 
 app.listen(3333, () => console.log('🚀 It is running on port 3333'))
