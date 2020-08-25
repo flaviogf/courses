be746fb1bc24974ca32344831fb47aa450dbc827
flaviogf <flavio.fernandes6@gmail.com>
2020-08-25 08:40:10 -0300

    rocketseat: go stack bootcamp 11

--- rocketseat/go_stack_bootcamp_11/lessons/typescript/src/routes.ts
+++ rocketseat/go_stack_bootcamp_11/lessons/typescript/src/routes.ts
@@ -2,7 +2,16 @@ import { Request, Response } from "express";
 import { createUser } from "./services/CreateUser";
 
 export function greeting(request: Request, response: Response) {
-  const user = createUser({ email: "frank@email.com", password: "test123" });
+  const user = createUser({
+    email: "frank@email.com",
+    password: "test123",
+    techs: [
+      "React.js",
+      "React Native",
+      { title: "Node.js", experience: 100 },
+      { title: "C#", experience: 100 },
+    ],
+  });
 
   return response.json({ data: user });
 }
--- rocketseat/go_stack_bootcamp_11/lessons/typescript/src/services/CreateUser.ts
+++ rocketseat/go_stack_bootcamp_11/lessons/typescript/src/services/CreateUser.ts
@@ -1,15 +1,22 @@
-interface CreateUserData {
-  name?: string;
-  email: string;
-  password: string;
-}
-
-export function createUser({ name, email, password }: CreateUserData) {
+export function createUser({ name, email, password, techs }: CreateUserData) {
   const user = {
     name,
     email,
     password,
+    techs,
   };
 
   return user;
+}
+
+interface CreateUserData {
+  name?: string;
+  email: string;
+  password: string;
+  techs: Array<string | TechData>;
+}
+
+interface TechData {
+  title: string;
+  experience: number;
 }
