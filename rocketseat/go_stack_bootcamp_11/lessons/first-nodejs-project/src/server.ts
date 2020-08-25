00330e9d8a51c0deca46fd44ab7c8ad51ba84a80
flaviogf <flavio.fernandes6@gmail.com>
2020-08-25 09:06:22 -0300

    rocketseat: go stack bootcamp 11

---
+++ rocketseat/go_stack_bootcamp_11/lessons/first-nodejs-project/src/server.ts
@@ -0,0 +1,7 @@
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => res.json({ ok: true }));
+
+app.listen(3333, () => console.log("🚀 It is running on port 3333"));
