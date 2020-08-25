dff300c7edac07061f7d44a12aaa2f0877b21268
flaviogf <flavio.fernandes6@gmail.com>
2020-08-25 16:48:34 -0300

    rocketseat: go stack bootcamp 11

--- rocketseat/go_stack_bootcamp_11/lessons/first-nodejs-project/src/routes/index.ts
+++ rocketseat/go_stack_bootcamp_11/lessons/first-nodejs-project/src/routes/index.ts
@@ -2,6 +2,19 @@ import { Router } from 'express'
 
 const routes = Router()
 
-routes.get('/', (req, res) => res.json({ ok: true }))
+routes.post('/', (req, res) => {
+  const { email, password } = req.body
+
+  const user = {
+    email,
+    password,
+  }
+
+  return res.json(user)
+})
+
+routes.get('/', (req, res) => {
+  return res.json({ ok: true })
+})
 
 export default routes
