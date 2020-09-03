586433832240aedb0488cabcfd6ae230cd89a478
flaviogf <flavio.fernandes6@gmail.com>
2020-09-03 14:33:13 -0300

    rocketseat: go stack bootcamp 11

--- rocketseat/go_stack_bootcamp_11/lessons/first-reactjs-project/src/pages/Dashboard/index.tsx
+++ rocketseat/go_stack_bootcamp_11/lessons/first-reactjs-project/src/pages/Dashboard/index.tsx
@@ -1,9 +1,51 @@
 import React from 'react'
-import { Title } from './style'
+import { FiChevronRight } from 'react-icons/fi'
+import { Title, Form, Repositories } from './style'
+import logo from '../../assets/logo.svg'
 
 const Dashboard: React.FC = () => (
   <>
+    <img src={logo} alt="Github Explorer" />
     <Title>Browse repositories on GitHub</Title>
+    <Form>
+      <input placeholder="Enter the name of a repository" />
+      <button type="submit">Search</button>
+    </Form>
+    <Repositories>
+      <a href="#">
+        <img
+          src="https://avatars0.githubusercontent.com/u/17479978?s=460&u=c6050ae9efb110dc7efc1d280a7442cea76a1997&v=4"
+          alt="flaviogf"
+        />
+        <div>
+          <strong>flaviogf/courses</strong>
+          <p>This repository is destined to save all my courses and studies.</p>
+        </div>
+        <FiChevronRight size={20} />
+      </a>
+      <a href="#">
+        <img
+          src="https://avatars0.githubusercontent.com/u/17479978?s=460&u=c6050ae9efb110dc7efc1d280a7442cea76a1997&v=4"
+          alt="flaviogf"
+        />
+        <div>
+          <strong>flaviogf/courses</strong>
+          <p>This repository is destined to save all my courses and studies.</p>
+        </div>
+        <FiChevronRight size={20} />
+      </a>
+      <a href="#">
+        <img
+          src="https://avatars0.githubusercontent.com/u/17479978?s=460&u=c6050ae9efb110dc7efc1d280a7442cea76a1997&v=4"
+          alt="flaviogf"
+        />
+        <div>
+          <strong>flaviogf/courses</strong>
+          <p>This repository is destined to save all my courses and studies.</p>
+        </div>
+        <FiChevronRight size={20} />
+      </a>
+    </Repositories>
   </>
 )
 
--- rocketseat/go_stack_bootcamp_11/lessons/first-reactjs-project/src/pages/Dashboard/style.ts
+++ rocketseat/go_stack_bootcamp_11/lessons/first-reactjs-project/src/pages/Dashboard/style.ts
@@ -1,6 +1,92 @@
 import styled from 'styled-components'
+import { shade } from 'polished'
 
 export const Title = styled.h1`
   color: #3a3a3a;
   font-size: 48px;
+  line-height: 56px;
+  margin-top: 80px;
+  max-width: 450px;
+`
+
+export const Form = styled.div`
+  display: flex;
+  margin-top: 40px;
+  max-width: 700px;
+
+  input {
+    border: 0;
+    border-radius: 5px 0 0 5px;
+    flex: 1;
+    height: 70px;
+    padding: 0 24px;
+
+    &::placeholder {
+      color: #a8a8b3;
+    }
+  }
+
+  button {
+    background: #04d361;
+    border: 0;
+    border-radius: 0 5px 5px 0;
+    color: #fff;
+    font-weight: bold;
+    height: 70px;
+    transition: background 0.2s;
+    width: 210px;
+
+    &:hover {
+      background: ${shade(0.2, '#04d361')};
+    }
+  }
+`
+
+export const Repositories = styled.ul`
+  margin-top: 80px;
+  max-width: 700px;
+
+  a {
+    align-items: center;
+    background: #fff;
+    border-radius: 5px;
+    display: flex;
+    padding: 24px;
+    text-decoration: none;
+    transition: transform 0.2s;
+
+    & + a {
+      margin-top: 16px;
+    }
+
+    &:hover {
+      transform: translateX(10px);
+    }
+
+    img {
+      border-radius: 50%;
+      height: 64px;
+      width: 64px;
+    }
+
+    div {
+      margin-left: 16px;
+
+      strong {
+        color: #3d3d4d;
+        font-size: 20px;
+      }
+
+      p {
+        color: #a8a8b3;
+        font-size: 18px;
+        margin-top: 4px;
+      }
+    }
+
+    svg {
+      color: #cbcbd6;
+      margin-left: auto;
+    }
+  }
 `
