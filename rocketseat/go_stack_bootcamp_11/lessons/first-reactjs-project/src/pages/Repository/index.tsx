e1e9ec546d589bae47ba77ab62cd17986be63cb8
flaviogf <flavio.fernandes6@gmail.com>
2020-09-08 13:45:56 -0300

    rocketseat: go stack bootcamp 11

--- rocketseat/go_stack_bootcamp_11/lessons/first-reactjs-project/src/pages/Repository/index.tsx
+++ rocketseat/go_stack_bootcamp_11/lessons/first-reactjs-project/src/pages/Repository/index.tsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { FiChevronLeft, FiChevronRight } from 'react-icons/fi'
 import { Link, useParams } from 'react-router-dom'
 import logo from '../../assets/logo.svg'
+import api from '../../services/api'
 import { Header, RepositoryInfo, Issues } from './style'
 
 const Repository: React.FC = () => {
+  const [repository, setRepository] = useState<Repository | null>(null)
+  const [issues, setIssues] = useState<Issue[]>([])
+
   const params = useParams<RepositoryParams>()
+
+  useEffect(() => {
+    api
+      .get<Repository>(`/repos/${params.full_name}`)
+      .then((res) => res.data)
+      .then(setRepository)
+
+    api
+      .get(`/repos/${params.full_name}/issues`)
+      .then((res) => res.data)
+      .then(setIssues)
+  }, [params.full_name])
 
   return (
     <>
@@ -17,39 +33,43 @@ const Repository: React.FC = () => {
         </Link>
       </Header>
 
-      <RepositoryInfo>
-        <header>
-          <img src="https://avatars2.githubusercontent.com/u/17479978?v=4" />
-          <div>
-            <strong>asdf</strong>
-            <p>asdf</p>
-          </div>
-        </header>
+      {repository && (
+        <RepositoryInfo>
+          <header>
+            <img src={repository.owner.avatar_url} alt={repository.owner.login} />
+            <div>
+              <strong>{repository.full_name}</strong>
+              <p>{repository.description}</p>
+            </div>
+          </header>
 
-        <ul>
-          <li>
-            <strong>1808</strong>
-            <span>Stars</span>
-          </li>
-          <li>
-            <strong>48</strong>
-            <span>Forks</span>
-          </li>
-          <li>
-            <strong>67</strong>
-            <span>Issues</span>
-          </li>
-        </ul>
-      </RepositoryInfo>
+          <ul>
+            <li>
+              <strong>{repository.stargazers_count}</strong>
+              <span>Stars</span>
+            </li>
+            <li>
+              <strong>{repository.forks_count}</strong>
+              <span>Forks</span>
+            </li>
+            <li>
+              <strong>{repository.open_issues_count}</strong>
+              <span>Open Issues</span>
+            </li>
+          </ul>
+        </RepositoryInfo>
+      )}
 
       <Issues>
-        <a href="#">
-          <div>
-            <strong>asdf</strong>
-            <p>asdf</p>
-          </div>
-          <FiChevronRight size={20} />
-        </a>
+        {issues.map((it) => (
+          <a key={it.id} href={it.html_url} target="_blank">
+            <div>
+              <strong>{it.title}</strong>
+              <p>{it.user.login}</p>
+            </div>
+            <FiChevronRight size={20} />
+          </a>
+        ))}
       </Issues>
     </>
   )
@@ -57,6 +77,27 @@ const Repository: React.FC = () => {
 
 interface RepositoryParams {
   full_name: string
+}
+
+interface Repository {
+  description: string
+  forks_count: number
+  full_name: string
+  owner: {
+    avatar_url: string
+    login: string
+  }
+  open_issues_count: number
+  stargazers_count: number
+}
+
+interface Issue {
+  id: number
+  html_url: string
+  title: string
+  user: {
+    login: string
+  }
 }
 
 export default Repository
--- rocketseat/go_stack_bootcamp_11/lessons/first-reactjs-project/src/pages/Repository/style.ts
+++ rocketseat/go_stack_bootcamp_11/lessons/first-reactjs-project/src/pages/Repository/style.ts
@@ -77,4 +77,46 @@ export const RepositoryInfo = styled.div`
   }
 `
 
-export const Issues = styled.div``
+export const Issues = styled.div`
+  margin-top: 80px;
+  max-width: 700px;
+
+  a {
+    align-items: center;
+    background: #fff;
+    border-radius: 5px;
+    display: flex;
+    padding: 24px;
+    text-decoration: none;
+    transition: transform 0.2s;
+
+    & + a {
+      margin-top: 16px;
+    }
+
+    &:hover {
+      transform: translateX(10px);
+    }
+
+    div {
+      flex: 1;
+      margin: 0 16px;
+
+      strong {
+        color: #3d3d4d;
+        font-size: 20px;
+      }
+
+      p {
+        color: #a8a8b3;
+        font-size: 18px;
+        margin-top: 4px;
+      }
+    }
+
+    svg {
+      color: #cbcbd6;
+      margin-left: auto;
+    }
+  }
+`
