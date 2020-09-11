55e9f0de7016d58c852d44e7f1f647bc913f2436
flaviogf <flavio.fernandes6@gmail.com>
2020-09-11 16:05:14 -0300

    alura: javascript

---
+++ alura/javascript_polyglot_developer/public/cap-01/app.js
@@ -0,0 +1,4 @@
+import { bmi } from "./who.js";
+
+console.log(bmi({ weight: 62, height: 1.7 }));
+console.log(bmi({ height: 1.7, weight: 62 }));
---
+++ alura/javascript_polyglot_developer/public/cap-05/app/app.js
@@ -0,0 +1,1 @@
+import { Person } from './models/person.js';
