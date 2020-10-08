694e04ccf6a460bd9ae476f045109bb47a277d89
flaviogf <flavio.fernandes6@gmail.com>
2020-10-08 07:31:05 -0300

    alura: golang

--- alura/go_iniciando_com_a_linguagem/hello/hello.go
+++ alura/go_iniciando_com_a_linguagem/hello/hello.go
@@ -3,26 +3,31 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
 )
 
 func main() {
 	showPresentation("Frank", 1.1)
 
-	fmt.Println("1- Begin monitoring")
-	fmt.Println("2- Show logs")
-	fmt.Println("0- Exit")
+	for {
+		fmt.Println("1- Begin monitoring")
+		fmt.Println("2- Show logs")
+		fmt.Println("0- Exit")
 
-	option := readOption()
+		option := readOption()
 
-	switch option {
-	case 1:
-		beginMonitoring()
-	case 2:
-		fmt.Println("Displaying logs...")
-	case 0:
-		fmt.Println("Exiting...")
-	default:
-		fmt.Println("Command wasn't recognized")
+		switch option {
+		case 1:
+			beginMonitoring()
+		case 2:
+			fmt.Println("Displaying logs...")
+		case 0:
+			fmt.Println("Exiting...")
+			os.Exit(0)
+		default:
+			fmt.Println("Command wasn't recognized")
+			os.Exit(-1)
+		}
 	}
 }
 
@@ -39,7 +44,13 @@ func readOption() int {
 
 func beginMonitoring() {
 	fmt.Println("Monitoring...")
-	url := "https://www.alura.com.br"
+	url := "http://random-status-code.herokuapp.com/"
 	resp, _ := http.Get(url)
-	fmt.Println(resp)
+
+	if resp.StatusCode == 200 {
+		fmt.Println("website: ", url, "is running 🚀")
+		return
+	}
+
+	fmt.Println("website: ", url, "isn't available 🧨")
 }
