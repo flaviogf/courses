b2c47ec074d18087abb05faafc70c4d2a1cfb4e0
flaviogf <flavio.fernandes6@gmail.com>
2020-10-11 15:15:19 -0300

    geral: c++

---
+++ geral/codebeauty_c++/ASCII/ASCII.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    char c1, c2, c3, c4, c5;
+
+    cout << "Enter 5 letters\n";
+
+    cin >> c1 >> c2 >> c3 >> c4 >> c5;
+
+    cout << int(c1) << " " << int(c2) << " " << int(c3) << " " << int(c4) << " " << int(c5) << "\n";
+}
