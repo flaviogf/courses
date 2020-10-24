aaa0a453d14739eaf14acff3d39fdcf1f4807ded
flaviogf <flavio.fernandes6@gmail.com>
2020-10-24 20:43:45 -0300

    alura: shell scripting

--- alura/course_shell_scripting_002/server-monitoring.sh
+++ alura/course_shell_scripting_002/server-monitoring.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
-response=$(curl -w %{http_code} --output /dev/null --silent localhost)
+status=$(curl -w %{http_code} --output /dev/null --silent localhost)
 
-echo $response
+if [ $status -eq 200 ]
+then
+	echo "It's everything ok"
+else
+	echo "Something is wrong"
+	docker start httpd
+	echo "Apache was started"
+fi
+
