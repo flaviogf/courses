9dc363df5b53d803592acdeaadf4ca25c8874a32
flaviogf <flavio.fernandes6@gmail.com>
2020-10-27 10:34:52 -0300

    alura: shell scripting

--- alura/course_shell_scripting_002/check-used-memory.sh
+++ alura/course_shell_scripting_002/check-used-memory.sh
@@ -6,5 +6,9 @@ used=$(free | grep -i mem | awk '{ print $3 }')
 
 relation=$(echo "scale=2;$used / $total * 100" | bc -l | awk -F. '{ print $1 }')
 
-echo $relation
-
+if [ $relation -ge 5 ]
+then
+mail -s "Memory Oversize" flaviogf6@outlook.com<<d
+  Currently, the used memory is $(free -h | grep -i mem | awk '{ print $3 }') because of this is recommended do something about it.
+d
+fi
