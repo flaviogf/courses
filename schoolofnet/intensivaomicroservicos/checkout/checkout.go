dfffa72ed32a463d8cd6919182bd3b29510485b1
flaviogf <flavio.fernandes6@gmail.com>
2020-10-29 21:19:00 -0300

    full cycle: microservices

--- schoolofnet/intensivaomicroservicos/checkout/checkout.go
+++ schoolofnet/intensivaomicroservicos/checkout/checkout.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"html/template"
 	"io/ioutil"
 	"net/http"
@@ -13,12 +14,14 @@ import (
 func main() {
 	r := mux.NewRouter()
 
-	r.HandleFunc("/{id}", DisplayCheckout)
+	r.HandleFunc("/finish", FinishHandler)
+
+	r.HandleFunc("/{id}", DisplayCheckoutHandler)
 
 	http.ListenAndServe(":8082", r)
 }
 
-func DisplayCheckout(w http.ResponseWriter, r *http.Request) {
+func DisplayCheckoutHandler(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 
 	url := os.Getenv("PRODUCT_URL")
@@ -36,8 +39,29 @@ func DisplayCheckout(w http.ResponseWriter, r *http.Request) {
 	t.ExecuteTemplate(w, "Checkout", product)
 }
 
+func FinishHandler(w http.ResponseWriter, r *http.Request) {
+	var order Order
+	order.Name = r.FormValue("name")
+	order.Email = r.FormValue("email")
+	order.Phone = r.FormValue("phone")
+	order.ProductID = r.FormValue("product_id")
+
+	bytes, _ := json.Marshal(order)
+
+	fmt.Println(string(bytes))
+
+	w.Write([]byte("Processed"))
+}
+
 type Product struct {
 	UUID    string  `json:"uuid"`
 	Product string  `json:"product"`
 	Price   float64 `json:"price,string"`
+}
+
+type Order struct {
+	Name      string `json:"name"`
+	Email     string `json:"email"`
+	Phone     string `json:"phone"`
+	ProductID string `json:"product_id"`
 }
