b1f1098ce123836bae0e63eb8bf13228ed3f2941
flaviogf <flavio.fernandes6@gmail.com>
2020-11-07 09:56:37 -0300

    coders: golang

---
+++ coders/golang/tipos/pseudoheranca/pseudoheranca.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+type carro struct {
+	nome            string
+	velocidadeAtual int
+}
+
+type ferrari struct {
+	carro
+	turboLigado bool
+}
+
+func main() {
+	f := ferrari{}
+	f.nome = "F40"
+	f.velocidadeAtual = 0
+	f.turboLigado = true
+
+	fmt.Printf("A ferrari %s esta com o turbo ligado? %t\n", f.nome, f.turboLigado)
+	fmt.Println(f)
+}
---
+++ coders/golang/tipos/struct/struct.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+type produto struct {
+	nome     string
+	preco    float64
+	desconto float64
+}
+
+func (p produto) precoComDesconto() float64 {
+	return p.preco * (1 - p.desconto)
+}
+
+func main() {
+	var produto1 produto
+	produto1 = produto{
+		nome:     "Lapis",
+		preco:    1.79,
+		desconto: 0.05,
+	}
+
+	fmt.Println(produto1, produto1.precoComDesconto())
+
+	produto2 := produto{
+		nome:     "Notebook",
+		preco:    1989.0,
+		desconto: 0.10,
+	}
+
+	fmt.Println(produto2.precoComDesconto())
+}
