c961231f4b909ef85a58863b3a3abfc5ff0395b3
flaviogf <flavio.fernandes6@gmail.com>
2020-11-10 19:54:11 -0300

    coders: golang

---
+++ coders/golang/http/static/static.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	fs := http.FileServer(http.Dir("public"))
+
+	http.Handle("/", fs)
+
+	log.Println("Executando")
+
+	log.Fatal(http.ListenAndServe(":3000", nil))
+}
