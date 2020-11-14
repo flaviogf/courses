4a69122863d15a9db85f675f3af82a7804a22321
flaviogf <flavio.fernandes6@gmail.com>
2020-11-14 16:33:34 -0300

    alura: golang

--- alura/go_oo/003/bank/main.go
+++ alura/go_oo/003/bank/main.go
@@ -13,11 +13,25 @@ type Account struct {
 	Balance int
 }
 
+func (self *Account) Deposit(value int) error {
+	if value <= 0 {
+		return errors.New("value to deposit must be greater than 0")
+	}
+
+	self.Balance += value
+
+	return nil
+}
+
 func (self *Account) Withdraw(value int) error {
+	if value <= 0 {
+		return errors.New("value to withdraw must be greater than 0")
+	}
+
 	hasNoBalance := self.Balance-value < 0
 
 	if hasNoBalance {
-		return errors.New("Value is not available")
+		return errors.New("value to withdraw is not available")
 	}
 
 	self.Balance -= value
@@ -33,12 +47,20 @@ func main() {
 	err := account.Withdraw(500)
 
 	if err != nil {
-		log.Fatalf("Something goes wrong! Detail: %s", err.Error())
+		log.Println(err.Error())
 	}
 
 	err = account.Withdraw(500)
 
 	if err != nil {
-		log.Fatalf("Something goes wrong! Detail: %s", err.Error())
+		log.Println(err.Error())
 	}
+
+	err = account.Deposit(100)
+
+	if err != nil {
+		log.Println(err.Error())
+	}
+
+	fmt.Println(account)
 }
