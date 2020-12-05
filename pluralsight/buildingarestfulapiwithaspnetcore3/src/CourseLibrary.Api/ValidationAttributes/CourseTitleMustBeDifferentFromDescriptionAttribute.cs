8cd1ccbb42694c773470cf4c8f0632a658e60973
flaviogf <flavio.fernandes6@gmail.com>
2020-12-05 19:41:33 -0300

    pluralsight: building a restful api with aspnet core 3

--- pluralsight/buildingarestfulapiwithaspnetcore3/src/CourseLibrary.Api/ValidationAttributes/CourseTitleMustBeDifferentFromDescriptionAttribute.cs
+++ pluralsight/buildingarestfulapiwithaspnetcore3/src/CourseLibrary.Api/ValidationAttributes/CourseTitleMustBeDifferentFromDescriptionAttribute.cs
@@ -6,13 +6,18 @@ namespace CourseLibrary.Api.ValidationAttributes
 {
     public class CourseTitleMustBeDifferentFromDescriptionAttribute : ValidationAttribute
     {
+        public CourseTitleMustBeDifferentFromDescriptionAttribute()
+        {
+            ErrorMessage = "The title and description must be different";
+        }
+
         protected override ValidationResult IsValid(object value, ValidationContext validationContext)
         {
             var course = (CourseForCreationDto)validationContext.ObjectInstance;
 
             if (course.Title.Equals(course.Description, StringComparison.InvariantCultureIgnoreCase))
             {
-                return new ValidationResult("The title and description must be different", new string[] { nameof(CourseTitleMustBeDifferentFromDescriptionAttribute) });
+                return new ValidationResult(ErrorMessage, new string[] { nameof(CourseTitleMustBeDifferentFromDescriptionAttribute) });
             }
 
             return ValidationResult.Success;
