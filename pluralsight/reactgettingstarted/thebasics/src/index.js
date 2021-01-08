71964c01202942d5e09bcffa5cb957057eefef94
flaviogf <flavio.fernandes6@gmail.com>
2021-01-08 16:41:34 -0300

    pluralsight: react getting started

--- pluralsight/reactgettingstarted/thebasics/src/index.js
+++ pluralsight/reactgettingstarted/thebasics/src/index.js
@@ -1,7 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
 
-ReactDOM.render(<Sample2 />, document.getElementById("root"));
+ReactDOM.render(<Sample4 />, document.getElementById("root"));
 
 function Sample1() {
   return <h1>Hello World!</h1>;
@@ -9,4 +9,16 @@ function Sample1() {
 
 function Sample2() {
   return React.createElement("h1", null, "Hello World!!!");
+}
+
+function Sample3() {
+  const [counter, setCounter] = useState(0);
+
+  return <button onClick={() => setCounter(counter + 1)}>{counter}</button>;
+}
+
+function Sample4() {
+  const [counter, setCounter] = useState(5);
+
+  return <button onClick={() => setCounter(counter * 2)}>{counter}</button>;
 }
