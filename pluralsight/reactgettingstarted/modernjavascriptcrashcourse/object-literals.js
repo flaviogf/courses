ff0c13f5eb0bf7dac1e8f9eb3119a60d2742807e
flaviogf <flavio.fernandes6@gmail.com>
2021-01-11 07:37:46 -0300

    react getting started

---
+++ pluralsight/reactgettingstarted/modernjavascriptcrashcourse/object-literals.js
@@ -0,0 +1,13 @@
+const mistery = "answer";
+const inverseOfPI = 1 / Math.PI;
+
+const obj = {
+  p1: 10,
+  p2: 20,
+  f1() {},
+  f2: () => {},
+  [mistery]: 42,
+  inverseOfPI,
+};
+
+console.log(obj);
