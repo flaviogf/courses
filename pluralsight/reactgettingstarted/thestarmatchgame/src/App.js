8aaf7acf3435c2a3a3a7ce6eb957de136b096628
flaviogf <flavio.fernandes6@gmail.com>
2021-01-14 09:51:11 -0300

    pluralsight: react getting started

--- pluralsight/reactgettingstarted/thestarmatchgame/src/App.js
+++ pluralsight/reactgettingstarted/thestarmatchgame/src/App.js
@@ -3,6 +3,22 @@ import "./App.css";
 
 export default function App() {
   const [stars, setStars] = useState(9);
+  const [availableNumbers, setAvailableNumbers] = useState(utils.range(1, 9));
+  const [candidateNumbers, setCandidateNumbers] = useState([]);
+
+  const candidatesAreWrong = utils.sum(candidateNumbers) > stars;
+
+  const numberStatus = (number) => {
+    if (!availableNumbers.includes(number)) {
+      return "used";
+    }
+
+    if (candidateNumbers.includes(number)) {
+      return candidatesAreWrong ? "wrong" : "candidate";
+    }
+
+    return "available";
+  };
 
   return (
     <div className="App__container">
@@ -16,7 +32,11 @@ export default function App() {
         </div>
         <div className="App__right">
           {utils.range(1, 9).map((number) => (
-            <PlayNumber key={number} number={number} />
+            <PlayNumber
+              key={number}
+              number={number}
+              status={numberStatus(number)}
+            />
           ))}
         </div>
       </div>
@@ -30,11 +50,23 @@ function StarDisplay({ stars }) {
     .map((starId) => <div key={starId} className="App__star"></div>);
 }
 
-function PlayNumber({ number }) {
-  return <button className="App__number">{number}</button>;
+function PlayNumber({ number, status }) {
+  return (
+    <button className="App__number" style={{ backgroundColor: colors[status] }}>
+      {number}
+    </button>
+  );
 }
 
 const utils = {
+  sum: (arr) => arr.reduce((acc, curr) => acc + curr, 0),
   range: (min, max) => Array.from({ length: max - min + 1 }, (_, i) => min + i),
   random: (min, max) => min + Math.floor(Math.random() * (max - min + 1)),
+};
+
+const colors = {
+  available: "lightgray",
+  used: "lightgreen",
+  wrong: "lightcoral",
+  candidate: "deepskyblue",
 };
