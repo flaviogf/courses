8c3b82515569669f1d3a6c2d3d965ec6e5db315f
flaviogf <flavio.fernandes6@gmail.com>
2021-03-14 11:37:57 -0300

    pluralsight: go getting started

---
+++ pluralsight/gogettingstarted/foruntillcondition/go.mod
@@ -0,0 +1,3 @@
+module github.com/flaviogf/foruntillcondition
+
+go 1.16
---
+++ pluralsight/gogettingstarted/foruntillcondition/main.go
@@ -0,0 +1,17 @@
+package main
+
+func main() {
+	var i int
+
+	for i < 5 {
+		println(i)
+
+		i++
+
+		if i == 3 {
+			continue
+		}
+
+		println("continuing...")
+	}
+}
