cd450ab3a835413834b5374dbe7b3f0d023cfa85
flaviogf <flavio.fernandes6@gmail.com>
2021-03-14 12:06:21 -0300

    pluralsight: go getting started

---
+++ pluralsight/gogettingstarted/creatinginfiniteloops/go.mod
@@ -0,0 +1,3 @@
+module github.com/flaviogf/creatinginfiniteloops
+
+go 1.16
---
+++ pluralsight/gogettingstarted/creatinginfiniteloops/main.go
@@ -0,0 +1,15 @@
+package main
+
+func main() {
+	i := 0
+
+	for {
+		if i == 5 {
+			break
+		}
+
+		println(i)
+
+		i++
+	}
+}
