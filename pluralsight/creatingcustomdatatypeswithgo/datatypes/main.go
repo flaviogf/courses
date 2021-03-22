4e1d026fe4cad43791ce9b7bcf9601b6b7f85fbd
flaviogf <flavio.fernandes6@gmail.com>
2021-03-22 17:32:43 -0300

    pluralsight: creating custom data types with go

--- pluralsight/creatingcustomdatatypeswithgo/datatypes/main.go
+++ pluralsight/creatingcustomdatatypeswithgo/datatypes/main.go
@@ -25,4 +25,18 @@ func main() {
 	fmt.Println(frank.Twitter())
 
 	fmt.Println(frank.Twitter().RedirectUrl())
+
+	usa := organization.NewSocialSecurityNumber("123")
+
+	eu := organization.NewEuropeanUnionNumber("123", "United State of America")
+
+	fmt.Printf("%s %s\n", usa.ID(), usa.Country())
+
+	fmt.Printf("%s %s\n", eu.ID(), eu.Country())
+
+	if usa == eu {
+		fmt.Println("We matched")
+	} else {
+		fmt.Println("We did not match")
+	}
 }
