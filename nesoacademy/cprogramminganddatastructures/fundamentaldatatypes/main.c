f31e17ecd6986088ef0191b5d2098132724fee52
flaviogf <flavio.fernandes6@gmail.com>
2021-04-18 12:09:43 -0300

    nesoacademy: c programming and data structures

---
+++ nesoacademy/cprogramminganddatastructures/fundamentaldatatypes/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+  printf("%lu\n", sizeof(int));
+  printf("1100 = %2.f\n", pow(2,3) * 1 + pow(2,2) * 1 + pow(2,1) * 0 + pow(2,0) * 0);
+  return 0;
+}
