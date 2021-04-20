206ccde12616b6e530a83242f96034b192db8930
flaviogf <flavio.fernandes6@gmail.com>
2021-04-20 13:00:12 -0300

    pluralsight: mocking with moq and xunit

--- pluralsight/mockingwithmoqandxunit/CreditCardApplications.Tests/CreditCardApplicationEvaluatorShould.cs
+++ pluralsight/mockingwithmoqandxunit/CreditCardApplications.Tests/CreditCardApplicationEvaluatorShould.cs
@@ -38,20 +38,36 @@ namespace CreditCardApplications.Tests
         {
             Mock<IFrequentFlyerNumberValidator> validator = new();
 
-            CreditCardApplicationEvaluator sut = new(validator.Object);
+            validator.Setup(it => it.IsValid(It.IsRegex(@"[a-z]"))).Returns(true);
 
-            validator.Setup(it => it.IsValid("x")).Returns(true);
+            CreditCardApplicationEvaluator sut = new(validator.Object);
 
             CreditCardApplication application = new()
             {
                 GrossAnnualIncome = 19_999,
                 Age = 49,
-                FrequentFlyerNumber = "x"
+                FrequentFlyerNumber = "y"
             };
 
             CreditCardApplicationDecision decision = sut.Evaluate(application);
 
             Assert.Equal(CreditCardApplicationDecision.AutoDeclined, decision);
+        }
+
+        [Fact]
+        public void ReferInvalidFrequentFlyerApplication()
+        {
+            Mock<IFrequentFlyerNumberValidator> validator = new(MockBehavior.Strict);
+
+            validator.Setup(it => it.IsValid(It.IsAny<string>())).Returns(false);
+
+            CreditCardApplicationEvaluator sut = new(validator.Object);
+
+            CreditCardApplication application = new();
+
+            CreditCardApplicationDecision decision = sut.Evaluate(application);
+
+            Assert.Equal(CreditCardApplicationDecision.ReferredToHuman, decision);
         }
     }
 }
