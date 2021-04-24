8fd00c70ffad84c1b3e35902d11136453686db3a
flaviogf <flavio.fernandes6@gmail.com>
2021-04-24 14:25:16 -0300

    pluralsight: ruby getting started

--- pluralsight/rubygettingstarted/word_count.rb
+++ pluralsight/rubygettingstarted/word_count.rb
@@ -3,7 +3,15 @@
 FILE_NAME = "romeo-juliet.txt"
 
 def words_from_file(file_name)
-  File.open(file_name).read.downcase.gsub(/[^a-z]/, "").split
+  File.read(file_name).downcase.gsub(/[^a-z]/, " ").split
 end
 
 words = words_from_file(FILE_NAME)
+
+words_count = {}
+
+words.each do |word|
+  words_count[word] = 0 unless words_count[word]
+  words_count[word] += 1
+end
+
