ef5294468471e1d214089aa79328b756d24b728e
flaviogf <flavio.fernandes6@gmail.com>
2021-06-16 20:20:45 -0300

    geral: livro conhecendo ruby

--- geral/livro_conhecendo_ruby/basico/regex.rb
+++ geral/livro_conhecendo_ruby/basico/regex.rb
@@ -21,3 +21,21 @@ puts "1 teste" !~ regex1
 puts "1 teste" !~ regex2
 
 puts "1 teste" !~ regex3
+
+puts regex1.match? "1 teste"
+
+puts "o rato roeu a roupa do rei de Roma".scan(/r[a-z]+/i)
+
+"Frank Castle" =~ /(\w+)( )(\w+)/
+
+puts $1 + $2 + $3
+
+puts "Frank Castle".sub(/(\w+)( )(\w+)/i, '\3 \1')
+
+matcher = /(?<objeto>\w{5})(.*)(?<cidade>\w{4})$/.match("o rato roeu a roupa do rei de Roma")
+
+puts matcher[:objeto]
+
+puts matcher[:cidade]
+
+puts matcher.named_captures
