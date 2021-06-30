f0d562d8af93b608904959cfe10bb3a6c2061bd4
flaviogf <flavio.fernandes6@gmail.com>
2021-06-30 19:19:04 -0300

    geral: livro rspec

--- geral/livro_rspec_crie_especificacoes_executaveis_em_ruby/pokemon_battle/db/schema.rb
+++ geral/livro_rspec_crie_especificacoes_executaveis_em_ruby/pokemon_battle/db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_29_225707) do
+ActiveRecord::Schema.define(version: 2021_06_30_221438) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.boolean "approved"
+    t.text "content"
   end
 
   create_table "pokemons", force: :cascade do |t|
--- geral/livro_rspec_crie_especificacoes_executaveis_em_ruby/pokemon_battle/spec/factories/articles.rb
+++ geral/livro_rspec_crie_especificacoes_executaveis_em_ruby/pokemon_battle/spec/factories/articles.rb
@@ -1,8 +1,14 @@
 # frozen_string_literal: true
 
 FactoryGirl.define do
+  sequence :title do |n|
+    "Some tips of RSpec #{n}"
+  end
+
   factory :article do
-    title 'Article 1'
+    title
+    content { "Content of the article #{title}. Approved: #{approved}" }
+    created_at { 2.days.ago }
 
     factory :approved_article do
       approved true
