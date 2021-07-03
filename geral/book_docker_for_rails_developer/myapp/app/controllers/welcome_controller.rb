76245195e91b6c2c607a782fed4f3bcfc0f7db87
flaviogf <flavio.fernandes6@gmail.com>
2021-07-03 07:50:15 -0300

    book: docker for rails developer

---
+++ geral/book_docker_for_rails_developer/myapp/app/controllers/welcome_controller.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+class WelcomeController < ApplicationController
+  def index
+    redis.incr :page_hits 
+
+    @page_hits = redis.get :page_hits
+  end
+
+  private
+
+  def redis
+    @redis ||= Redis.new(host: 'redis', port: 6379)
+  end
+end
