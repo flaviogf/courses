d85887dc1d3b7707abc72b7978a7278cf1f8654a
flaviogf <flavio.fernandes6@gmail.com>
2021-07-06 08:28:06 -0300

    geral: book practical vim

---
+++ geral/book_practical_vim/the_vim_way/2_foo_bar.js
@@ -0,0 +1,7 @@
+// $ - go to end of the line
+// A - go to end of the line and enter in the insert mode
+// i - enter in the insert mode
+
+var foo = 1;
+var bar = 'a';
+var foobar = foo + bar;
---
+++ geral/book_practical_vim/the_vim_way/3_concat.js
@@ -0,0 +1,5 @@
+// f{char} - find the next occurrence of the char
+// ;       - repeat the last search
+// .       - repeat the last command
+
+var foo = "method("+argument1+","+argument2+")";
