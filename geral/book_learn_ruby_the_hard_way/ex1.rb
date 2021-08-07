1d748ffa3a43c80462358b0f427a18568a67714c
flaviogf <flavio.fernandes6@gmail.com>
2021-08-07 10:00:54 -0300

    geral: learn ruby the hard way

---
+++ geral/book_learn_ruby_the_hard_way/ex1.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+puts 'Hello World'
+puts 'Hello World Again'
+puts 'I like typing this.'
+puts 'This is fun.'
+puts 'Yay! Printing'
+puts 'I\'d much rather you \'not\'.'
+puts 'I "said" do not touh this.'
