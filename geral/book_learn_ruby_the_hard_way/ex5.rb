7ca441bdbf66e1af980ed1e3d79467560a2ec174
flaviogf <flavio.fernandes6@gmail.com>
2021-08-07 10:27:59 -0300

    geral: learn ruby the hard way

---
+++ geral/book_learn_ruby_the_hard_way/ex5.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+name = 'Zed A. Shaw'
+age = 35
+height = 74
+weight = 180
+eyes = 'Blue'
+teeth = 'White'
+hair = 'Brown'
+
+puts "Let's talk about #{name}"
+puts "He's #{height} inches tall"
+puts "He's #{weight} punds heavy"
+puts "Actually that's not too heavy."
+puts "He's got #{eyes} eyes and #{hair} hair."
+puts "His teeth are usually #{teeth} depending on the coffee."
+
+# this line is tricky, try to get it exactly right
+
+puts "If I add #{age}, #{height} and #{weight} I get #{age + height + weight}"
