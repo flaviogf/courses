09212a8fb7fa2a9ef585be8fd5e508895dd2e993
flaviogf <flavio.fernandes6@gmail.com>
2021-08-08 08:58:07 -0300

    geral: learn ruby the hard way

---
+++ geral/book_learn_ruby_the_hard_way/ex10.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+tabby_cat = "\tI'm a tabbed in."
+persian_cat = "I'm split\non a line."
+backslash_cat = "I'm \\ a \\ cat."
+
+fat_cat = """
+I'll do a list:
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass
+"""
+
+puts tabby_cat
+puts persian_cat
+puts backslash_cat
+puts fat_cat
