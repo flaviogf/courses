254f3e692788ed53c5d7e9b9e4e0a79323d1b544
flaviogf <flavio.fernandes6@gmail.com>
2021-08-11 16:24:43 -0300

    geral: learn ruby the hard way

--- geral/book_learn_ruby_the_hard_way/ex35.rb
+++ geral/book_learn_ruby_the_hard_way/ex35.rb
@@ -46,3 +46,44 @@ def bear_room
     end
   end
 end
+
+def cthulhu_room
+  puts 'Here you see the great evil Cthulhu.'
+  puts 'He, it, whatever stares at you and you go insane.'
+  puts 'Do you flee for your life or eat your head?'
+
+  print '> '
+  choice = $stdin.gets.chomp
+
+  if choice.include?('flee')
+    start
+  elsif choice.include?('head')
+    dead('Well that was tasty!')
+  else
+    cthulhu_room
+  end
+end
+
+def dead(why)
+  puts why, 'Good job'
+  exit(0)
+end
+
+def start
+  puts 'You are in a dark room'
+  puts 'There is a door to your right and left.'
+  puts 'Which one do you take?'
+
+  print '> '
+  choice = $stdin.gets.chomp
+
+  if choice == 'left'
+    bear_room
+  elsif choice == 'right'
+    cthulhu_room
+  else
+    dead('You stumble around the room until you starve.')
+  end
+end
+
+start
