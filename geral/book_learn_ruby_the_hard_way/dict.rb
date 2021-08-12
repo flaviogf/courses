9f51cd008c3f89a921217a5b294cf9f333630ef5
flaviogf <flavio.fernandes6@gmail.com>
2021-08-12 13:34:32 -0300

    geral: learn ruby the hard way

--- geral/book_learn_ruby_the_hard_way/dict.rb
+++ geral/book_learn_ruby_the_hard_way/dict.rb
@@ -6,11 +6,16 @@ module Dict
   def new(num_buckets = 256)
     result = []
 
-    num_buckets.times do |i|
-      result.push(i)
+    num_buckets.times do
+      result.push([])
     end
 
     result
+  end
+
+  def get_bucket(dict, key)
+    bucket_id = hash_key(dict, key)
+    dict[bucket_id]
   end
 
   def hash_key(dict, key)
