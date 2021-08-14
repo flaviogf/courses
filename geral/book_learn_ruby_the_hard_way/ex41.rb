79e8544113fa89fd0f8bf3c6c745a9348d1f9296
flaviogf <flavio.fernandes6@gmail.com>
2021-08-13 21:25:01 -0300

    geral: learn ruby the hard way

--- geral/book_learn_ruby_the_hard_way/ex41.rb
+++ geral/book_learn_ruby_the_hard_way/ex41.rb
@@ -21,3 +21,61 @@ Net::HTTP.get_response(URI(WORD_URL)) do |resp|
     WORDS.concat(words.split)
   end
 end
+
+def craft_names(rand_words, snippet, pattern, caps: false)
+  names = snippet.scan(pattern).map do
+    word = rand_words.pop
+    caps ? word.capitalize : word
+  end
+
+  names * 2
+end
+
+def craft_params(rand_words, snippet, pattern)
+  names = (0...snippet.scan(pattern).length).map do
+    param_count = rand(1..3)
+    params = (0...param_count).map { |_x| rand_words.pop }
+    params.join(', ')
+  end
+
+  names * 2
+end
+
+def convert(snippet, phrase)
+  rand_words = WORDS.sort_by { rand }
+  class_names = craft_names(rand_words, snippet, /###/, caps: true)
+  other_names = craft_names(rand_words, snippet, /\*\*\*/)
+  param_names = craft_names(rand_words, snippet, /@@@/)
+
+  results = []
+
+  [snippet, phrase].each do |sentence|
+    result = sentence.gsub(/###/) { |_x| class_names.pop }
+
+    result.gsub!(/\*\*\*/) { |_x| other_names.pop }
+
+    result.gsub!(/@@@/) { |_x| param_names.pop }
+
+    results.push(result)
+  end
+
+  results
+end
+
+loop do
+  snippets = PHRASES.keys.sort_by { rand }
+
+  snippets.each do |snippet|
+    phrase = PHRASES[snippet]
+
+    question, answer = convert(snippet, phrase)
+
+    question, answer = answer, question if PHRASE_FIRST
+
+    print question, "\n\n>"
+
+    exit(0) unless $stdin.gets
+
+    puts "\nANSWER: %s\n\n" % answer
+  end
+end
