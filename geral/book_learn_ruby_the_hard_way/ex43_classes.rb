23ea44989f3b26eff2108e878bcd318fd01364ca
flaviogf <flavio.fernandes6@gmail.com>
2021-08-14 16:02:33 -0300

    geral: learn ruby the hard way

---
+++ geral/book_learn_ruby_the_hard_way/ex43_classes.rb
@@ -0,0 +1,54 @@
+# frozen_string_literal: true
+
+class Scene
+  def enter
+  end
+end
+
+class Engine
+  def initialize(scene_map)
+  end
+
+  def play
+  end
+end
+
+class Death < Scene
+  def enter
+  end
+end
+
+class CentralCorridor < Scene
+  def enter
+  end
+end
+
+class LaseWeaponArmory < Scene
+  def enter
+  end
+end
+
+class TheBridge < Scene
+  def enter
+  end
+end
+
+class EscapePod < Scene
+  def enter
+  end
+end
+
+class Map
+  def initialize(start_scene)
+  end
+
+  def next_scene(scene_name)
+  end
+
+  def opening_scene
+  end
+end
+
+a_map = Map.new('central_corridor')
+a_game = Engine.new(a_map)
+a_game.play()
