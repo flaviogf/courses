13177178835c106ccab86d65056874afd02a4bdf
flaviogf <flavio.fernandes6@gmail.com>
2021-08-27 08:45:12 -0300

    geral: book confident ruby

---
+++ geral/book_confident_ruby/ex3/lib/ex3.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+def seconds_in_day
+  24 * 60 * 60
+end
+
+puts seconds_in_day
+
+SECONDS_IN_DAY = 24 * 60 * 60
+
+puts SECONDS_IN_DAY
+
+def seconds_in_days(num_days)
+  num_days * 24 * 60 * 60
+end
+
+puts seconds_in_days(2)
