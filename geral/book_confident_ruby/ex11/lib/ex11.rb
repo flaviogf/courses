ac63d8b365ea148d7c7b180c509e6c774260be89
flaviogf <flavio.fernandes6@gmail.com>
2021-09-02 10:09:33 -0300

    geral: book confident ruby

--- geral/book_confident_ruby/ex11/lib/ex11.rb
+++ geral/book_confident_ruby/ex11/lib/ex11.rb
@@ -3,7 +3,10 @@
 module Ex11
   module_function
 
-  def log_reading(value)
-    '[READING] 3.14'
+  def log_reading(reading_or_readings)
+    readings = Array(reading_or_readings)
+    readings.map do |reading|
+      '[READING] %3.2f' % reading
+    end
   end
 end
--- geral/book_confident_ruby/ex11/spec/ex11_spec.rb
+++ geral/book_confident_ruby/ex11/spec/ex11_spec.rb
@@ -7,7 +7,23 @@ RSpec.describe Ex11 do
         described_class.log_reading(3.14)
       end
 
-      it { is_expected.to eq('[READING] 3.14') }
+      it { is_expected.to eq(['[READING] 3.14']) }
+    end
+
+    context 'when pass "[]"' do
+      subject do
+        described_class.log_reading([])
+      end
+
+      it { is_expected.to eq([]) }
+    end
+
+    context 'when pass "[87.9, 45,8674, 32]"' do
+      subject do
+        described_class.log_reading([87.9, 45.8674, 32])
+      end
+
+      it { is_expected.to eq(['[READING] 87.90', '[READING] 45.87', '[READING] 32.00']) }
     end
   end
 end
