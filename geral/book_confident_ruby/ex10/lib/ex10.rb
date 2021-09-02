9a9b91c2f41a2fcb6d154b4db1a1168bcb2297de
flaviogf <flavio.fernandes6@gmail.com>
2021-09-02 09:01:35 -0300

    geral: book confident ruby

--- geral/book_confident_ruby/ex10/lib/ex10.rb
+++ geral/book_confident_ruby/ex10/lib/ex10.rb
@@ -4,6 +4,11 @@ module Ex10
   module_function
 
   def pretty_int(value)
-    '1,000'
+    decimal = Integer(value).to_s
+    leader = decimal.slice!(0, decimal.length % 3)
+    decimal.gsub!(/\d{3}(?!$)/, '\0,')
+    decimal = nil if decimal.empty?
+    leader = nil if leader.empty?
+    [leader, decimal].compact.join(',')
   end
 end
--- geral/book_confident_ruby/ex10/spec/ex10_spec.rb
+++ geral/book_confident_ruby/ex10/spec/ex10_spec.rb
@@ -9,5 +9,21 @@ RSpec.describe Ex10 do
 
       it { is_expected.to eq('1,000') }
     end
+
+    context 'when pass "23"' do
+      subject do
+        described_class.pretty_int(23)
+      end
+
+      it { is_expected.to eq('23') }
+    end
+
+    context 'when pass "4567.8"' do
+      subject do
+        described_class.pretty_int(4567.8)
+      end
+
+      it { is_expected.to eq('4,567') }
+    end
   end
 end
