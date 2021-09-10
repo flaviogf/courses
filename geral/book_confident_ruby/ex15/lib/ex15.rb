107eb2bd2d90c4fb1be9e40d4dc7414f4d5d5f0d
flaviogf <flavio.fernandes6@gmail.com>
2021-09-10 12:41:45 -0300

    geral: book confident ruby

--- geral/book_confident_ruby/ex15/lib/ex15.rb
+++ geral/book_confident_ruby/ex15/lib/ex15.rb
@@ -1,4 +1,40 @@
 # frozen_string_literal: true
 
 module Ex15
+  class BenchmarkedLogger
+    class IrcBot
+      def initialize(bot)
+        @bot = bot
+      end
+
+      def <<(message)
+        @bot.dispatch(message)
+      end
+    end
+
+    def initialize(sink = $stdout)
+      @sink = case sink
+              when Bot then IrcBot.new(sink)
+              else sink
+              end
+    end
+
+    def info(message)
+      start_time = Time.now
+      yield
+      duration = Time.now - start_time
+      line = format("[%<duration>1.2f] %<message>s\n", duration: duration, message: message)
+      @sink << line
+    end
+  end
+
+  class Bot
+    def initialize(stream = $stdout)
+      @stream = stream
+    end
+
+    def dispatch(message)
+      @stream << "BOT: #{message}"
+    end
+  end
 end
