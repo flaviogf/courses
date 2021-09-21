5ad439e470e4e9753737661c5cab72867a0573f2
flaviogf <flavio.fernandes6@gmail.com>
2021-09-21 12:55:00 -0300

    geral: book confident ruby

---
+++ geral/book_confident_ruby/ex26/lib/ex26.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Ex26
+  Point = Struct.new(:x, :y)
+end
---
+++ geral/book_confident_ruby/ex26/spec/point_spec.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+module Ex26
+  RSpec.describe Point do
+    it { is_expected.to respond_to(:x) }
+
+    it { is_expected.to respond_to(:y) }
+  end
+end
