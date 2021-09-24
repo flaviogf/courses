597b494a43b58de5d8481ff5ade2f8c3001cbea3
flaviogf <flavio.fernandes6@gmail.com>
2021-09-24 09:06:26 -0300

    geral: book confident ruby

--- geral/book_confident_ruby/ex29/lib/ex29.rb
+++ geral/book_confident_ruby/ex29/lib/ex29.rb
@@ -3,7 +3,11 @@
 module Ex29
   class << self
     def find_words(prefix)
-      []
+      words.select { |w| w.downcase =~ /\A#{prefix.downcase}/ }
+    end
+
+    def words
+      File.readlines('words').map(&:chomp)
     end
   end
 end
--- geral/book_confident_ruby/ex29/spec/ex29_spec.rb
+++ geral/book_confident_ruby/ex29/spec/ex29_spec.rb
@@ -5,10 +5,26 @@ RSpec.describe Ex29 do
 
   describe '.find_words' do
     context 'when it finds a word' do
+      let(:words) { %w[GNU LINUX APACHE RUBY] }
+
+      before { allow(described_class).to receive(:words).and_return(words) }
+
       it 'returns an array' do
         result = described_class.find_words('g')
 
         expect(result).to be_an(Array)
+      end
+
+      it 'returns an array that contains a word' do
+        result = described_class.find_words('g')
+
+        expect(result.size).to eq(1)
+      end
+
+      it 'returns an array that contains the word "GNU"' do
+        result = described_class.find_words('g')
+
+        expect(result).to include('GNU')
       end
     end
   end
