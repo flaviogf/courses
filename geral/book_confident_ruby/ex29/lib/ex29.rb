1e74ba86155eb6658b4fcebcbc2583eed8ccd369
flaviogf <flavio.fernandes6@gmail.com>
2021-09-24 08:55:50 -0300

    geral: book confident ruby

--- geral/book_confident_ruby/ex29/lib/ex29.rb
+++ geral/book_confident_ruby/ex29/lib/ex29.rb
@@ -1,4 +1,8 @@
 # frozen_string_literal: true
 
 module Ex29
+  class << self
+    def find_words
+    end
+  end
 end
---
+++ geral/book_confident_ruby/ex29/spec/ex29_spec.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+RSpec.describe Ex29 do
+  it { is_expected.to respond_to(:find_words) }
+end
