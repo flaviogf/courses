5936b1d26d5bd05ac762f357aaf034a2b733c52f
flaviogf <flavio.fernandes6@gmail.com>
2021-09-30 13:05:16 -0300

    geral: seven databases in seven weeks

--- geral/book_seven_databases_in_seven_weeks/postgresql/ex5/db/structure.sql
+++ geral/book_seven_databases_in_seven_weeks/postgresql/ex5/db/structure.sql
@@ -1,3 +1,9 @@
+CREATE EXTENSION tablefunc;
+CREATE EXTENSION dict_xsyn;
+CREATE EXTENSION fuzzystrmatch;
+CREATE EXTENSION pg_trgm;
+CREATE EXTENSION cube;
+
 CREATE TABLE genres (
   name text UNIQUE,
   position integer
