e355bf3109870332e51f1c7c3bff32e4acb1abb5
flaviogf <flavio.fernandes6@gmail.com>
2021-10-08 13:09:31 -0300

    geral: programming in c

---
+++ geral/book_programming_in_c/ex13/lib/app.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+  int a = 25, b = 5, c = 10, d = 7;
+
+  printf("a = %i, b = %i, c = %i, and d = %i\n", a, b, c, d);
+  printf("a %% b = %i\n", a % b);
+  printf("a %% c = %i\n", a % c);
+  printf("a %% d = %i\n", a % d);
+  printf("a / d * d + a %% d = %i\n", a / d * d + a % d);
+
+  return 0;
+}
