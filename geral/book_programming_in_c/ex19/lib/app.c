1fdb2eab4a9f0b7ccb4b8ba15fdb19bb01c82417
flaviogf <flavio.fernandes6@gmail.com>
2021-10-09 07:54:53 -0300

    geral: programming in c

---
+++ geral/book_programming_in_c/ex19/lib/app.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main()
+{
+  int triangularNumber;
+  triangularNumber = 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8;
+  printf("The eighth triangular numbers is %i\n", triangularNumber);
+  return 0;
+}
