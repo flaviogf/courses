a7395bb39e8bf8085a7ea166e9ea2d14000a374b
flaviogf <flavio.fernandes6@gmail.com>
2021-10-13 14:12:55 -0300

    geral: seven databases in seven weeks

--- geral/book_seven_databases_in_seven_weeks/mongodb/ex6/lib/ex6.rb
+++ geral/book_seven_databases_in_seven_weeks/mongodb/ex6/lib/ex6.rb
@@ -2,8 +2,24 @@
 
 module Ex6
   class << self
-    def populate_phones
-      puts 'populating phones'
+    def populate_phones(area: 800, start: 5_550_000, final: 5_650_000)
+      (start..final).collect do |i|
+        country = 1 + (Integer(rand * 8) << 2)
+        num = country * 1e10 + area * 1e7 + i
+        prefix = Integer(i * 1e-4) << 0
+        full_number = "#{country} #{area} - #{i}"
+
+        {
+          _id: num,
+          components: {
+            country: country,
+            area: area,
+            prefix: prefix,
+            number: i,
+            display: full_number
+          }
+        }
+      end
     end
   end
 end
