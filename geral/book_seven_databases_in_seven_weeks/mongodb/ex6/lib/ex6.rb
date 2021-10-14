285125cb4aa8cafd2b328651c9226210bc26eb1d
flaviogf <flavio.fernandes6@gmail.com>
2021-10-14 13:05:02 -0300

    geral: seven databases in seven weeks

--- geral/book_seven_databases_in_seven_weeks/mongodb/ex6/lib/ex6.rb
+++ geral/book_seven_databases_in_seven_weeks/mongodb/ex6/lib/ex6.rb
@@ -1,25 +1,46 @@
 # frozen_string_literal: true
 
+require 'mongo'
+
 module Ex6
   class << self
     def populate_phones(area: 800, start: 5_550_000, final: 5_650_000)
-      (start..final).collect do |i|
-        country = 1 + (Integer(rand * 8) << 2)
-        num = country * 1e10 + area * 1e7 + i
-        prefix = Integer(i * 1e-4) << 0
-        full_number = "#{country} #{area} - #{i}"
+      @area = area
+      @start = start
+      @final = final
 
-        {
-          _id: num,
-          components: {
-            country: country,
-            area: area,
-            prefix: prefix,
-            number: i,
-            display: full_number
-          }
-        }
-      end
+      db[:phones].bulk_write(phones)
+
+      puts 'Well done'
+    end
+
+    private
+
+    attr_reader :area, :start, :final
+
+    def phones
+      (start..final).collect { |i| phone(i) }
+    end
+
+    def phone(index)
+      country = 1 + (Integer(rand * 8) << 2)
+      num = country * 1e10 + area * 1e7 + index
+      prefix = Integer(index * 1e-4) << 0
+      full_number = "#{country} #{area} - #{index}"
+
+      components = { country: country, area: area, prefix: prefix, number: index, display: full_number }
+
+      insert_one = { _id: num, components: components }
+
+      { insert_one: insert_one }
+    end
+
+    def db
+      client.database
+    end
+
+    def client
+      Mongo::Client.new(['db'], database: 'book', user: 'mongodb', password: 'mongodb', auth_source: 'admin')
     end
   end
 end
