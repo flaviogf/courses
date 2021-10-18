c6fc54a0f0019fa741fd7d37df01275b79d49075
flaviogf <flavio.fernandes6@gmail.com>
2021-10-18 17:40:37 -0300

    geral: book programming in c

--- geral/book_programming_in_c/ex51/lib/app.c
+++ geral/book_programming_in_c/ex51/lib/app.c
@@ -3,12 +3,11 @@
 int main()
 {
   int arrayValues[10] = { 0, 1, 4, 9, 16 };
-  int i;
 
-  for(i = 5; i < 10; ++i)
+  for(int i = 5; i < 10; ++i)
     arrayValues[i] = i * i;
 
-  for(i = 0; i < 10; ++i)
+  for(int i = 0; i < 10; ++i)
     printf("arrayValues[%i] = %i\n", i, arrayValues[i]);
 
   return 0;
