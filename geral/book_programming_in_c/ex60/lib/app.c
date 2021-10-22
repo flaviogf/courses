3335009de9ae571d19022415e39b00d132352601
flaviogf <flavio.fernandes6@gmail.com>
2021-10-22 09:53:19 -0300

    geral: book programming in c

--- geral/book_programming_in_c/ex60/lib/app.c
+++ geral/book_programming_in_c/ex60/lib/app.c
@@ -2,6 +2,15 @@
 
 int main()
 {
-  printf("Hey\n");
+  int numbers[50];
+
+  for(int i = 1, j = 2; i < 50; ++i, ++j)
+    numbers[i] = j;
+
+  for(int i = 0; i < 50; ++i)
+    printf("%i ", numbers[i]);
+
+  printf("\n");
+
   return 0;
 }
