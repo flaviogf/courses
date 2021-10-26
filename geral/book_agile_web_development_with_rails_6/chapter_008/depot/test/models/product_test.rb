0a78f6e83e843c930ea5cb84ff41b7874c157512
flaviogf <flavio.fernandes6@gmail.com>
2021-10-26 17:24:59 -0300

    geral: book agile web development with rails 6

--- geral/book_agile_web_development_with_rails_6/chapter_008/depot/test/models/product_test.rb
+++ geral/book_agile_web_development_with_rails_6/chapter_008/depot/test/models/product_test.rb
@@ -1,15 +1,27 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
-  test 'when title is empty expect to has an error' do
+  test 'when title is nil or empty expect to has an error' do
     product = Product.new(title: '')
     product.valid?
     assert product.errors[:title].any?
   end
 
-  test 'when description is empty expect to has an error' do
+  test 'when description is nil or empty expect to has an error' do
     product = Product.new(description: '')
     product.valid?
     assert product.errors[:description].any?
+  end
+
+  test 'when image_url is nil or empty expect to has an error' do
+    product = Product.new(image_url: '')
+    product.valid?
+    assert product.errors[:image_url].any?
+  end
+
+  test 'when price is less than 0.1 expect to has an error' do
+    product = Product.new(price: 0)
+    product.valid?
+    assert product.errors[:price].any?
   end
 end
