a1c32fa0e970f251868bedffa5c57c2a796e580e
flaviogf <flavio.fernandes6@gmail.com>
2021-10-27 12:48:08 -0300

    geral: book agile web development with rails 6

--- geral/book_agile_web_development_with_rails_6/chapter_008/depot/test/controllers/products_controller_test.rb
+++ geral/book_agile_web_development_with_rails_6/chapter_008/depot/test/controllers/products_controller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class ProductsControllerTest < ActionDispatch::IntegrationTest
   setup do
-    @product = products(:one)
+    @product = products(:ruby)
   end
 
   test "should get index" do
