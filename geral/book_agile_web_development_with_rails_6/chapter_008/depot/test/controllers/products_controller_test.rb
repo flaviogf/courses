114da8747100f3821fe7fe716e608e13cea8f2aa
flaviogf <flavio.fernandes6@gmail.com>
2021-10-27 12:53:11 -0300

    geral: book agile web development with rails 6

--- geral/book_agile_web_development_with_rails_6/chapter_008/depot/test/controllers/products_controller_test.rb
+++ geral/book_agile_web_development_with_rails_6/chapter_008/depot/test/controllers/products_controller_test.rb
@@ -35,7 +35,7 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should update product" do
-    patch product_url(@product), params: { product: { description: @product.description, image_url: @product.image_url, price: @product.price, title: @product.title } }
+    patch product_url(@product), params: { product: { description: @product.description, image_url: @product.image_url, price: @product.price, title: @product_title } }
     assert_redirected_to product_url(@product)
   end
 
