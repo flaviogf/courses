d07cbdb457f667cbe6c9b8ed69ef7b5ef203bb15
flaviogf <flavio.fernandes6@gmail.com>
2021-10-31 09:15:27 -0300

    geral: book programming in c

--- geral/book_programming_in_c/ex98/lib/app.c
+++ geral/book_programming_in_c/ex98/lib/app.c
@@ -4,9 +4,8 @@
 
 int main(int argc, char **argv)
 {
-  printf("squareRoot(2.0) = %f\n", squareRoot(2.0));
+  printf("squareRoot(4.0) = %f\n", squareRoot(4.0));
   printf("squareRoot(144.0) = %f\n", squareRoot(144.0));
-  printf("squareRoot(17.5) = %f\n", squareRoot(17.5));
 
   return EXIT_SUCCESS;
 }
--- geral/book_programming_in_c/ex98/lib/helper.c
+++ geral/book_programming_in_c/ex98/lib/helper.c
@@ -1,9 +1,16 @@
 float absoluteValue(float x)
 {
+  if(x < 0) x = -x;
+
   return x;
 }
 
 float squareRoot(float x)
 {
-  return x;
+  float guess = 1.0;
+
+  while(absoluteValue(((guess * guess / x) -1) * 100) != 0)
+    guess = (x / guess + guess) / 2.0;
+
+  return guess;
 }
