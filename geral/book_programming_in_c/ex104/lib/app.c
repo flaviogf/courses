90cd55ef0c662967f668ff0a57a100831daf9491
flaviogf <flavio.fernandes6@gmail.com>
2021-11-02 07:31:04 -0300

    geral: book programming in c

--- geral/book_programming_in_c/ex104/lib/app.c
+++ geral/book_programming_in_c/ex104/lib/app.c
@@ -1,8 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+struct time
+{
+  int hour;
+  int minutes;
+  int seconds;
+};
+
 int main(int argc, char **argv)
 {
-  printf("Hey\n");
+  printf("Enter the time (hh:mm:ss): ");
+  struct time currentTime;
+  scanf("%i:%i:%i", &currentTime.hour, &currentTime.minutes, &currentTime.seconds);
+
+  printf("Updated times is %.2i:%.2i:%.2i.\n", currentTime.hour, currentTime.minutes, currentTime.seconds);
+
   return EXIT_SUCCESS;
 }
