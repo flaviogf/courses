0cfeb1564f8ea35885075dd5bb9038e8e242008f
flaviogf <flavio.fernandes6@gmail.com>
2021-11-07 08:48:04 -0300

    geral: book programming in c

--- geral/book_programming_in_c/ex112/lib/app.c
+++ geral/book_programming_in_c/ex112/lib/app.c
@@ -1,8 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "helper.h"
 
 int main(int argc, char **argv)
 {
-  printf("Hey there\n");
+  const char str1[] = { "Test " };
+  const char str2[] = { "works" };
+
+  char result[11];
+  concat(result, str1, 5, str2, 6);
+
+  for(int i = 0; i < 11; ++i) printf("%c", result[i]);
+
+  printf("\n");
+
   return EXIT_SUCCESS;
 }
