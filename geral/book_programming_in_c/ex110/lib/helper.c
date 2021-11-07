db797e1d70575226dfe0369ba112473e4be5b4b6
flaviogf <flavio.fernandes6@gmail.com>
2021-11-07 08:14:51 -0300

    geral: book programming in c

---
+++ geral/book_programming_in_c/ex110/lib/helper.c
@@ -0,0 +1,5 @@
+#include "helper.h"
+
+void concat(char result[], const char str1[], int n1, const char str2[], int n2)
+{
+}
---
+++ geral/book_programming_in_c/ex110/lib/helper.h
@@ -0,0 +1,1 @@
+void concat(char result[], const char str1[], int n1, const char str2[], int n2);
