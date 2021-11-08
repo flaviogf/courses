8e52b99a2d4081b3145b9d15420023221d468e50
flaviogf <flavio.fernandes6@gmail.com>
2021-11-08 12:31:53 -0300

    geral: book programming in c

--- geral/book_programming_in_c/ex114/lib/app.c
+++ geral/book_programming_in_c/ex114/lib/app.c
@@ -6,7 +6,7 @@ int main(int argc, char **argv)
   printf("Enter text:\n");
 
   char str1[81], str2[81], str3[81];
-  scanf("%s%s%s", str1, str2, str3);
+  scanf("%80s%80s%80s", str1, str2, str3);
 
   printf("\nstr1 = %s\nstr2 = %s\nstr3 = %s\n", str1, str2, str3);
 
