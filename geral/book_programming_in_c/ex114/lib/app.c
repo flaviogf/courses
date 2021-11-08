ee587ce746152dc5b0eb98f43254eb19a9681170
flaviogf <flavio.fernandes6@gmail.com>
2021-11-08 12:30:56 -0300

    geral: book programming in c

--- geral/book_programming_in_c/ex114/lib/app.c
+++ geral/book_programming_in_c/ex114/lib/app.c
@@ -3,6 +3,12 @@
 
 int main(int argc, char **argv)
 {
-  printf("Hey\n");
+  printf("Enter text:\n");
+
+  char str1[81], str2[81], str3[81];
+  scanf("%s%s%s", str1, str2, str3);
+
+  printf("\nstr1 = %s\nstr2 = %s\nstr3 = %s\n", str1, str2, str3);
+
   return EXIT_SUCCESS;
 }
