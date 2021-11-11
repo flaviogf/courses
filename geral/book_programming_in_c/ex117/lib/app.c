4386a4433bbf524d398818762492ff7a07b2eb53
flaviogf <flavio.fernandes6@gmail.com>
2021-11-11 13:22:45 -0300

    geral: book programming in c

--- geral/book_programming_in_c/ex117/lib/app.c
+++ geral/book_programming_in_c/ex117/lib/app.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "helper.h"
 
 int main(int argc, char **argv)
 {
-  printf("Hey\n");
+  printf("%i\n", alphabetic('a'));
+  printf("%i\n", alphabetic('0'));
   return EXIT_SUCCESS;
 }
