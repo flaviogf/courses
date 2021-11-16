3192126e3490b6b4483735299e131a65bb50d06f
flaviogf <flavio.fernandes6@gmail.com>
2021-11-16 13:11:52 -0300

    geral: book agile web development with rails 6

--- geral/book_agile_web_development_with_rails_6/chapter_008/depot/test/controllers/store_controller_test.rb
+++ geral/book_agile_web_development_with_rails_6/chapter_008/depot/test/controllers/store_controller_test.rb
@@ -4,6 +4,10 @@ class StoreControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
     get store_index_url
     assert_response :success
+    assert_select 'nav.side_nav a', minimum: 4
+    assert_select 'main ul.catalog li', 1
+    assert_select 'h2', 'Programming in ruby'
+    assert_select '.price', /\$[,\d]+\.\d\d/
   end
 
 end
