95256acb60361697391cb9755ba98a70f5510790
flaviogf <flavio.fernandes6@gmail.com>
2021-11-17 16:39:09 -0300

    geral: book agile web development with rails 6

--- geral/book_agile_web_development_with_rails_6/depot/test/models/product_test.rb
+++ geral/book_agile_web_development_with_rails_6/depot/test/models/product_test.rb
@@ -23,4 +23,21 @@ class ProductTest < ActiveSupport::TestCase
     product.price = 1
     assert product.valid?
   end
+
+  test 'image url' do
+    ok = %w[fred.gif fred.jpg fred.png FRED.JPG FRED.Jpg http://a.b.c/x/y/z/fred.gif]
+    bad = %w[fred.doc fred.gif/more fred.gif.more]
+
+    ok.each do |image_url|
+      assert new_product(image_url).valid?, "#{image_url} shouldn't be invalid"
+    end
+
+    bad.each do |image_url|
+      assert new_product(image_url).invalid?, "#{image_url} shouldn't be valid"
+    end
+  end
+
+  def new_product(image_url)
+    Product.new(title: 'My Book Title', description: 'yyy', price: 1, image_url: image_url)
+  end
 end
