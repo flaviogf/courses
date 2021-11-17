38e06319149b4e4eb71186ace3796b00f8d20fef
flaviogf <flavio.fernandes6@gmail.com>
2021-11-17 16:49:57 -0300

    geral: book agile web development with rails 6

--- geral/book_agile_web_development_with_rails_6/depot/test/models/product_test.rb
+++ geral/book_agile_web_development_with_rails_6/depot/test/models/product_test.rb
@@ -1,6 +1,8 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
+  fixtures :products
+
   test 'product attributes must not be empty' do
     product = Product.new
     assert product.invalid?
@@ -35,6 +37,18 @@ class ProductTest < ActiveSupport::TestCase
     bad.each do |image_url|
       assert new_product(image_url).invalid?, "#{image_url} shouldn't be valid"
     end
+  end
+
+  test 'product is not valid without a unique title' do
+    product = Product.new(title: products(:ruby).title, description: 'yyy', price: 1, image_url: 'fred.gif')
+    assert product.invalid?
+    assert_equal ['has already been taken'], product.errors[:title]
+  end
+
+  test 'product is not valid without a unique title - i18n' do
+    product = Product.new(title: products(:ruby).title, description: 'yyy', price: 1, image_url: 'fred.gif')
+    assert product.invalid?
+    assert_equal [I18n.translate('errors.messages.taken')], product.errors[:title]
   end
 
   def new_product(image_url)
