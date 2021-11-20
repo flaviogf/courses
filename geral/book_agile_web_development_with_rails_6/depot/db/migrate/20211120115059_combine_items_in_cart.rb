4fd70bcb42d69915c59fef19bcdd85fd52ab213d
flaviogf <flavio.fernandes6@gmail.com>
2021-11-20 08:58:56 -0300

    geral: book agile web development with rails 6

---
+++ geral/book_agile_web_development_with_rails_6/depot/db/migrate/20211120115059_combine_items_in_cart.rb
@@ -0,0 +1,29 @@
+class CombineItemsInCart < ActiveRecord::Migration[6.0]
+  def up
+    Cart.all.each do |cart|
+      sums = cart.line_items.group(:product_id).sum(:quantity)
+
+      sums.each do |product_id, quantity|
+        cart.line_items.where(product_id: product_id).delete_all
+
+        item = cart.line_items.build(product_id: product_id)
+        item.quantity = quantity
+        item.save!
+      end
+    end
+  end
+
+  def down
+    LineItem.where('quantity > 1').each do |line_item|
+      line_item.quantity.times do
+        LineItem.create(
+          cart_id: line_item.cart_id,
+          product_id: line_item.product_id,
+          quantity: 1
+        )
+      end
+
+      line_item.destroy
+    end
+  end
+end
--- geral/book_agile_web_development_with_rails_6/depot/db/schema.rb
+++ geral/book_agile_web_development_with_rails_6/depot/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_20_114528) do
+ActiveRecord::Schema.define(version: 2021_11_20_115059) do
 
   create_table "carts", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
