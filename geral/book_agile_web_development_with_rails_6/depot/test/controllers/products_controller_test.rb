5e420fc49cf4d1859e0c348456c6c1589c0741a6
flaviogf <flavio.fernandes6@gmail.com>
2021-11-23 13:09:25 -0300

    geral: book agile web development with rails 6

--- geral/book_agile_web_development_with_rails_6/depot/test/controllers/products_controller_test.rb
+++ geral/book_agile_web_development_with_rails_6/depot/test/controllers/products_controller_test.rb
@@ -9,6 +9,7 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
     get products_url
     assert_response :success
+    assert_select 'tr', 4
   end
 
   test "should get new" do
