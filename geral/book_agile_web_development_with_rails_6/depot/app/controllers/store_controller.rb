03072db3887bc4ae6eda86c5b16f0167a2363bca
flaviogf <flavio.fernandes6@gmail.com>
2021-11-23 13:20:52 -0300

    geral: book agile web development with rails 6

--- geral/book_agile_web_development_with_rails_6/depot/app/controllers/store_controller.rb
+++ geral/book_agile_web_development_with_rails_6/depot/app/controllers/store_controller.rb
@@ -1,9 +1,18 @@
 class StoreController < ApplicationController
   include CurrentCart
 
+  before_action :incr_times
   before_action :set_cart
 
   def index
     @products = Product.order(:title)
+  end
+
+  private
+
+  def incr_times
+    session[:times] ||= 0
+    session[:times] += 1
+    logger.info "The current user entered #{session[:times]} at the store page"
   end
 end
