d6745a836dd420a3d01b2a701ab1817f9770cdeb
flaviogf <flavio.fernandes6@gmail.com>
2021-11-24 13:17:42 -0300

    geral: book agile web development with rails 6

--- geral/book_agile_web_development_with_rails_6/depot/test/models/cart_test.rb
+++ geral/book_agile_web_development_with_rails_6/depot/test/models/cart_test.rb
@@ -1,7 +1,19 @@
 require 'test_helper'
 
 class CartTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  fixtures :products
+
+  test 'add cart' do
+    cart = Cart.new
+
+    2.times do
+      cart.add_product(products(:ruby))
+      cart.save!
+    end
+
+    cart.add_product(products(:one))
+    cart.save!
+
+    assert_equal cart.line_items.count, 2
+  end
 end
