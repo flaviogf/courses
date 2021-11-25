5111cb85f118b9bdb80bed5d7451f02b4ae61415
flaviogf <flavio.fernandes6@gmail.com>
2021-11-25 16:52:51 -0300

    geral: book agile web development with rails 6

--- geral/book_agile_web_development_with_rails_6/depot/app/models/line_item.rb
+++ geral/book_agile_web_development_with_rails_6/depot/app/models/line_item.rb
@@ -1,6 +1,7 @@
 class LineItem < ApplicationRecord
   belongs_to :product
-  belongs_to :cart
+  belongs_to :cart, optional: true
+  belongs_to :order, optional: true
 
   def total_price
     product.price * quantity
