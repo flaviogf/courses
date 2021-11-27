a6f6e6eccc86d263da1ee2cef21b55376f37a0f2
flaviogf <flavio.fernandes6@gmail.com>
2021-11-27 11:30:29 -0300

    geral: book agile web development with rails 6

--- geral/book_agile_web_development_with_rails_6/depot/test/application_system_test_case.rb
+++ geral/book_agile_web_development_with_rails_6/depot/test/application_system_test_case.rb
@@ -1,5 +1,11 @@
 require "test_helper"
+require "socket"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
+  driven_by :selenium, using: :chrome, screen_size: [1400, 1400], options: { browser: :remote, url: "http://chrome:4444/wd/hub", capabilities: %i[chrome] }
+
+  def setup
+    host! "http://#{IPSocket.getaddress(Socket.gethostname)}"
+    super
+  end
 end
--- geral/book_agile_web_development_with_rails_6/depot/test/test_helper.rb
+++ geral/book_agile_web_development_with_rails_6/depot/test/test_helper.rb
@@ -2,9 +2,13 @@ ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
 
+Capybara.server_host = '0.0.0.0'
+Capybara.server_port = 4000
+Capybara.app_host = 'http://web:4000'
+
 class ActiveSupport::TestCase
   # Run tests in parallel with specified workers
-  parallelize(workers: :number_of_processors)
+  parallelize(workers: 1)
 
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
