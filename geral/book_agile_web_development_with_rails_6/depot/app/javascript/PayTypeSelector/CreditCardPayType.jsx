a1e0caf2a9fdc0a4e113b7e7b1f6c529ea42b4a1
flaviogf <flavio.fernandes6@gmail.com>
2021-11-27 09:27:57 -0300

    geral: book agile web development with rails 6

--- geral/book_agile_web_development_with_rails_6/depot/app/javascript/PayTypeSelector/CreditCardPayType.jsx
+++ geral/book_agile_web_development_with_rails_6/depot/app/javascript/PayTypeSelector/CreditCardPayType.jsx
@@ -1,7 +1,19 @@
 import React from 'react'
 
 function CreditCardPayType() {
-  return <h1>Credit Card Pay Type</h1>
+  return (
+    <div>
+      <div className="field">
+        <label htmlFor="order_credit_card_number">CC</label>
+        <input type="password" name="order[credit_card_number]" id="order_credit_card_number" />
+      </div>
+
+      <div className="field">
+        <label htmlFor="order_expiration_date">Expiry</label>
+        <input type="text" name="order[expiration_date]" id="order_expiration_date" size="9" placeholder="e.g. 03/19" />
+      </div>
+    </div>
+  )
 }
 
 export default CreditCardPayType
