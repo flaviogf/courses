84abebcbee384aaa7c08cf48d9d06a0af02afdc9
flaviogf <flavio.fernandes6@gmail.com>
2021-11-28 09:13:40 -0300

    geral: book agile web development with rails 6

--- geral/book_agile_web_development_with_rails_6/depot/config/environments/development.rb
+++ geral/book_agile_web_development_with_rails_6/depot/config/environments/development.rb
@@ -61,4 +61,5 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { :host => 'http://localhost:3001' }
 end
