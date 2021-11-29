ccb051dc33ab0d990cc36194d848317be9dc3234
flaviogf <flavio.fernandes6@gmail.com>
2021-11-29 16:12:55 -0300

    geral: book agile web development with rails 6

--- geral/book_agile_web_development_with_rails_6/depot/lib/pago.rb
+++ geral/book_agile_web_development_with_rails_6/depot/lib/pago.rb
@@ -2,7 +2,23 @@ require 'ostruct'
 
 class Pago
   class << self
-    def make_payment
+    def make_payment(order_id:, payment_method:, payment_details:)
+      case payment_method
+      when :check
+        Rails.logger.info("Processing check: #{payment_details.fetch(:routing)}/#{payment_details.fetch(:account)}"
+      when :credit_card
+        Rails.logger.info("Processing credit card: #{payment_details.fetch(:cc_num)}/#{payment_details.fetch(:expiration_month)}/#{payment_details.fetch(:expiration_year)}"
+      when :po
+        Rails.logger.info("Processing purchase order: #{payment_details.fetch(:po_number)}"
+      else
+        raise StandardError, "Unknown payment method #{payment_method}"
+      end
+
+      sleep 3 unless Rails.env.test?
+
+      Rails.logger.info('Done Processing Payment')
+
+      OpenStruct.new(succeed?: true)
     end
   end
 end
