da7b9ec17e6fe86ddbc5fdf3f1fe6c27fbcf4f39
flaviogf <flavio.fernandes6@gmail.com>
2021-12-02 13:23:09 -0300

    geral: book agile web development with rails 6

--- geral/book_agile_web_development_with_rails_6/depot/app/controllers/sessions_controller.rb
+++ geral/book_agile_web_development_with_rails_6/depot/app/controllers/sessions_controller.rb
@@ -3,6 +3,12 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by(name: params[:name])
+
+    return redirect_to login_url, alert: 'Invalid user/password combination' unless user.try(:authenticate, params[:password])
+
+    session[:id] = user.id
+    redirect_to admin_url
   end
 
   def destroy
