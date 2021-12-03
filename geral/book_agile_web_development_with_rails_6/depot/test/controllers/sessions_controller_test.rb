47f9b723ef360cb6d22796acfe17c8bc3ee5bdea
flaviogf <flavio.fernandes6@gmail.com>
2021-12-03 16:19:27 -0300

    geral: book agile web development with rails 6

--- geral/book_agile_web_development_with_rails_6/depot/test/controllers/sessions_controller_test.rb
+++ geral/book_agile_web_development_with_rails_6/depot/test/controllers/sessions_controller_test.rb
@@ -1,19 +1,21 @@
 require 'test_helper'
 
 class SessionsControllerTest < ActionDispatch::IntegrationTest
-  test "should get new" do
-    get sessions_new_url
+  test 'should prompt for login' do
+    get login_url
     assert_response :success
   end
 
-  test "should get create" do
-    get sessions_create_url
-    assert_response :success
+  test 'should login' do
+    dave = users(:dave)
+    post login_url, params: { name: dave.name, password: 'secret' }
+    assert_redirected_to admin_url
+    assert_equal dave.id, session[:user_id]
   end
 
-  test "should get destroy" do
-    get sessions_destroy_url
-    assert_response :success
+  test 'shoud fail login' do
+    dave = users(:dave)
+    post login_url, params: { name: dave.name, password: 'wrong' }
+    assert_redirected_to login_url
   end
-
 end
