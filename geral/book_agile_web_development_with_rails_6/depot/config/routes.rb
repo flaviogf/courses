7a381305a54496813926f7f371387db2806cc4d7
flaviogf <flavio.fernandes6@gmail.com>
2021-12-03 13:43:53 -0300

    geral: book agile web development with rails 6

--- geral/book_agile_web_development_with_rails_6/depot/config/routes.rb
+++ geral/book_agile_web_development_with_rails_6/depot/config/routes.rb
@@ -1,13 +1,19 @@
 Rails.application.routes.draw do
-  get 'admin/index'
-  get 'sessions/new'
-  get 'sessions/create'
-  get 'sessions/destroy'
+  root 'store#index', as: 'store_index'
+
+  get 'admin' => 'admin#index'
+
+  controller :sessions do
+    get 'login' => :new
+    post 'login' => :create
+    delete 'logout' => :destroy
+  end
+
   resources :users
   resources :orders
   resources :line_items
   resources :carts
-  root 'store#index', as: 'store_index'
+
   resources :products do
     get :who_bought, on: :member
   end
