92eaeac69d22a4dc8475609654aa93ca5a2f9449
flaviogf <flavio.fernandes6@gmail.com>
2021-12-03 13:36:17 -0300

    geral: book agile web development with rails 6

--- geral/book_agile_web_development_with_rails_6/depot/app/controllers/sessions_controller.rb
+++ geral/book_agile_web_development_with_rails_6/depot/app/controllers/sessions_controller.rb
@@ -7,10 +7,12 @@ class SessionsController < ApplicationController
 
     return redirect_to login_url, alert: 'Invalid user/password combination' unless user.try(:authenticate, params[:password])
 
-    session[:id] = user.id
+    session[:user_id] = user.id
     redirect_to admin_url
   end
 
   def destroy
+    session[:user_id] = nil
+    redirect_to store_index_url, notice: 'Logged out'
   end
 end
