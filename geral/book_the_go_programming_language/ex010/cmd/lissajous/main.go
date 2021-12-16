0d86f11863727a9c4ddf8936f14c4964050adcff
flaviogf <flavio.fernandes6@gmail.com>
2021-12-16 13:21:46 -0300

    book: the go programming language

---
+++ geral/book_the_go_programming_language/ex010/cmd/lissajous/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"image/color"
+	"io"
+	"os"
+)
+
+var palette = []color.Color{color.White, color.Black}
+
+const (
+	whiteIndex = 0
+	blackIndex = 1
+)
+
+func main() {
+	lissajous(os.Stdout)
+}
+
+func lissajous(out io.Writer) {
+}
