bf126696fcda696d3c5d1ce1de947a123ded1c55
flaviogf <flavio.fernandes6@gmail.com>
2021-12-17 11:33:46 -0300

    book: the go programming language

---
+++ geral/book_the_go_programming_language/ex011/cmd/fetch/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+)
+
+func main() {
+	for _, url := range os.Args[1:] {
+		resp, err := http.Get(url)
+
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+			os.Exit(1)
+		}
+
+		defer resp.Body.Close()
+
+		bytes, err := ioutil.ReadAll(resp.Body)
+
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
+			os.Exit(1)
+		}
+
+		fmt.Printf("%s\n", bytes)
+	}
+}
