b4c1c54b9eb385006e5f5b95b66ca7a896fafa6c
flaviogf <flavio.fernandes6@gmail.com>
2021-12-17 14:53:40 -0300

    book: the go programming language

---
+++ geral/book_the_go_programming_language/ex015/cmd/server/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe("localhost:8080", nil))
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+}
---
+++ geral/book_the_go_programming_language/ex015/go.mod
@@ -0,0 +1,3 @@
+module github.com/flaviogf/courses/geral/book_the_go_programming_language/ex015
+
+go 1.17
