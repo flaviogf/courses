5d4c31aae474f597cb4e5c82a84c1a8db3783c8b
flaviogf <flavio.fernandes6@gmail.com>
2021-12-18 09:15:56 -0300

    book: the go programming language

--- geral/book_the_go_programming_language/ex018/cmd/boiling/main.go
+++ geral/book_the_go_programming_language/ex018/cmd/boiling/main.go
@@ -2,6 +2,12 @@ package main
 
 import "fmt"
 
+const boilingF = 212.0
+
 func main() {
-	fmt.Println("It works")
+	var f = boilingF
+
+	var c = (f - 32) * 5 / 9
+
+	fmt.Printf("boiling point = %gºF or %gºC\n", f, c)
 }
