4b9bdb9868a62eae8b06d66fa877d6339f61427c
flaviogf <flavio.fernandes6@gmail.com>
2021-12-21 11:51:52 -0300

    book: learn test with go

--- geral/book_learn_go_with_tests/ex009/iteration.go
+++ geral/book_learn_go_with_tests/ex009/iteration.go
@@ -1,5 +1,15 @@
 package iteration
 
+func SumAll(values ...[]int) []int {
+	sum := make([]int, len(values))
+
+	for i, value := range values {
+		sum[i] = Sum(value)
+	}
+
+	return sum
+}
+
 func Sum(values []int) int {
 	sum := 0
 
