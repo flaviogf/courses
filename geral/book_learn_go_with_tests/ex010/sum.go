ce6cd8dd1a79a02414c7f418be76f8feaa6b1eef
flaviogf <flavio.fernandes6@gmail.com>
2021-12-22 09:38:17 -0300

    geral: book learn go with tests

--- geral/book_learn_go_with_tests/ex010/sum.go
+++ geral/book_learn_go_with_tests/ex010/sum.go
@@ -1,5 +1,19 @@
 package sum
 
+func SumAllTails(values ...[]int) []int {
+	result := make([]int, len(values))
+
+	for i, value := range values {
+		if len(value) == 0 {
+			result[i] = 0
+		} else {
+			result[i] = Sum(value[1:])
+		}
+	}
+
+	return result
+}
+
 func SumAll(values ...[]int) []int {
 	result := make([]int, len(values))
 
--- geral/book_learn_go_with_tests/ex010/sum_test.go
+++ geral/book_learn_go_with_tests/ex010/sum_test.go
@@ -5,6 +5,26 @@ import (
 	"testing"
 )
 
+func TestSumAllTails(t *testing.T) {
+	checkSum := func(t testing.TB, got, want []int) {
+		t.Helper()
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got: %v, want: %v", got, want)
+		}
+	}
+
+	got := SumAllTails([]int{1, 2}, []int{0, 9})
+	want := []int{2, 9}
+	checkSum(t, got, want)
+
+	t.Run("with an empty slice", func(t *testing.T) {
+		got := SumAllTails([]int{})
+		want := []int{0}
+		checkSum(t, got, want)
+	})
+}
+
 func TestSumAll(t *testing.T) {
 	got := SumAll([]int{1, 2}, []int{0, 9})
 	want := []int{3, 9}
