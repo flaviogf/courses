219440647708dc4e752fa182063440ce4bc69161
flaviogf <flavio.fernandes6@gmail.com>
2021-12-25 07:54:09 -0300

    geral: book learn go with tests

--- geral/book_learn_go_with_tests/ex018/wallet_test.go
+++ geral/book_learn_go_with_tests/ex018/wallet_test.go
@@ -14,4 +14,16 @@ func TestWallet(t *testing.T) {
 			t.Errorf("got: %s, want: %s", got, want)
 		}
 	})
+
+	t.Run("Withdraw", func(t *testing.T) {
+		wallet := Wallet{Bitcoin(100)}
+		wallet.Withdraw(Bitcoin(50))
+
+		got := wallet.Balance()
+		want := Bitcoin(50)
+
+		if got != want {
+			t.Errorf("got: %s, want: %s", got, want)
+		}
+	})
 }
