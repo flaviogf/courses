19c2e2a6335e5d07954bf8557059d0eaacdf8c62
flaviogf <flavio.fernandes6@gmail.com>
2021-12-25 07:47:17 -0300

    geral: book learn go with tests

--- geral/book_learn_go_with_tests/ex017/wallet_test.go
+++ geral/book_learn_go_with_tests/ex017/wallet_test.go
@@ -3,29 +3,27 @@ package wallet
 import "testing"
 
 func TestWallet(t *testing.T) {
+	assertBalance := func(t testing.TB, wallet *Wallet, want Bitcoin) {
+		t.Helper()
+
+		got := wallet.Balance()
+
+		if got != want {
+			t.Errorf("got: %s, want: %s", got, want)
+		}
+	}
+
 	t.Run("Deposit", func(t *testing.T) {
 		wallet := Wallet{}
 		wallet.Deposit(Bitcoin(100))
 
-		got := wallet.Balance()
-		want := Bitcoin(100)
-
-		if got != want {
-			t.Errorf("got: %s, want: %s", got, want)
-		}
+		assertBalance(t, &wallet, Bitcoin(100))
 	})
 
 	t.Run("Withdraw", func(t *testing.T) {
-		wallet := Wallet{}
-		wallet.Deposit(Bitcoin(100))
-
+		wallet := Wallet{Bitcoin(100)}
 		wallet.Withdraw(Bitcoin(50))
 
-		got := wallet.Balance()
-		want := Bitcoin(50)
-
-		if got != want {
-			t.Errorf("got: %s, want: %s", got, want)
-		}
+		assertBalance(t, &wallet, Bitcoin(50))
 	})
 }
