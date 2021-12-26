60c3211bd78ebf38663df8f5fe91a64916d34654
flaviogf <flavio.fernandes6@gmail.com>
2021-12-26 07:50:39 -0300

    geral: book learn go with tests

--- geral/book_learn_go_with_tests/ex020/dictionary.go
+++ geral/book_learn_go_with_tests/ex020/dictionary.go
@@ -7,11 +7,11 @@ var ErrNotFound = errors.New("could not find the word you were looking for")
 type Dictionary map[string]string
 
 func (d Dictionary) Search(word string) (string, error) {
-	word, ok := d[word]
+	definition, ok := d[word]
 
 	if !ok {
 		return "", ErrNotFound
 	}
 
-	return word, nil
+	return definition, nil
 }
