063feddb80e76f676b90dba08459dfe249073446
flaviogf <flavio.fernandes6@gmail.com>
2021-12-27 10:36:18 -0300

    geral: book learn go with tests

--- geral/book_learn_go_with_tests/ex023/dictionary.go
+++ geral/book_learn_go_with_tests/ex023/dictionary.go
@@ -3,11 +3,23 @@ package dictionary
 import "errors"
 
 var ErrNotFound = errors.New("could not find the word you were looking for")
+var ErrWordExists = errors.New("cannot add word because it already exists")
 
 type Dictionary map[string]string
 
-func (d Dictionary) Add(word, definition string) {
-	d[word] = definition
+func (d Dictionary) Add(word, definition string) error {
+	_, err := d.Search(word)
+
+	switch err {
+	case ErrNotFound:
+		d[word] = definition
+	case nil:
+		return ErrWordExists
+	default:
+		return err
+	}
+
+	return nil
 }
 
 func (d Dictionary) Search(word string) (string, error) {
--- geral/book_learn_go_with_tests/ex023/dictionary_test.go
+++ geral/book_learn_go_with_tests/ex023/dictionary_test.go
@@ -7,6 +7,14 @@ func TestAdd(t *testing.T) {
 	dictionary.Add("test", "it is just a test")
 	got, _ := dictionary.Search("test")
 	assertDefinition(t, got, "it is just a test")
+
+	t.Run("when word already exists", func(t *testing.T) {
+		dictionary := Dictionary{"test": "it is just a test"}
+		err := dictionary.Add("test", "it will not work")
+		got, _ := dictionary.Search("test")
+		assertError(t, err, ErrWordExists)
+		assertDefinition(t, got, "it is just a test")
+	})
 }
 
 func TestSearch(t *testing.T) {
