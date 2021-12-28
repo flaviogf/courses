c65dfa2f8fe72ab8e9392cd6c20dbf83dbca0745
flaviogf <flavio.fernandes6@gmail.com>
2021-12-28 10:12:16 -0300

    geral: book learn go with tests

--- geral/book_learn_go_with_tests/ex025/countdown.go
+++ geral/book_learn_go_with_tests/ex025/countdown.go
@@ -3,16 +3,29 @@ package main
 import (
 	"fmt"
 	"io"
+	"os"
+	"time"
 )
 
 type Sleeper interface {
 	Sleep()
 }
 
+type DefaultSleeper struct{}
+
+func (d *DefaultSleeper) Sleep() {
+	time.Sleep(1 * time.Second)
+}
+
 const (
 	countdownStart = 3
 	finalWord      = "GO!"
 )
+
+func main() {
+	s := &DefaultSleeper{}
+	Countdown(os.Stdout, s)
+}
 
 func Countdown(w io.Writer, s Sleeper) {
 	for i := countdownStart; i > 0; i-- {
