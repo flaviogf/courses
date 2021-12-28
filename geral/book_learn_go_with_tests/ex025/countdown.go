6fc16eb4138c793d54f8146d67f36ca49f4ca5d2
flaviogf <flavio.fernandes6@gmail.com>
2021-12-28 10:09:48 -0300

    geral: book learn go with tests

--- geral/book_learn_go_with_tests/ex025/countdown.go
+++ geral/book_learn_go_with_tests/ex025/countdown.go
@@ -5,15 +5,21 @@ import (
 	"io"
 )
 
+type Sleeper interface {
+	Sleep()
+}
+
 const (
 	countdownStart = 3
 	finalWord      = "GO!"
 )
 
-func Countdown(w io.Writer) {
+func Countdown(w io.Writer, s Sleeper) {
 	for i := countdownStart; i > 0; i-- {
+		s.Sleep()
 		fmt.Fprintf(w, "%d\n", i)
 	}
 
+	s.Sleep()
 	fmt.Fprintln(w, finalWord)
 }
--- geral/book_learn_go_with_tests/ex025/countdown_test.go
+++ geral/book_learn_go_with_tests/ex025/countdown_test.go
@@ -2,13 +2,33 @@ package main
 
 import (
 	"bytes"
+	"reflect"
 	"testing"
 )
 
+const (
+	sleep = "sleep"
+	write = "write"
+)
+
+type SpySleeperWriter struct {
+	Calls []string
+}
+
+func (s *SpySleeperWriter) Sleep() {
+	s.Calls = append(s.Calls, sleep)
+}
+
+func (s *SpySleeperWriter) Write(b []byte) (int, error) {
+	s.Calls = append(s.Calls, write)
+	return 0, nil
+}
+
 func TestCountdown(t *testing.T) {
 	b := &bytes.Buffer{}
+	s := &SpySleeperWriter{}
 
-	Countdown(b)
+	Countdown(b, s)
 
 	got := b.String()
 	want := `3
@@ -20,4 +40,27 @@ GO!
 	if got != want {
 		t.Errorf("got: %s, want: %s", got, want)
 	}
+
+	t.Run("it sleeps before each print", func(t *testing.T) {
+		s := &SpySleeperWriter{}
+
+		Countdown(s, s)
+
+		want := []string{
+			sleep,
+			write,
+			sleep,
+			write,
+			sleep,
+			write,
+			sleep,
+			write,
+		}
+
+		got := s.Calls
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got: %q, want: %q", got, want)
+		}
+	})
 }
