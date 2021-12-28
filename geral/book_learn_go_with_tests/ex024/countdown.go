57b028608f8f0f1c2fe0f3158f7107ff13fcd463
flaviogf <flavio.fernandes6@gmail.com>
2021-12-28 09:41:36 -0300

    geral: book learn go with tests

--- geral/book_learn_go_with_tests/ex024/countdown.go
+++ geral/book_learn_go_with_tests/ex024/countdown.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"io"
+	"os"
+	"time"
 )
 
 const (
@@ -10,10 +12,16 @@ const (
 	finalWord      = "GO!"
 )
 
+func main() {
+	Countdown(os.Stdout)
+}
+
 func Countdown(b io.Writer) {
 	for i := countdownStart; i > 0; i-- {
+		time.Sleep(1 * time.Second)
 		fmt.Fprintf(b, "%d\n", i)
 	}
 
+	time.Sleep(1 * time.Second)
 	fmt.Fprintln(b, finalWord)
 }
