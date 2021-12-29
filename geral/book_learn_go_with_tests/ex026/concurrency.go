75ae07fdd3f878efc180efa87627c9bb5305d46a
flaviogf <flavio.fernandes6@gmail.com>
2021-12-29 09:59:08 -0300

    geral: book learn go with tests

--- geral/book_learn_go_with_tests/ex026/concurrency.go
+++ geral/book_learn_go_with_tests/ex026/concurrency.go
@@ -1,7 +1,37 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+)
+
 type Checker interface {
 	Check(url string) bool
+}
+
+type HttpChecker struct{}
+
+func (hc *HttpChecker) Check(url string) bool {
+	resp, err := http.Get(url)
+
+	if err != nil {
+		return false
+	}
+
+	return resp.StatusCode == 200
+}
+
+func main() {
+	c := &HttpChecker{}
+
+	urls := []string{
+		"https://www.google.com",
+		"https://www.youtube.com",
+	}
+
+	result := CheckWebsites(c, urls)
+
+	fmt.Printf("%v\n", result)
 }
 
 func CheckWebsites(c Checker, urls []string) map[string]bool {
