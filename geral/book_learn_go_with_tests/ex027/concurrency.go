f2d8f1ff3e47e728cc225332881361d74063149b
flaviogf <flavio.fernandes6@gmail.com>
2021-12-30 09:19:54 -0300

    geral: book learn go with tests

--- geral/book_learn_go_with_tests/ex027/concurrency.go
+++ geral/book_learn_go_with_tests/ex027/concurrency.go
@@ -1,7 +1,53 @@
 package main
 
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"strings"
+)
+
+var urlStringVar string
+
 type Checker interface {
 	Check(url string) bool
+}
+
+type HttpChecker struct{}
+
+func (hc *HttpChecker) Check(url string) bool {
+	resp, err := http.Get(url)
+
+	if err != nil {
+		return false
+	}
+
+	return resp.StatusCode == 200
+}
+
+func init() {
+	flag.StringVar(&urlStringVar, "url", "", "a comma-separate list of URLs")
+}
+
+func main() {
+	flag.Parse()
+
+	if urlStringVar == "" {
+		log.Fatal("you must specify at least one URL")
+	}
+
+	urls := strings.Split(urlStringVar, ",")
+
+	if len(urls) == 0 {
+		log.Fatal("you must specify at least one URL")
+	}
+
+	c := &HttpChecker{}
+
+	result := CheckWebsites(c, urls)
+
+	fmt.Printf("%v\n", result)
 }
 
 func CheckWebsites(c Checker, urls []string) map[string]bool {
