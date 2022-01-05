e4c75e8cdacda6fefdac32a1ffeb931e84d7f4f2
flaviogf <flavio.fernandes6@gmail.com>
2022-01-05 10:40:18 -0300

    geral: book network programming with go

---
+++ geral/book_network_programming_with_go/ex003/main_test.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"context"
+	"net"
+	"syscall"
+	"testing"
+	"time"
+)
+
+func TestDialContext(t *testing.T) {
+	ctx, cancel := context.WithDeadline(context.Background(), time.Now().Add(4*time.Millisecond))
+
+	defer cancel()
+
+	var d net.Dialer
+
+	d.Control = func(_, _ string, _ syscall.RawConn) error {
+		time.Sleep(5 * time.Millisecond)
+		return nil
+	}
+
+	_, err := d.DialContext(ctx, "tcp", ":8080")
+
+	if err == nil {
+		t.Fatal("did not get an error, but would want one")
+	}
+}
