24c56e1f4288618c977bf8e5b30e1855d59a328d
flaviogf <flavio.fernandes6@gmail.com>
2022-01-09 15:02:17 -0300

    geral: book network programming with go

---
+++ geral/book_network_programming_with_go/ex011/go.mod
@@ -0,0 +1,3 @@
+module github.com/flaviogf/courses/geral/book_network_programming_with_go/ex011
+
+go 1.17
---
+++ geral/book_network_programming_with_go/ex011/main_test.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+type User struct {
+	FirstName string
+	LastName  string
+}
+
+func TestPostUser(t *testing.T) {
+	s := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusAccepted)
+	}))
+
+	defer s.Close()
+
+	buf := &bytes.Buffer{}
+	user := &User{"Frank", "Castle"}
+	err := json.NewEncoder(buf).Encode(user)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	resp, err := http.Post(s.URL, "application/json", buf)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if resp.StatusCode != http.StatusAccepted {
+		t.Errorf("got: %d, want: %d", resp.StatusCode, http.StatusAccepted)
+	}
+}
