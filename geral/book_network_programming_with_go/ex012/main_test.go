d5489959519c634a5557dccdf634587e448d1cfa
flaviogf <flavio.fernandes6@gmail.com>
2022-01-09 15:51:42 -0300

    geral: book network programming with go

--- geral/book_network_programming_with_go/ex012/main_test.go
+++ geral/book_network_programming_with_go/ex012/main_test.go
@@ -2,9 +2,13 @@ package main
 
 import (
 	"bytes"
+	"fmt"
+	"io"
 	"io/ioutil"
 	"mime/multipart"
 	"net/http"
+	"os"
+	"path/filepath"
 	"testing"
 	"time"
 )
@@ -18,6 +22,35 @@ func TestMultipartPost(t *testing.T) {
 		"description": "Form values with attached files",
 	} {
 		w.WriteField(k, v)
+	}
+
+	for i, file := range []string{
+		"./hello.txt",
+		"./goodbye.txt",
+	} {
+		filePart, err := w.CreateFormFile(fmt.Sprintf("file%d", i+1), filepath.Base(file))
+
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		f, err := os.Open(file)
+
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		_, err = io.Copy(filePart, f)
+
+		if err != nil {
+			t.Fatal(err)
+		}
+	}
+
+	err := w.Close()
+
+	if err != nil {
+		t.Fatal(err)
 	}
 
 	req, err := http.NewRequest(http.MethodPost, "https://httpbin.org/post", body)
