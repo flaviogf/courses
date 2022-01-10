a59c4efa9a2b37c8df8db73409fc9e1def317f6c
flaviogf <flavio.fernandes6@gmail.com>
2022-01-10 11:56:51 -0300

    geral: book cloud native go

--- geral/book_cloud_native_go/ex001/circuit_breaker.go
+++ geral/book_cloud_native_go/ex001/circuit_breaker.go
@@ -1,11 +1,51 @@
 package main
 
-import "context"
+import (
+	"context"
+	"errors"
+	"sync"
+	"time"
+)
+
+var ErrServiceUnreachable = errors.New("service unreachable")
 
 type Circuit func(ctx context.Context) (string, error)
 
 func Breaker(circuit Circuit, failureThreshold int) Circuit {
+	consecutiveFailures := 0
+	lastAttempt := time.Now()
+	m := &sync.RWMutex{}
+
 	return func(ctx context.Context) (string, error) {
-		return circuit(ctx)
+		m.RLock()
+
+		diff := consecutiveFailures - failureThreshold
+
+		if diff >= 0 {
+			shouldRetryAt := lastAttempt.Add(5 * time.Second)
+
+			if time.Now().Before(shouldRetryAt) {
+				m.RUnlock()
+				return "", ErrServiceUnreachable
+			}
+		}
+
+		m.RUnlock()
+
+		resp, err := circuit(ctx)
+
+		m.Lock()
+		defer m.Unlock()
+
+		lastAttempt = time.Now()
+
+		if err != nil {
+			consecutiveFailures++
+			return resp, err
+		}
+
+		consecutiveFailures = 0
+
+		return resp, err
 	}
 }
--- geral/book_cloud_native_go/ex001/circuit_breaker_test.go
+++ geral/book_cloud_native_go/ex001/circuit_breaker_test.go
@@ -2,19 +2,60 @@ package main
 
 import (
 	"context"
+	"errors"
+	"reflect"
 	"testing"
+	"time"
 )
 
+var ErrOps = errors.New("ops...")
+
+type Result struct {
+	resp string
+	err  error
+}
+
 func TestBreaker(t *testing.T) {
+	n := 0
+
 	doubleCircuit := func(ctx context.Context) (string, error) {
-		return "", nil
+		n++
+
+		if n < 6 {
+			return "", ErrOps
+		}
+
+		return "OK", nil
 	}
 
 	breaker := Breaker(doubleCircuit, 5)
 
-	_, err := breaker(context.Background())
+	results := []Result{}
 
-	if err != nil {
-		t.Error("did not want an error, but got one")
+	for i := 0; i < 10; i++ {
+		resp, err := breaker(context.Background())
+
+		results = append(results, Result{resp, err})
+
+		if err == ErrServiceUnreachable {
+			time.Sleep(5 * time.Second)
+		}
+	}
+
+	expectedResults := []Result{
+		Result{"", ErrOps},
+		Result{"", ErrOps},
+		Result{"", ErrOps},
+		Result{"", ErrOps},
+		Result{"", ErrOps},
+		Result{"", ErrServiceUnreachable},
+		Result{"OK", nil},
+		Result{"OK", nil},
+		Result{"OK", nil},
+		Result{"OK", nil},
+	}
+
+	if !reflect.DeepEqual(results, expectedResults) {
+		t.Errorf("got: %v, want: %v", results, expectedResults)
 	}
 }
