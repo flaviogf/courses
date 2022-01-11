13f3fa3a348a4a6c64cdcae05fdc628143d33e38
flaviogf <flavio.fernandes6@gmail.com>
2022-01-11 11:11:46 -0300

    book: cloud native go

--- geral/book_cloud_native_go/ex002/debounce.go
+++ geral/book_cloud_native_go/ex002/debounce.go
@@ -2,13 +2,32 @@ package main
 
 import (
 	"context"
+	"sync"
 	"time"
 )
 
 type Circuit func(ctx context.Context) (string, error)
 
 func Debounce(circuit Circuit, d time.Duration) Circuit {
+	var threshold time.Time
+	var result string
+	var err error
+	var m sync.RWMutex
+
 	return func(ctx context.Context) (string, error) {
-		return circuit(ctx)
+		m.Lock()
+
+		defer func() {
+			threshold = time.Now().Add(d)
+			m.Unlock()
+		}()
+
+		if time.Now().Before(threshold) {
+			return result, err
+		}
+
+		result, err = circuit(ctx)
+
+		return result, err
 	}
 }
--- geral/book_cloud_native_go/ex002/debounce_test.go
+++ geral/book_cloud_native_go/ex002/debounce_test.go
@@ -2,20 +2,57 @@ package main
 
 import (
 	"context"
+	"fmt"
+	"reflect"
 	"testing"
 	"time"
 )
 
 func TestDebounce(t *testing.T) {
+	n := 0
+
 	doubleCircuit := func(ctx context.Context) (string, error) {
-		return "OK", nil
+		n++
+		return fmt.Sprintf("%d", n), nil
 	}
 
-	debounce := Debounce(doubleCircuit, 5*time.Second)
+	debounce := Debounce(doubleCircuit, 1*time.Second)
 
-	_, err := debounce(context.Background())
+	results := []string{}
+	results = append(results, execute(t, debounce, 5)...)
+	time.Sleep(1 * time.Second)
+	results = append(results, execute(t, debounce, 5)...)
 
-	if err != nil {
-		t.Error("did not want an error, but got one")
+	expectedResults := []string{
+		"1",
+		"1",
+		"1",
+		"1",
+		"1",
+		"2",
+		"2",
+		"2",
+		"2",
+		"2",
 	}
+
+	if !reflect.DeepEqual(results, expectedResults) {
+		t.Errorf("want: %v, got: %v", expectedResults, results)
+	}
+}
+
+func execute(t testing.TB, debounce func(context.Context) (string, error), times int) []string {
+	results := []string{}
+
+	for i := 0; i < times; i++ {
+		resp, err := debounce(context.TODO())
+
+		if err != nil {
+			t.Errorf("#%d error: %v", i+1, err)
+		}
+
+		results = append(results, resp)
+	}
+
+	return results
 }
