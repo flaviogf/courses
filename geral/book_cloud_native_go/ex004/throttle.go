4b3717c62b5983feafc46d6ddf6e59e251323f42
flaviogf <flavio.fernandes6@gmail.com>
2022-01-12 17:03:01 -0300

    book: cloud native go

--- geral/book_cloud_native_go/ex004/throttle.go
+++ geral/book_cloud_native_go/ex004/throttle.go
@@ -2,13 +2,47 @@ package main
 
 import (
 	"context"
+	"errors"
+	"sync"
 	"time"
 )
 
+var ErrTooManyCalls = errors.New("too many calls")
+
 type Effector func(ctx context.Context) (string, error)
 
-func Throttle(effector Effector, max, refil int, d time.Duration) Effector {
+func Throttle(effector Effector, max, refil int, delay time.Duration) Effector {
+	tokens := max
+	once := sync.Once{}
+
 	return func(ctx context.Context) (string, error) {
+		once.Do(func() {
+			ticker := time.NewTicker(delay)
+
+			go func(ctx context.Context) {
+				for {
+					select {
+					case <-ctx.Done():
+						return
+					case <-ticker.C:
+						t := tokens + refil
+
+						if t > max {
+							t = max
+						}
+
+						tokens = t
+					}
+				}
+			}(ctx)
+		})
+
+		if tokens <= 0 {
+			return "", ErrTooManyCalls
+		}
+
+		tokens--
+
 		return effector(ctx)
 	}
 }
--- geral/book_cloud_native_go/ex004/throttle_test.go
+++ geral/book_cloud_native_go/ex004/throttle_test.go
@@ -2,9 +2,15 @@ package main
 
 import (
 	"context"
+	"reflect"
 	"testing"
 	"time"
 )
+
+type Result struct {
+	resp string
+	err  error
+}
 
 func TestThrottle(t *testing.T) {
 	doubleEffector := func(ctx context.Context) (string, error) {
@@ -13,12 +19,28 @@ func TestThrottle(t *testing.T) {
 
 	throttle := Throttle(doubleEffector, 5, 1, 1*time.Second)
 
-	results := make([]string, 10)
+	results := make([]Result, 10)
 
 	for i := 0; i < 10; i++ {
-		resp, _ := throttle(context.TODO())
-		results[i] = resp
+		resp, err := throttle(context.TODO())
+		r := Result{resp, err}
+		results[i] = r
 	}
 
-	t.Log(results)
+	expectedResults := []Result{
+		Result{"OK", nil},
+		Result{"OK", nil},
+		Result{"OK", nil},
+		Result{"OK", nil},
+		Result{"OK", nil},
+		Result{"", ErrTooManyCalls},
+		Result{"", ErrTooManyCalls},
+		Result{"", ErrTooManyCalls},
+		Result{"", ErrTooManyCalls},
+		Result{"", ErrTooManyCalls},
+	}
+
+	if !reflect.DeepEqual(results, expectedResults) {
+		t.Errorf("want: %v, got: %v", expectedResults, results)
+	}
 }
