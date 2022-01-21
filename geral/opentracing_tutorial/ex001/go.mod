5a222b9c2a5b632f975818c0de64a7f15cd93261
flaviogf <flavio.fernandes6@gmail.com>
2022-01-21 13:45:19 -0300

    opentracing-tutorial

--- geral/opentracing_tutorial/ex001/go.mod
+++ geral/opentracing_tutorial/ex001/go.mod
@@ -1,3 +1,13 @@
 module github.com/flaviogf/courses/geral/opentracing_tutorial/ex001
 
 go 1.17
+
+require (
+	github.com/go-logr/logr v1.2.2 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
+	go.opentelemetry.io/otel v1.3.0 // indirect
+	go.opentelemetry.io/otel/exporters/jaeger v1.3.0 // indirect
+	go.opentelemetry.io/otel/sdk v1.3.0 // indirect
+	go.opentelemetry.io/otel/trace v1.3.0 // indirect
+	golang.org/x/sys v0.0.0-20220114195835-da31bd327af9 // indirect
+)
--- geral/opentracing_tutorial/ex001/main.go
+++ geral/opentracing_tutorial/ex001/main.go
@@ -4,12 +4,36 @@ import (
 	"fmt"
 	"log"
 	"os"
+
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/exporters/jaeger"
+	"go.opentelemetry.io/otel/sdk/resource"
+	tracesdk "go.opentelemetry.io/otel/sdk/trace"
+	semconv "go.opentelemetry.io/otel/semconv/v1.4.0"
 )
 
 func main() {
 	if len(os.Args[1:]) == 0 {
 		log.Fatalln("you must specify a name")
 	}
+
+	exp, err := jaeger.New(jaeger.WithCollectorEndpoint(jaeger.WithEndpoint(os.Getenv("JAGER_ENDPOINT"))))
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	res := resource.NewWithAttributes(
+		semconv.SchemaURL,
+		semconv.ServiceNameKey.String(os.Getenv("SERVICE_NAME")),
+	)
+
+	tp := tracesdk.NewTracerProvider(
+		tracesdk.WithBatcher(exp),
+		tracesdk.WithResource(res),
+	)
+
+	otel.SetTracerProvider(tp)
 
 	sayHello(os.Args[1])
 }
