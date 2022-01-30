23401f636bbc6e78eceb83004d47b6267cbd0d15
flaviogf <flavio.fernandes6@gmail.com>
2022-01-30 09:45:58 -0500

    geral: book mastering distributed tracing

--- geral/book_mastering_distributed_tracing/ex002/big_brother.go
+++ geral/book_mastering_distributed_tracing/ex002/big_brother.go
@@ -3,8 +3,13 @@ package main
 import (
 	"database/sql"
 	"fmt"
+	"log"
+	"net"
+	"net/http"
 	"os"
+	"time"
 
+	"github.com/gorilla/mux"
 	_ "github.com/lib/pq"
 )
 
@@ -12,14 +17,34 @@ func main() {
 	db, err := sql.Open(os.Getenv("DB_DRIVER"), os.Getenv("DB_DATASOURCE"))
 
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
 
 	err = db.Ping()
 
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
 
-	fmt.Println("It works")
+	r := mux.NewRouter()
+
+	r.HandleFunc("/people/{name}", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "It works")
+	})
+
+	l, err := net.Listen("tcp", "0.0.0.0:80")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	s := http.Server{
+		Handler:           r,
+		IdleTimeout:       time.Minute,
+		ReadHeaderTimeout: 5 * time.Second,
+	}
+
+	if err := s.Serve(l); err != nil {
+		log.Fatal(err)
+	}
 }
--- geral/book_mastering_distributed_tracing/ex002/go.mod
+++ geral/book_mastering_distributed_tracing/ex002/go.mod
@@ -2,4 +2,7 @@ module github.com/courses/geral/book_mastering_distributed_tracing/ex002
 
 go 1.17
 
-require github.com/lib/pq v1.10.4
+require (
+	github.com/gorilla/mux v1.8.0
+	github.com/lib/pq v1.10.4
+)
