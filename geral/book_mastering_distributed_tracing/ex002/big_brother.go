29840ebb7de4b2e08b3b49d048241a139169f898
flaviogf <flavio.fernandes6@gmail.com>
2022-01-30 08:59:00 -0500

    geral: book mastering distributed tracing

--- geral/book_mastering_distributed_tracing/ex002/big_brother.go
+++ geral/book_mastering_distributed_tracing/ex002/big_brother.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/lib/pq"
+)
 
 func main() {
+	db, err := sql.Open("postgres", "")
+
+	if err != nil {
+		panic(err)
+	}
+
+	err = db.Ping()
+
+	if err != nil {
+		panic(err)
+	}
+
 	fmt.Println("It works")
 }
--- geral/book_mastering_distributed_tracing/ex002/go.mod
+++ geral/book_mastering_distributed_tracing/ex002/go.mod
@@ -1,3 +1,5 @@
 module github.com/courses/geral/book_mastering_distributed_tracing/ex002
 
 go 1.17
+
+require github.com/lib/pq v1.10.4
