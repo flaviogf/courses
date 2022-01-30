618b5a7d3fc0afebe4fb1ae6102446c6cd1d843f
flaviogf <flavio.fernandes6@gmail.com>
2022-01-30 09:05:01 -0500

    geral: book mastering distributed tracing

--- geral/book_mastering_distributed_tracing/ex002/big_brother.go
+++ geral/book_mastering_distributed_tracing/ex002/big_brother.go
@@ -3,12 +3,13 @@ package main
 import (
 	"database/sql"
 	"fmt"
+	"os"
 
 	_ "github.com/lib/pq"
 )
 
 func main() {
-	db, err := sql.Open("postgres", "")
+	db, err := sql.Open(os.Getenv("DB_DRIVER"), os.Getenv("DB_DATASOURCE"))
 
 	if err != nil {
 		panic(err)
