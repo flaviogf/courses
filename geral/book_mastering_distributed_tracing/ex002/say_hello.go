3e9982c704429435aca86932dfcabd80531bdc94
flaviogf <flavio.fernandes6@gmail.com>
2022-02-02 11:49:07 -0500

    geral: book mastering distributed tracing

--- geral/book_mastering_distributed_tracing/ex002/say_hello.go
+++ geral/book_mastering_distributed_tracing/ex002/say_hello.go
@@ -1,7 +1,57 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"log"
+	"net"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/gorilla/mux"
+)
 
 func main() {
-	fmt.Println("It works")
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	l, err := net.Listen("tcp", "0.0.0.0:80")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	r := mux.NewRouter()
+
+	r.Handle("/", &SayHelloHandler{})
+
+	s := http.Server{
+		Handler:           r,
+		IdleTimeout:       time.Minute,
+		ReadHeaderTimeout: 5 * time.Second,
+	}
+
+	go func() {
+		if err := s.Serve(l); err != nil {
+			log.Println(err)
+		}
+	}()
+
+	signalCh := make(chan os.Signal, 1)
+	signal.Notify(signalCh, syscall.SIGTERM, syscall.SIGINT)
+
+	<-signalCh
+
+	s.Shutdown(ctx)
+
+	log.Println("Server finished")
+}
+
+type SayHelloHandler struct{}
+
+func (s *SayHelloHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "It works")
 }
