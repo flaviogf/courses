201d556ed51809507ceb655e670eaf0754e5cc0d
flaviogf <flavio.fernandes6@gmail.com>
2022-03-14 13:06:04 -0300

    geral: book ruby performance optimization

---
+++ geral/ruby_performance_optimization/ex001/optimized.rb
@@ -0,0 +1,14 @@
+require 'benchmark'
+
+GC.disable
+
+number_of_rows = 5999
+number_of_columns = 100
+
+data = Array.new(number_of_rows) { Array.new(number_of_columns) { 'x' * 1000 } }
+
+time = Benchmark.realtime do
+  _ = data.collect { |row| row.join(',') }.join('\n')
+end
+
+puts time.round(2)
