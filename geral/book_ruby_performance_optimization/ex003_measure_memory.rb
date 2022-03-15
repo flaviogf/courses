9d7cc85e68000e783c86a1a48ff0fd76024a365c
flaviogf <flavio.fernandes6@gmail.com>
2022-03-15 13:39:06 -0300

    geral: book ruby performance optimization

--- geral/book_ruby_performance_optimization/ex003_measure_memory.rb
+++ geral/book_ruby_performance_optimization/ex003_measure_memory.rb
@@ -5,8 +5,12 @@ num_cols = 10
 
 data = Array.new(num_rows) { Array.new(num_cols) { 'x' * 1000 } }
 
+puts "#{`ps -o rss= -p #{Process.pid}`.to_i / 1024} MB"
+
 time = Benchmark.realtime do
   _ = data.map { |row| row.join(',') }.join('\n')
 end
+
+puts "#{`ps -o rss= -p #{Process.pid}`.to_i / 1024} MB"
 
 puts time.round(2)
