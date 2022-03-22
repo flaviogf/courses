e4ae0b03b1b713e24a50af8fff032af637439e5e
flaviogf <flavio.fernandes6@gmail.com>
2022-03-22 13:12:06 -0300

    geral: book ruby performance optimization

---
+++ geral/book_ruby_performance_optimization/ex005.rb
@@ -0,0 +1,9 @@
+require 'csv'
+
+data = CSV.open('data.csv')
+
+output = data.readlines.map do |line|
+  line.map { |col| col.downcase.gsub(/\b('?[a-z])/) { $1.capitalize } }
+end
+
+File.open('output.csv', 'w+') { |f| f.write(output.join("\n")) }
