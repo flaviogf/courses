19e5b50a298586f455fc8a89ba434ea1d74018d7
flaviogf <flavio.fernandes6@gmail.com>
2022-04-18 13:06:44 -0300

    geral: book distributed service with go

---
+++ geral/book_distributed_service_with_go/chapter_001/proglog/go.mod
@@ -0,0 +1,3 @@
+module github.com/flaviogf/proglog
+
+go 1.18
---
+++ geral/book_distributed_service_with_go/chapter_001/proglog/internal/server/log.go
@@ -0,0 +1,26 @@
+package server
+
+import "sync"
+
+type Log struct {
+	mu      sync.Mutex
+	records []Record
+}
+
+func NewLog() *Log {
+	return &Log{}
+}
+
+func (c *Log) Append(record Record) (uint64, error) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	record.Offset = uint64(len(c.records))
+	c.records = append(c.records, record)
+
+	return record.Offset, nil
+}
+
+type Record struct {
+	Offset uint64
+}
