65e9e6116acf30018fb282849e63afc0c9f94541
flaviogf <flavio.fernandes6@gmail.com>
2022-04-24 14:22:48 -0300

    geral: bigtable get started

--- geral/bigtable_get_started/main.go
+++ geral/bigtable_get_started/main.go
@@ -1,11 +1,41 @@
 package main
 
 import (
-	"fmt"
+	"context"
+	"log"
 
-	_ "cloud.google.com/go/bigtable"
+	"cloud.google.com/go/bigtable"
 )
 
 func main() {
-	fmt.Println("It works")
+	project := "project"
+	instance := "instance"
+	tableName := "hello-world"
+
+	ctx := context.Background()
+	adminClient, err := bigtable.NewAdminClient(ctx, project, instance)
+
+	if err != nil {
+		log.Fatalf("Could not create admin client: %v\n", err)
+	}
+
+	tables, err := adminClient.Tables(ctx)
+
+	if err != nil {
+		log.Fatalf("Could not fetch table list: %v\n", err)
+	}
+
+	if !sliceContains(tables, tableName) {
+		log.Printf("Creating table: %v\n", tableName)
+	}
+}
+
+func sliceContains(tables []string, tableName string) bool {
+	for _, table := range tables {
+		if table == tableName {
+			return true
+		}
+	}
+
+	return false
 }
