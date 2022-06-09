eb727752ceda0ab04f54b84849f65adca27125c0
flaviogf <flavio.fernandes6@gmail.com>
2022-06-09 13:36:28 -0300

    geral: practical common lisp

---
+++ geral/practical_common_lisp/002/example_010.lisp
@@ -0,0 +1,26 @@
+(defun make-cd (artist title rating ripped)
+  (list :artist artist :title title :rating rating :ripped ripped))
+
+(defvar *db* nil)
+
+(defun append-record (cd)
+  (push cd *db*))
+
+(defun dump-db ()
+  (format t "~{~{~a:~10T~a~%~}~%~}" *db*))
+
+(defun prompt-read (field)
+  (format *query-io* "~a: " field)
+  (finish-output *query-io*)
+  (read-line *query-io*))
+
+(defun prompt-for-cd ()
+  (make-cd
+   (prompt-read "artist")
+   (prompt-read "title")
+   (or (parse-integer (prompt-read "rating") :junk-allowed t) 0)
+   (yes-or-no-p "ripped?")))
+
+(defun add-cds ()
+  (loop (append-record (prompt-for-cd))
+   (unless (yes-or-no-p "another?") (return))))
