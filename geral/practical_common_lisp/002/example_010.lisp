87c71f7c2e64250e6c783a3c65dcf479b909a22a
flaviogf <flavio.fernandes6@gmail.com>
2022-06-13 17:13:53 -0300

    geral: practical common lisp

--- geral/practical_common_lisp/002/example_010.lisp
+++ geral/practical_common_lisp/002/example_010.lisp
@@ -28,3 +28,7 @@
 (defun save-db (filename)
   (with-open-file (out filename :direction :output :if-exists :supersede)
     (with-standard-io-syntax (print *db* out))))
+
+(defun load-db (filename)
+  (with-open-file (in filename)
+    (with-standard-io-syntax (setf *db* (read in)))))
