47b931f561c5af41b85e74e98b948bd1c7dd2401
flaviogf <flavio.fernandes6@gmail.com>
2022-06-18 01:30:58 -0300

    geral: practical common lisp

--- geral/practical_common_lisp/002/example_013.lisp
+++ geral/practical_common_lisp/002/example_013.lisp
@@ -11,7 +11,11 @@
 (defun print-cds (cds)
   (dolist (cd cds) (print-cd cd)))
 
+(defun select-by-artist (cds artist)
+  (remove-if-not (lambda (cd) (equal (getf cd :artist) artist)) cds))
+
 (let ((cds nil))
   (push (new-cd "Linkin Park" "Meteora" 10 t) cds)
   (push (new-cd "Guns N' Roses" "Appetite for Destruction" 10 t) cds)
-  (print-cds cds))
+  (print-cds cds)
+  (print-cds (select-by-artist cds "Guns N' Roses")))
