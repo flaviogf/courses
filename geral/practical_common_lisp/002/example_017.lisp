e0fa34361c89fdd6f7606774ca5491bebab6e7e0
flaviogf <flavio.fernandes6@gmail.com>
2022-06-19 17:11:08 -0300

    geral: practical common lisp

--- geral/practical_common_lisp/002/example_017.lisp
+++ geral/practical_common_lisp/002/example_017.lisp
@@ -9,8 +9,13 @@
                 (if artist (equal (getf cd :artist) artist) t)
                 (if title (equal (getf cd :title) title) t))))
 
+(defun dump (cds)
+  (with-open-file (file "example_017.db" :direction :output :if-exists :supersede)
+    (with-standard-io-syntax (print cds file))))
+
 (let ((cds nil))
   (push (new-cd "Linkin Park" "In the end") cds)
   (push (new-cd "Guns N' Roses" "Welcome to the jungle") cds)
   (select cds (where :artist "Linkin Park"))
-  (select cds (where :title "Welcome to the jungle")))
+  (select cds (where :title "Welcome to the jungle"))
+  (dump cds))
