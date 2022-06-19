403c874c7f3c3f5059dcd2a122c8d627f1df67a9
flaviogf <flavio.fernandes6@gmail.com>
2022-06-19 16:48:56 -0300

    geral: practical common lisp

--- geral/practical_common_lisp/002/example_017.lisp
+++ geral/practical_common_lisp/002/example_017.lisp
@@ -1,13 +1,16 @@
-(defun new-cd (artist)
-  (list :artist artist))
+(defun new-cd (artist title)
+  (list :artist artist :title title))
 
 (defun select (cds predicate)
   (remove-if-not predicate cds))
 
-(defun where (&key artist)
-  (lambda (cd) (if artist (equal (getf cd :artist) artist) t)))
+(defun where (&key artist title)
+  (lambda (cd) (and
+                (if artist (equal (getf cd :artist) artist) t)
+                (if title (equal (getf cd :title) title) t))))
 
 (let ((cds nil))
-  (push (new-cd "Linkin Park") cds)
-  (push (new-cd "Guns N' Roses") cds)
-  (select cds (where :artist "Linkin Park")))
+  (push (new-cd "Linkin Park" "In the end") cds)
+  (push (new-cd "Guns N' Roses" "Welcome to the jungle") cds)
+  (select cds (where :artist "Linkin Park"))
+  (select cds (where :title "Welcome to the jungle")))
