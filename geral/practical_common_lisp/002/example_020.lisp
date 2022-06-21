c33444e71d9a69534468d5075fd4f2aadb708043
flaviogf <flavio.fernandes6@gmail.com>
2022-06-21 13:13:18 -0300

    geral: practical common lisp

--- geral/practical_common_lisp/002/example_020.lisp
+++ geral/practical_common_lisp/002/example_020.lisp
@@ -1,3 +1,8 @@
 (defmacro backwards (expr) (reverse expr))
 
 (backwards ("hello, world" t format))
+
+(defun make-comparison-expr (field value)
+  (list 'equal (list 'getf field) value))
+
+(make-comparison-expr :artist "Frank")
