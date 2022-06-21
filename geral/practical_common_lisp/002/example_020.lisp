660903b1c269735559fadb2f5937c4c80aee9cb7
flaviogf <flavio.fernandes6@gmail.com>
2022-06-21 13:29:32 -0300

    geral: practical common lisp

--- geral/practical_common_lisp/002/example_020.lisp
+++ geral/practical_common_lisp/002/example_020.lisp
@@ -6,3 +6,13 @@
   `(equal (getf cd ,field) ,value))
 
 (make-comparison-expr :artist "Frank")
+
+(defmacro where (field value)
+  `(lambda (cd) ,(make-comparison-expr field value)))
+
+(defun new-cd (artist)
+  (list :artist artist))
+
+(let ((cd (new-cd "Frank")))
+  (print (funcall (where :artist "Wrong") cd))
+  (print (funcall (where :artist "Frank") cd)))
