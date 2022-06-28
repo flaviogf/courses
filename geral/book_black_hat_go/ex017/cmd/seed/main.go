b372ebfa874b254005d93d193a3085e2f32e9f85
flaviogf <flavio.fernandes6@gmail.com>
2022-06-28 13:24:54 -0300

    geral: black hat go

---
+++ geral/book_black_hat_go/ex017/cmd/seed/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"context"
+	"log"
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func main() {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://root:example@localhost:27017"))
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	collection := client.Database("store").Collection("transcations")
+
+	docs := []interface{}{
+		bson.D{
+			{"ccnum", "4444333322221111"},
+			{"date", "2019-01-05"},
+			{"amount", 100.12},
+			{"cvv", "1234"},
+			{"exp", "09/2020"},
+		},
+		bson.D{
+			{"ccnum", "4444123456789012"},
+			{"date", "2019-01-07"},
+			{"amount", 2400.18},
+			{"cvv", "5544"},
+			{"exp", "02/2021"},
+		},
+		bson.D{
+			{"ccnum", "4465122334455667"},
+			{"date", "2019-01-29"},
+			{"amount", 1450.87},
+			{"cvv", "9876"},
+			{"exp", "06/2020"},
+		},
+	}
+
+	_, err = collection.InsertMany(ctx, docs)
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	log.Println("Database seeded")
+}
