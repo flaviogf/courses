f68196af6fd397c6d41f6d41a23eae81d16f640e
flaviogf <flavio.fernandes6@gmail.com>
2022-07-04 13:23:26 -0300

    geral: go black hat

--- geral/book_black_hat_go/ex017/dbminer/dbminer.go
+++ geral/book_black_hat_go/ex017/dbminer/dbminer.go
@@ -1,5 +1,10 @@
 package dbminer
 
+import (
+	"fmt"
+	"regexp"
+)
+
 type DatabaseMiner interface {
 	GetSchema() (*Schema, error)
 }
@@ -14,6 +19,44 @@ type Database struct {
 }
 
 type Table struct {
-	Name   string
-	Column []string
+	Name    string
+	Columns []string
+}
+
+func Search(m DatabaseMiner) error {
+	s, err := m.GetSchema()
+
+	if err != nil {
+		return err
+	}
+
+	regex := getRegex()
+
+	for _, db := range s.Databases {
+		for _, table := range db.Tables {
+			for _, column := range table.Columns {
+				for _, r := range regex {
+					if r.MatchString(column) {
+						fmt.Println(db)
+						fmt.Printf("[+] HIT: %s\n", column)
+					}
+				}
+			}
+		}
+	}
+
+	return nil
+}
+
+func getRegex() []*regexp.Regexp {
+	return []*regexp.Regexp{
+		regexp.MustCompile("(?i)social"),
+		regexp.MustCompile("(?i)ssn"),
+		regexp.MustCompile("(?i)pass(word)?"),
+		regexp.MustCompile("(?i)hash"),
+		regexp.MustCompile("(?i)ccnum"),
+		regexp.MustCompile("(?i)card"),
+		regexp.MustCompile("(?i)security"),
+		regexp.MustCompile("(?i)key"),
+	}
 }
