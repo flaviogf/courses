959c00a0b5f97e1b06abeb7dde8de187d0b1637c
flaviogf <flavio.fernandes6@gmail.com>
2022-07-04 13:42:02 -0300

    geral: go black hat

--- geral/book_black_hat_go/ex017/dbminer/mongo.go
+++ geral/book_black_hat_go/ex017/dbminer/mongo.go
@@ -1,5 +1,14 @@
 package dbminer
 
+import (
+	"context"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
+)
+
 type MongoMiner struct {
 }
 
@@ -9,6 +18,19 @@ func NewMongoMiner() *MongoMiner {
 
 func (m *MongoMiner) GetSchema() (*Schema, error) {
 	var result Schema
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://root:example@localhost:27017"))
+
+	if err != nil {
+		return nil, err
+	}
+
+	if err = client.Ping(ctx, readpref.Primary()); err != nil {
+		return nil, err
+	}
 
 	return &result, nil
 }
--- geral/book_black_hat_go/ex017/go.mod
+++ geral/book_black_hat_go/ex017/go.mod
@@ -2,6 +2,8 @@ module gitub.com/flaviogf/courses/geral/book_black_hat_go/ex017
 
 go 1.18
 
+require go.mongodb.org/mongo-driver v1.9.1
+
 require (
 	github.com/go-stack/stack v1.8.0 // indirect
 	github.com/golang/snappy v0.0.1 // indirect
@@ -11,7 +13,6 @@ require (
 	github.com/xdg-go/scram v1.0.2 // indirect
 	github.com/xdg-go/stringprep v1.0.2 // indirect
 	github.com/youmark/pkcs8 v0.0.0-20181117223130-1be2e3e5546d // indirect
-	go.mongodb.org/mongo-driver v1.9.1 // indirect
 	golang.org/x/crypto v0.0.0-20201216223049-8b5274cf687f // indirect
 	golang.org/x/sync v0.0.0-20190911185100-cd5d95a43a6e // indirect
 	golang.org/x/text v0.3.5 // indirect
