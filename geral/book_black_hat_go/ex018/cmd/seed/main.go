7d31492f21df8ce2c6edff3d2e3dabe58503b84e
flaviogf <flavio.fernandes6@gmail.com>
2022-07-12 14:14:20 -0300

    geral: black hat go

--- geral/book_black_hat_go/ex018/cmd/seed/main.go
+++ geral/book_black_hat_go/ex018/cmd/seed/main.go
@@ -1,12 +1,21 @@
 package main
 
 import (
+	"context"
 	"database/sql"
-	"fmt"
 	"log"
+	"time"
 
 	_ "github.com/lib/pq"
 )
+
+type Transaction struct {
+	CCNum  string
+	Date   time.Time
+	Amount uint32
+	CVV    string
+	Exp    time.Time
+}
 
 func main() {
 	connStr := "user=postgres password=postgres dbname=store sslmode=disable"
@@ -20,5 +29,47 @@ func main() {
 		log.Fatalln(err)
 	}
 
-	fmt.Println("It works")
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	conn, err := db.Conn(ctx)
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	const format = "2006-01-02"
+
+	date, _ := time.Parse(format, "2019-01-05")
+	exp, _ := time.Parse(format, "2020-09-01")
+
+	transactions := []Transaction{
+		Transaction{
+			"4444333322221111",
+			date,
+			10012,
+			"1234",
+			exp,
+		},
+	}
+
+	query := "INSERT INTO transactions(ccnum, date, amount, cvv, exp) VALUES ($1, $2, $3, $4, $5)"
+
+	for _, t := range transactions {
+		result, err := conn.ExecContext(ctx, query, t.CCNum, t.Date, t.Amount, t.CVV, t.Exp)
+
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		rows, err := result.RowsAffected()
+
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		if rows != 1 {
+			log.Fatalf("expected single row affected, got: %d rows affected\n", rows)
+		}
+	}
 }
--- geral/book_black_hat_go/ex018/data/structure.sql
+++ geral/book_black_hat_go/ex018/data/structure.sql
@@ -1,1 +1,10 @@
 CREATE DATABASE store;
+
+CREATE TABLE transactions (
+  id SERIAL PRIMARY KEY,
+  ccnum VARCHAR(32),
+  date DATE,
+  amount MONEY,
+  cvv CHAR(2),
+  exp DATE
+);
