f08912d9433e5e54e050e1cf602f9433c1310c75
flaviogf <flavio.fernandes6@gmail.com>
2022-07-12 13:50:51 -0300

    geral: black hat go

--- geral/book_black_hat_go/ex018/cmd/seed/main.go
+++ geral/book_black_hat_go/ex018/cmd/seed/main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"database/sql"
+	"fmt"
+	"log"
+
+	_ "github.com/lib/pq"
+)
 
 func main() {
+	connStr := "user=postgres password=postgres dbname=store sslmode=disable"
+	db, err := sql.Open("postgres", connStr)
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	if err = db.Ping(); err != nil {
+		log.Fatalln(err)
+	}
+
 	fmt.Println("It works")
 }
--- geral/book_black_hat_go/ex018/go.mod
+++ geral/book_black_hat_go/ex018/go.mod
@@ -1,3 +1,5 @@
 module github.com/flaviogf/courses/geral/book_black_hat_go/ex018
 
 go 1.18
+
+require github.com/lib/pq v1.10.6
