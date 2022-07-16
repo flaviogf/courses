86607a2aa9d3c4c7c8434079a90b8c3dc09681be
flaviogf <flavio.fernandes6@gmail.com>
2022-07-16 17:07:49 -0300

    geral: black hat go

--- geral/book_black_hat_go/ex018/cmd/query/main.go
+++ geral/book_black_hat_go/ex018/cmd/query/main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"database/sql"
+	"log"
+
+	_ "github.com/lib/pq"
+)
 
 func main() {
-	fmt.Println("Hey")
+	connStr := "user=postgres password=postgres port=54320 dbname=store sslmode=disable"
+	conn, err := sql.Open("postgres", connStr)
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	if err = conn.Ping(); err != nil {
+		log.Fatalln(err)
+	}
+
+	log.Println("It works")
 }
