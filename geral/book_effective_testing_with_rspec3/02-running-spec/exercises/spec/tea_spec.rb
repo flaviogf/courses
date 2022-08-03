9b3feb06ba30876da9905597b74214a9eda74fcf
flaviogf <flavio.fernandes6@gmail.com>
2022-08-02 21:21:44 -0300

    geral: book effective testing with rspec 3

--- geral/book_effective_testing_with_rspec3/02-running-spec/exercises/spec/tea_spec.rb
+++ geral/book_effective_testing_with_rspec3/02-running-spec/exercises/spec/tea_spec.rb
@@ -6,6 +6,10 @@ class Tea
   def flavor
     :earl_grey
   end
+
+  def temperature
+    200.1
+  end
 end
 
 RSpec.describe Tea do
@@ -13,5 +17,9 @@ RSpec.describe Tea do
 
   it 'tastes like Earl Grey' do
     expect(tea.flavor).to be :earl_grey
+  end
+
+  it 'is hot' do
+    expect(tea.temperature).to be > 200.0
   end
 end
