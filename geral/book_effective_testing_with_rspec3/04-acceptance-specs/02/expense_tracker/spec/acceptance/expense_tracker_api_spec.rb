24b76de4320398877d6c654b14fd07b48130d217
flaviogf <flavio.fernandes6@gmail.com>
2022-08-07 07:36:00 -0300

    geral: book effective testing with rspec 3

---
+++ geral/book_effective_testing_with_rspec3/04-acceptance-specs/02/expense_tracker/spec/acceptance/expense_tracker_api_spec.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+require 'json'
+require 'rack/test'
+
+require 'spec_helper'
+
+module ExpenseTracker
+  RSpec.describe 'Expense Tracker API' do
+    include Rack::Test::Methods
+
+    it 'records submitted expenses' do
+      coffee = {
+        payee: 'Starbucks',
+        price: 575,
+        date: '2017-06-20'
+      }
+
+      post '/expenses', JSON.dump(coffee)
+    end
+
+    def app
+      ExpenseTracker::API.new
+    end
+  end
+end
