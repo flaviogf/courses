22ec838a0d437e078af9e580811fc7ad5eeb9c8e
flaviogf <flavio.fernandes6@gmail.com>
2022-09-03 15:08:25 -0300

    geral: book effective testing with rspec 3

--- geral/book_effective_testing_with_rspec3/05-unit-specs/exercise/expense_tracker/spec/unit/app/api_spec.rb
+++ geral/book_effective_testing_with_rspec3/05-unit-specs/exercise/expense_tracker/spec/unit/app/api_spec.rb
@@ -1,10 +1,22 @@
 # frozen_string_literal: true
 
+require 'spec_helper'
+
 module ExpenseTracker
   RSpec.describe API do
+    include Rack::Test::Methods
+
     describe '/expenses (POST)' do
       it 'returns status 200 (OK)' do
+        expense = { 'data' => 'something' }
+        post '/expenses', JSON.dump(expense)
+
+        expect(last_response.status). to eq(200)
       end
+    end
+
+    def app
+      ExpenseTracker::API.new
     end
   end
 end
