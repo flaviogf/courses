160a9b870c36b43c4020a37733025c9a2f106e88
flaviogf <flavio.fernandes6@gmail.com>
2022-09-03 15:04:41 -0300

    geral: book effective testing with rspec 3

---
+++ geral/book_effective_testing_with_rspec3/05-unit-specs/exercise/expense_tracker/spec/unit/app/api_spec.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+module ExpenseTracker
+  RSpec.describe API do
+    describe '/expenses (POST)' do
+      it 'returns status 200 (OK)' do
+      end
+    end
+  end
+end
