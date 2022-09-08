61d9e342599eb868c6061e2fc1c10092b1fd3c77
flaviogf <flavio.fernandes6@gmail.com>
2022-09-08 13:45:29 -0300

    book: fluent python

--- geral/book_fluent_python/001.py
+++ geral/book_fluent_python/001.py
@@ -41,3 +41,12 @@ if __name__ == '__main__':
 
     print(Card('Q', 'hearts') in deck)
     print(Card('7', 'beasts') in deck)
+
+    suit_values = dict(spades=3, hearts=2, diamonds=1, clubs=0)
+
+    def spades_high(card):
+        rank_value = FrenchDeck.ranks.index(card.rank)
+        return rank_value * len(suit_values) + suit_values[card.suit]
+
+    for card in sorted(deck, key=spades_high):
+        print(card)
