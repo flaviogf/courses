c47e3d14b9b176db6165ddf935285be145a03dda
flaviogf <flavio.fernandes6@gmail.com>
2022-10-09 17:36:33 -0300

    geral: go developing a restful api with go and gin

--- geral/go_developing_a_restful_api_with_go_and_gin/web-service-gin/main.go
+++ geral/go_developing_a_restful_api_with_go_and_gin/web-service-gin/main.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
 // album represents data about a record album
 type album struct {
 	ID     string  `json:"id"`
@@ -13,4 +19,15 @@ var albums = []album{
 	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
 	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
 	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sara Vaughan", Price: 39.99},
+}
+
+func main() {
+	router := gin.Default()
+	router.GET("/albums", getAlbums)
+
+	router.Run("localhost:8080")
+}
+
+func getAlbums(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, albums)
 }
