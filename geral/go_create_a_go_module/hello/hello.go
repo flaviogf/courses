7e7feef1017c90876eb30b4be5766dd0e3ec1e91
flaviogf <flavio.fernandes6@gmail.com>
2022-10-08 21:07:47 -0300

    geral: go create a go module

--- geral/go_create_a_go_module/hello/hello.go
+++ geral/go_create_a_go_module/hello/hello.go
@@ -1,12 +1,28 @@
 package main
 
 import (
-	"example.com/greetings"
 	"fmt"
+	"log"
+
+	"example.com/greetings"
 )
 
 func main() {
-	message := greetings.Hello("Gladys")
+	// Set properties of the predefined Logger, including
+	// the log entry prefix and a flag to disable printing
+	// the time, source file, and line number.
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
 
+	message, err := greetings.Hello("")
+
+	// If an error was returned, print it to the console and
+	// exit the program.
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// If no error was returned, print the returned message
+	// to the console
 	fmt.Println(message)
 }
