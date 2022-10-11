c37d5cae4493e0798d5565c9f06e5e91d36a789a
flaviogf <flavio.fernandes6@gmail.com>
2022-10-10 21:23:27 -0300

    geral: go getting started with generics

--- geral/go_getting_started_with_generics/generics/main.go
+++ geral/go_getting_started_with_generics/generics/main.go
@@ -16,6 +16,18 @@ func main() {
 	fmt.Printf("Non-Generic Sums: %v and %v\n", SumInts(ints), SumFloats(floats))
 }
 
+// SumIntsOrFloats sums the values of map m. It supports both int64 and float64
+// as types for map values.
+func SumIntsOrFloats[K comparable, V int64 | float64](m map[K]V) V {
+	var s V
+
+	for _, v := range m {
+		s += v
+	}
+
+	return s
+}
+
 func SumInts(m map[string]int64) int64 {
 	var s int64
 
