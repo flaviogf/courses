c3e1e4301527a9a0ebbedd56ed006c1e24d417c5
flaviogf <flavio.fernandes6@gmail.com>
2022-10-12 07:15:10 -0300

    geral: go getting started with fuzzing

--- geral/go_getting_started_with_fuzzing/fuzz/reverse_test.go
+++ geral/go_getting_started_with_fuzzing/fuzz/reverse_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"testing"
+	"unicode/utf8"
+)
 
 func TestReverse(t *testing.T) {
 	testcases := []struct {
@@ -12,10 +15,40 @@ func TestReverse(t *testing.T) {
 	}
 
 	for _, tc := range testcases {
-		rev := Reverse(tc.in)
+		rev, _ := Reverse(tc.in)
 
 		if rev != tc.want {
 			t.Errorf("Reverse: %q, want %q", rev, tc.want)
 		}
 	}
+}
+
+func FuzzReverse(f *testing.F) {
+	testcases := []string{"Hello, world", " ", "!12345"}
+
+	for _, tc := range testcases {
+		f.Add(tc)
+	}
+
+	f.Fuzz(func(t *testing.T, orig string) {
+		rev, err1 := Reverse(orig)
+
+		if err1 != nil {
+			return
+		}
+
+		doubleRev, err2 := Reverse(rev)
+
+		if err2 != nil {
+			return
+		}
+
+		if orig != doubleRev {
+			t.Errorf("Before: %q, after: %q", orig, doubleRev)
+		}
+
+		if utf8.ValidString(orig) && !utf8.ValidString(rev) {
+			t.Errorf("Reverse produced invalid UTF-8 string %q", rev)
+		}
+	})
 }
