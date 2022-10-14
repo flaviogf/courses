86cab930d4d145861dde4ffd877e24414d31fe93
flaviogf <flavio.fernandes6@gmail.com>
2022-10-14 17:42:35 -0300

    geral: go writing web applications

--- geral/go_writing_web_applications/gowiki/wiki.go
+++ geral/go_writing_web_applications/gowiki/wiki.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"errors"
 	"html/template"
 	"log"
 	"net/http"
 	"os"
+	"regexp"
 )
 
 type Page struct {
@@ -14,6 +16,8 @@ type Page struct {
 
 var templates = template.Must(template.ParseFiles("edit.html", "view.html"))
 
+var validPath = regexp.MustCompile("^/(edit|save|view)/([a-zA-Z0-9]+)$")
+
 func main() {
 	http.HandleFunc("/view/", viewHandler)
 	http.HandleFunc("/edit/", editHandler)
@@ -22,7 +26,12 @@ func main() {
 }
 
 func viewHandler(w http.ResponseWriter, r *http.Request) {
-	title := r.URL.Path[len("/view/"):]
+	title, err := getTitle(w, r)
+
+	if err != nil {
+		return
+	}
+
 	p, err := loadPage(title)
 
 	if err != nil {
@@ -34,7 +43,12 @@ func viewHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func editHandler(w http.ResponseWriter, r *http.Request) {
-	title := r.URL.Path[len("/edit/"):]
+	title, err := getTitle(w, r)
+
+	if err != nil {
+		return
+	}
+
 	p, err := loadPage(title)
 
 	if err != nil {
@@ -45,10 +59,15 @@ func editHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func saveHandler(w http.ResponseWriter, r *http.Request) {
-	title := r.URL.Path[len("/save/"):]
+	title, err := getTitle(w, r)
+
+	if err != nil {
+		return
+	}
+
 	body := r.FormValue("body")
 	p := &Page{Title: title, Body: []byte(body)}
-	err := p.save()
+	err = p.save()
 
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
@@ -67,6 +86,17 @@ func loadPage(title string) (*Page, error) {
 	}
 
 	return &Page{Title: title, Body: body}, nil
+}
+
+func getTitle(w http.ResponseWriter, r *http.Request) (string, error) {
+	m := validPath.FindStringSubmatch(r.URL.Path)
+
+	if m == nil {
+		http.NotFound(w, r)
+		return "", errors.New("invalid page title")
+	}
+
+	return m[2], nil
 }
 
 func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
