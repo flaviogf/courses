41d43ae709143cddcfa76e189fd70b7fd7bda67d
flaviogf <flavio.fernandes6@gmail.com>
2022-10-14 17:07:04 -0300

    geral: go writing web applications

--- geral/go_writing_web_applications/gowiki/wiki.go
+++ geral/go_writing_web_applications/gowiki/wiki.go
@@ -20,7 +20,13 @@ func main() {
 
 func viewHandler(w http.ResponseWriter, r *http.Request) {
 	title := r.URL.Path[len("/view/"):]
-	p, _ := loadPage(title)
+	p, err := loadPage(title)
+
+	if err != nil {
+		http.Redirect(w, r, "/edit/"+title, http.StatusFound)
+		return
+	}
+
 	renderTemplate(w, "view", p)
 }
 
