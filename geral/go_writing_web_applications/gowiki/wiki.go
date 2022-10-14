f74910eb4da5c98dc7be0409de50220eecf5c7de
flaviogf <flavio.fernandes6@gmail.com>
2022-10-14 16:50:25 -0300

    geral: go writin web applications

--- geral/go_writing_web_applications/gowiki/wiki.go
+++ geral/go_writing_web_applications/gowiki/wiki.go
@@ -14,6 +14,7 @@ type Page struct {
 
 func main() {
 	http.HandleFunc("/view/", viewHandler)
+	http.HandleFunc("/edit/", editHandler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
@@ -21,6 +22,23 @@ func viewHandler(w http.ResponseWriter, r *http.Request) {
 	title := r.URL.Path[len("/view/"):]
 	page, _ := loadPage(title)
 	fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", page.Title, page.Body)
+}
+
+func editHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/edit/"):]
+	p, err := loadPage(title)
+
+	if err != nil {
+		p = &Page{Title: title}
+	}
+
+	fmt.Fprintf(
+		w,
+		"<h1>Editing %s</h1><form action=\"/save/%s\" method=\"POST\"><textarea name=\"body\">%s</textarea><br><input type=\"submit\" value=\"Save\"></form>",
+		p.Title,
+		p.Title,
+		p.Body,
+	)
 }
 
 func loadPage(title string) (*Page, error) {
