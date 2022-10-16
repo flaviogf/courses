3a31670140e7d22521caecde9f5ab0adec14a4e5
flaviogf <flavio.fernandes6@gmail.com>
2022-10-16 18:51:51 -0300

    geral: go how to write go code

--- geral/go_how_to_write_go_code/hello/go.mod
+++ geral/go_how_to_write_go_code/hello/go.mod
@@ -1,3 +1,5 @@
 module example/user/hello
 
 go 1.19
+
+require github.com/google/go-cmp v0.5.9
--- geral/go_how_to_write_go_code/hello/hello.go
+++ geral/go_how_to_write_go_code/hello/hello.go
@@ -3,8 +3,11 @@ package main
 import (
 	"example/user/hello/morestrings"
 	"fmt"
+
+	"github.com/google/go-cmp/cmp"
 )
 
 func main() {
 	fmt.Println(morestrings.ReverseRunes("!oG ,olleH"))
+	fmt.Println(cmp.Diff("Hello World", "Hello Go"))
 }
