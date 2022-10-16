66319ef68ff15eb164b3ec0bd9a3322d1ee93919
flaviogf <flavio.fernandes6@gmail.com>
2022-10-16 18:49:42 -0300

    geral: go how to write go code

--- geral/go_how_to_write_go_code/hello/hello.go
+++ geral/go_how_to_write_go_code/hello/hello.go
@@ -1,7 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"example/user/hello/morestrings"
+	"fmt"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	fmt.Println(morestrings.ReverseRunes("!oG ,olleH"))
 }
