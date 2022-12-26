06cf268d4e6273de6d18ff13bde89d504866d92b
flaviogf <flavio.fernandes6@gmail.com>
2022-12-26 14:02:57 -0300

    geral: terraform

---
+++ geral/terraform_docker_tutorial/main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "2.24.0"
+    }
+  }
+}
+
+provider "docker" {}
+
+resource "docker_image" "nginx" {
+  name = "nginx:latest"
+}
+
+resource "docker_container" "nginx" {
+  image = docker_image.nginx.image_id
+  name  = "tutorial"
+  ports {
+    internal = 80
+    external = 8000
+  }
+}
