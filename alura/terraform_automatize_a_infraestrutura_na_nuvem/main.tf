01f0744dd458f68b875f416831e135c523bbe624
flaviogf <flavio.fernandes6@gmail.com>
2022-12-29 07:07:28 -0300

    alura: terraform

--- alura/terraform_automatize_a_infraestrutura_na_nuvem/main.tf
+++ alura/terraform_automatize_a_infraestrutura_na_nuvem/main.tf
@@ -12,10 +12,23 @@ provider "aws" {
 }
 
 resource "aws_instance" "dev" {
-  count         = 3
-  ami           = "ami-0574da719dca65348"
-  instance_type = "t2.micro"
+  count           = 3
+  ami             = "ami-0574da719dca65348"
+  instance_type   = "t2.micro"
+  key_name        = "terraform-aws"
+  security_groups = ["sg-0dbb8840ebbcd0c72"]
+
   tags = {
     Name = "dev-${count.index}"
+  }
+}
+
+resource "aws_security_group" "allow_ssh" {
+  name = "allow_ssh"
+
+  ingress {
+    from_port = 22
+    to_port   = 22
+    protocol  = "tcp"
   }
 }
