ec1a87a21533835de7d66bc31f7afab729c54ff0
flaviogf <flavio.fernandes6@gmail.com>
2023-01-28 15:00:25 -0300

    alura: java8

--- alura/java8_novidades/example2/src/main/java/br/com/flaviogf/example2/App.java
+++ alura/java8_novidades/example2/src/main/java/br/com/flaviogf/example2/App.java
@@ -1,19 +1,38 @@
 package br.com.flaviogf.example2;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class App {
   public static void main(String[] args) {
+    var t1 = new Thread(() -> execute("Thread 1"));
+    var t2 = new Thread(() -> execute("Thread 2"));
+
+    var threads = Arrays.asList(t1, t2);
+
+    threads.forEach(t -> t.start());
+
+    threads.forEach(t -> {
+      try {
+        t.join();
+      } catch (InterruptedException e) {
+      }
+    });
+  }
+
+  private static void execute(String name) {
     var courses = new ArrayList<String>();
 
     courses.add("casa do código");
     courses.add("alura online");
     courses.add("caelum");
 
+    System.out.println(name);
     courses.forEach(s -> System.out.println(s));
 
     courses.sort((s1, s2) -> Integer.compare(s1.length(), s2.length()));
 
+    System.out.println(name);
     courses.forEach(s -> System.out.println(s));
   }
 }
