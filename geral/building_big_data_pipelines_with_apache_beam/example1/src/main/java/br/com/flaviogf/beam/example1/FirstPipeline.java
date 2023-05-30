6e1400e2d12569c401e390a1775037aee0f42118
flaviogf <flavio.fernandes6@gmail.com>
2023-05-30 16:11:13 -0300

    geral: pipeline

---
+++ geral/building_big_data_pipelines_with_apache_beam/example1/src/main/java/br/com/flaviogf/beam/example1/FirstPipeline.java
@@ -0,0 +1,27 @@
+package br.com.flaviogf.beam.example1;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+import org.apache.beam.sdk.Pipeline;
+import org.apache.beam.sdk.transforms.Create;
+import org.apache.beam.sdk.values.PCollection;
+
+public class FirstPipeline {
+  public static void main(String[] args) throws IOException {
+    ClassLoader loader = FirstPipeline.class.getClassLoader();
+    String file = loader.getResource("lorem.txt").getFile();
+    List<String> lines = Files.readAllLines(Paths.get(file), StandardCharsets.UTF_8);
+
+    Pipeline pipeline = Pipeline.create();
+
+    PCollection<String> input = pipeline.apply(Create.of(lines));
+
+    input.apply(PrintElements.of());
+
+    pipeline.run().waitUntilFinish();
+  }
+}
---
+++ geral/building_big_data_pipelines_with_apache_beam/example1/src/main/java/br/com/flaviogf/beam/example1/PrintElements.java
@@ -0,0 +1,26 @@
+package br.com.flaviogf.beam.example1;
+
+import org.apache.beam.sdk.transforms.DoFn;
+import org.apache.beam.sdk.transforms.PTransform;
+import org.apache.beam.sdk.transforms.ParDo;
+import org.apache.beam.sdk.values.PCollection;
+import org.apache.beam.sdk.values.PDone;
+
+public class PrintElements<T> extends PTransform<PCollection<T>, PDone> {
+  public static <T> PrintElements<T> of() {
+    return new PrintElements<T>();
+  }
+
+  @Override
+  public PDone expand(PCollection<T> input) {
+    input.apply(ParDo.of(new LogResultDoFn<T>()));
+    return PDone.in(input.getPipeline());
+  }
+
+  private static class LogResultDoFn<T> extends DoFn<T, Void> {
+    @ProcessElement
+    public void process(@Element T element) {
+      System.out.println(element);
+    }
+  }
+}
